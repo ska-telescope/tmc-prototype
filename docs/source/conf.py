a31bdd22b856b7ca4ab18bacbe6ae82baf28e97e
Bartolini <m.bartolini@skatelescope.org>
2019-02-01 16:41:05 +0000

    add SKA theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,11 @@ import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
 
+import sphinx_rtd_theme
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
 # -- Project information -----------------------------------------------------
 
 project = 'TMC Prototype'
@@ -86,7 +91,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
