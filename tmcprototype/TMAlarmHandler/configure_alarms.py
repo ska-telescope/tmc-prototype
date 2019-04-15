09476a99b3808d3a3b5729a91f712f6cc5ac2a02
Aditya Dange <adityadange.ska@gmail.com>
2019-04-15 20:04:15 +0530

    AT1-195: Configure alarmS in TM AlarmHandler

--- tmcprototype/TMAlarmHandler/configure_alarms.py
+++ tmcprototype/TMAlarmHandler/configure_alarms.py
@@ -1,26 +1,19 @@
 from tango import DeviceProxy
+import json
+
+with open('/app/tmcprototype/TMAlarmHandler/alarms.json', 'r') as file:
+#with open('/home/user/projects/tmc-prototype/tmcprototype/TMAlarmHandler/alarms.json', 'r') as file:
+    jsonAlarmsString = file.read().replace('\n', '')
 
 # Creating DeviceProxy for Alarm Handler device
 alarmHandler_proxy = DeviceProxy("alarmhandler/1/1")
 
 # Configure Alarms on DishMaster WindSpeed attribute
-alarmHandler_proxy.command_inout("Load", "tag=alertDish1WindSpeed;"
-                                         "formula=(mid_d0001/elt/master/WindSpeed > 10);"
-                                         "on_delay=0;off_delay=0;priority=fault;"
-                                         "shlvd_time=2;group=gr_all;message=Dish1 Wind speed exceeded;"
-                                         "on_command=mid_d0001/elt/master/SetStowMode;enabled=1" )
-alarmHandler_proxy.command_inout("Load", "tag=alertDish2WindSpeed;"
-                                         "formula=(mid_d0002/elt/master/WindSpeed > 10);"
-                                         "on_delay=0;off_delay=0;priority=fault;"
-                                         "shlvd_time=2;group=gr_all;message=Dish2 Wind speed exceeded;"
-                                         "on_command=mid_d0002/elt/master/SetStowMode;enabled=1" )
-alarmHandler_proxy.command_inout("Load", "tag=alertDish3WindSpeed;"
-                                         "formula=(mid_d0003/elt/master/WindSpeed > 10);"
-                                         "on_delay=0;off_delay=0;priority=fault;"
-                                         "shlvd_time=2;group=gr_all;message=Dish3 Wind speed exceeded;"
-                                         "on_command=mid_d0003/elt/master/SetStowMode;enabled=1" )
-alarmHandler_proxy.command_inout("Load", "tag=alertDish4WindSpeed;"
-                                         "formula=(mid_d0004/elt/master/WindSpeed > 10);"
-                                         "on_delay=0;off_delay=0;priority=fault;"
-                                         "shlvd_time=2;group=gr_all;message=Dish4 Wind speed exceeded;"
-                                         "on_command=mid_d0004/elt/master/SetStowMode;enabled=1" )
+# Loading alarms.json file and creating an object
+try:
+    json_alarms = json.loads(jsonAlarmsString)
+except Exception as e:
+    print("Exception in JSON parsing:", e.__str__())
+
+for alarm in json_alarms:
+    alarmHandler_proxy.command_inout("Load", alarm["input_string"])
