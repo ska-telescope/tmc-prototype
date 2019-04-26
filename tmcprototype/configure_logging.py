075d4e7b79ceba50a1db14868d223f802aeddfaf
Aditya Dange <adityadange.ska@gmail.com>
2019-04-26 12:25:46 +0530

    Commit for pipeline testing
    The logger device runs intermittently. Due to this logger configuration fails. To run the logger functionality, manual configuration of the logging parameters is required.

--- tmcprototype/configure_logging.py
+++ tmcprototype/configure_logging.py
@@ -14,29 +14,26 @@ logger_device = "ref/elt/logger"
 logger_proxy = DeviceProxy(logger_device)
 
 logging_target = "device::" + logger_device
-print("logging_target: ", logging_target)
 
 # Parse json
 json_devices = json.loads(jsonDevices)
 
 for device in json_devices:
-    print("device name: ", device["devName"])
     if device["devName"] != logger_device:
-        # Set Logging Level
+        # Set Logging Level property
         device_proxy = DeviceProxy(device["devName"])
-        print("device_proxy : ", device_proxy )
         device_proxy.set_logging_level(5)
-        time.sleep(2)
+        time.sleep(3)
 
         try:
             # Set Logging target
             device_proxy.add_logging_target(logging_target)
-            time.sleep(2)
+            time.sleep(3)
         except DevFailed as df:
             print("Failed to set logging target: ", df)
 
-         # Setting Element Logging Level
+         # Setting Element Logging Level attribute on the deivce
         logger_proxy.command_inout("SetCentralLoggingLevel", ([5], [device["devName"]]))
-        time.sleep(2)
+        time.sleep(3)
         logger_proxy.command_inout("SetElementLoggingLevel", ([5], [device["devName"]]))
-        time.sleep(2)
+        time.sleep(3)
