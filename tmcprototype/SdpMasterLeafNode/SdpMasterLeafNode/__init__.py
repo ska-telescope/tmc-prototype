27fcfaa53a5285f3bca11ea0741fada797b5f280
vinodsathe <vinodnsathe.ska@gmail.com>
2019-06-28 20:11:54 +0530

    story-AT1-239: Created SDP Master Leaf Node.

---
+++ tmcprototype/SdpMasterLeafNode/SdpMasterLeafNode/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SdpMasterLeafNode project
+#
+#
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE.txt for more info.
+
+"""SdpMasterLeafNode
+
+TBD
+"""
+
+from . import release
+from .SdpMasterLeafNode import SdpMasterLeafNode, main
+
+__version__ = release.version
+__version_info__ = release.version_info
+__author__ = release.author
---
+++ tmcprototype/SdpMasterLeafNode/SdpMasterLeafNode/release.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SdpMasterLeafNode project
+#
+#
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE.txt for more info.
+
+"""Release information for Python Package"""
+
+name = """tangods-sdpmasterleafnode"""
+version = "1.0.0"
+version_info = version.split(".")
+description = """TBD"""
+author = "vinodnsathe.ska"
+author_email = "vinodnsathe.ska at gmail.com"
+license = """GPL"""
+url = """www.tango-controls.org"""
+copyright = """"""
