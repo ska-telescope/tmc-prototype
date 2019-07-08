73bcb844842037a373a35dd08784274fe9c0110d
vinodsathe <vinodnsathe.ska@gmail.com>
2019-07-08 11:35:59 +0530

    story-AT1-243: Merge updated SDP Master device with operating state attribute.

---
+++ tmcprototype/SdpMaster/SdpMaster/release.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SdpMaster project
+#
+#
+#
+# Distributed under the terms of the GPL license.
+# See LICENSE.txt for more info.
+
+"""Release information for Python Package"""
+
+name = """tangods-sdpmaster"""
+version = "1.0.0"
+version_info = version.split(".")
+description = """"""
+author = "kalyanit.ska"
+author_email = "kalyanit.ska at gmail.com"
+license = """GPL"""
+url = """www.tango-controls.org"""
+copyright = """"""
