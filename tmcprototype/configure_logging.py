15129f005c510005297983150639bafdf0dc0b03
Jayant Kumbhar <jayant.ska@gmail.com>
2019-10-21 12:10:56 +0530

    AT1-349: Update image version of CSP.

--- tmcprototype/configure_logging.py
+++ tmcprototype/configure_logging.py
@@ -1,17 +1,20 @@
 import sys
 import json
 import time
+import numpy as np
 from tango import DeviceProxy, DevFailed
 
 # Update file path to devices.json in order to test locally
 # To test on docker environment use path : /app/tmcprototoype/devices.json
-config_json_file = '/app/tmcprototype/devices.json'
+config_json_file = '/app/data/tmc-devices.json'
 with open(config_json_file, 'r') as file:
     str_json_devices = file.read().replace('\n', '')
 
 # Creating SKALogger DeviceProxy
 logger_device = "ref/elt/logger"
 logger_proxy = DeviceProxy(logger_device)
+print("Logger_proxy is :",logger_proxy)
+
 
 max_retries = 10
 retry = 0
@@ -25,6 +28,7 @@ while retry < max_retries:
 
 if(retry < max_retries):
     logging_target = "device::" + logger_device
+    print(" loggging target is",logging_target)
 
     try:
         # Parse json
@@ -33,24 +37,39 @@ if(retry < max_retries):
 
         for device_server in device_servers:
             try:
+
                 if (device_server != "SKALogger" and
                     device_server != "TangoTest" and
-                    device_server != "Databaseds" and
+                    device_server != "DataBase" and
                     device_server != "TangoAccessControl"
                 ):
                     server_instance_list = device_servers[device_server].keys()
+                    #print("server_instance_list is", server_instance_list)
                     for server_instance in server_instance_list:
                         devices_class_list = device_servers[device_server][server_instance].keys()
                         for devices_class in devices_class_list:
                             device_name_list = device_servers[device_server][server_instance][devices_class].keys()
                             for device_name in device_name_list:
+                                print("device name is", device_name)
                                 device_proxy = DeviceProxy(device_name)
+                                print("device_proxy name is",device_proxy)
+
                                 # Set Logging Level property
+                                print ("Logging target:", logging_target)
                                 device_proxy.add_logging_target(logging_target)
-                                # Set logging levels of the device
-                                logger_proxy.command_inout("SetCentralLoggingLevel", ([5], device_name))
-                                logger_proxy.command_inout("SetElementLoggingLevel", ([5], device_name))
-                                logger_proxy.command_inout("SetStorageLoggingLevel", ([5], device_name))
+                                try:
+                                    logging_level = []
+                                    logging_level.append(5)
+                                    source_device = []
+                                    source_device.append(device_name)
+                                    device_details = []
+                                    device_details.append(logging_level)
+                                    device_details.append(source_device)
+                                    logger_proxy.command_inout("SetCentralLoggingLevel",device_details)
+                                    logger_proxy.command_inout("SetElementLoggingLevel",device_details)
+                                    logger_proxy.command_inout("SetStorageLoggingLevel",device_details)
+                                except Exception as ex:
+                                    print ("Exception occured.",ex)
             except DevFailed as df:
                 print("Exception:", df)
     except Exception as ex:
