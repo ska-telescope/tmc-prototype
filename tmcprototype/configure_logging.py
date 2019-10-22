d8488c155e50d5c4940dbe52585ac2446c43334a
Jayant Kumbhar <jayant.ska@gmail.com>
2019-10-22 15:11:47 +0530

    AT1-349: Resolve review comments.

--- tmcprototype/configure_logging.py
+++ tmcprototype/configure_logging.py
@@ -1,28 +1,31 @@
 import sys
 import json
 import time
+import tango
 from tango import DeviceProxy, DevFailed
 
 # Update file path to devices.json in order to test locally
 # To test on docker environment use path : /app/tmcprototoype/devices.json
-config_json_file = '/app/tmcprototype/devices.json'
+config_json_file = '/app/data/tmc-devices.json'
 with open(config_json_file, 'r') as file:
     str_json_devices = file.read().replace('\n', '')
 
 # Creating SKALogger DeviceProxy
 logger_device = "ref/elt/logger"
 logger_proxy = DeviceProxy(logger_device)
-
+# Check if Logger device is up and running
 max_retries = 10
 retry = 0
 while retry < max_retries:
     try:
-        logger_proxy.ping()
+
         break
     except DevFailed:
         time.sleep(3)
         retry += 1
 
+excpt_msg = []
+excpt_count = 0
 if(retry < max_retries):
     logging_target = "device::" + logger_device
 
@@ -35,29 +38,54 @@ if(retry < max_retries):
             try:
                 if (device_server != "SKALogger" and
                     device_server != "TangoTest" and
-                    device_server != "Databaseds" and
+                    device_server != "DataBaseds" and
                     device_server != "TangoAccessControl"
                 ):
                     server_instance_list = device_servers[device_server].keys()
                     for server_instance in server_instance_list:
                         devices_class_list = device_servers[device_server][server_instance].keys()
                         for devices_class in devices_class_list:
-                            device_name_list = device_servers[device_server][server_instance][devices_class].keys()
+                            device_name_list = \
+                                device_servers[device_server][server_instance][devices_class].keys()
                             for device_name in device_name_list:
                                 device_proxy = DeviceProxy(device_name)
                                 # Set Logging Level property
                                 device_proxy.add_logging_target(logging_target)
-                                # Set logging levels of the device
-                                logger_proxy.command_inout("SetCentralLoggingLevel", ([5], device_name))
-                                logger_proxy.command_inout("SetElementLoggingLevel", ([5], device_name))
-                                logger_proxy.command_inout("SetStorageLoggingLevel", ([5], device_name))
-            except DevFailed as df:
-                print("Exception:", df)
-    except Exception as ex:
-        print("Exception: ", ex)
+                                try:
+                                    # Set logging level value
+                                    logging_level = []
+                                    logging_level.append(5)
+                                    # Add source TANGO device for logging
+                                    source_device = []
+                                    source_device.append(device_name)
+                                    device_details = []
+                                    device_details.append(logging_level)
+                                    device_details.append(source_device)
+                                    # Invoke Set commands at logger device to update logging levels
+                                    logger_proxy.command_inout("SetCentralLoggingLevel",device_details)
+                                    logger_proxy.command_inout("SetElementLoggingLevel",device_details)
+                                    logger_proxy.command_inout("SetStorageLoggingLevel",device_details)
+                                except Exception as except_occurred:
+                                    print ("Exception :", except_occurred)
+                                    excpt_msg.append(except_occurred)
+                                    excpt_count += 1
+            except DevFailed as dev_failed:
+                print("Exception: ", dev_failed)
+                excpt_msg.append(dev_failed)
+                excpt_count += 1
+    except Exception as except_occurred:
+        print("Exception : ", except_occurred)
+        excpt_msg.append(except_occurred)
+        excpt_count += 1
     ret_val = 0
 else:
-    print("Logger device not ready")
+    print("Logger device is not ready.")
     ret_val = -1
+
+if excpt_count > 0:
+    err_msg = ' '
+    for item in excpt_msg:
+        err_msg += item + "\n"
+    tango.Except.throw_exception(err_msg, tango.ErrSeverity.ERR)
 
 sys.exit(ret_val)
