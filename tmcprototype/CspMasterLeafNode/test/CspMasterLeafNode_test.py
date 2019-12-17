4dc5df8b0f5228327a7fdfb96dc506759c084bce
Apurva Patkar <apurva.ska@gmail.com>
2019-12-17 16:46:52 +0530

    new_logger: test case of logger is added.

--- tmcprototype/CspMasterLeafNode/test/CspMasterLeafNode_test.py
+++ tmcprototype/CspMasterLeafNode/test/CspMasterLeafNode_test.py
@@ -186,7 +186,7 @@ class TestCspMasterLeafNode(object):
     def test_loggingLevel(self, tango_context):
         """Test for loggingLevel"""
         # PROTECTED REGION ID(DishMaster.test_loggingLevel) ENABLED START #
-        tango_context.device.loggingLevel = TangoLoggingLevel.DEBUG
-        assert tango_context.device.loggingLevel == TangoLoggingLevel.DEBUG
+        tango_context.device.loggingLevel = TangoLoggingLevel.INFO
+        assert tango_context.device.loggingLevel == TangoLoggingLevel.INFO
         # PROTECTED REGION END #    //  DishMaster.test_loggingLevel
 
