a0c833b920e0fb222e892d224890121ff267c020
Sett Wai <swai@ska.ac.za>
2020-01-27 11:28:59 +0200

    Check-in test outline in pseudocode

---
+++ tmcprototype/CentralNode/test/central_node_test.py
@@ -0,0 +1,16 @@
+
+def test_telescope_health_state_is_degraded_when_one_or_more_tango_devices_fail_to_start():
+    # arrange:
+    # a telescope with at least one failed device
+    # device_under_test = CentralNode()
+
+    # act:
+    # device_under_test.StartUpTelescope()
+
+    # assert:
+    # all devices receive command ON (i.e. CentralNode sends Tango Command)
+    # CentralNode.assert_called('_read_device_attribute')
+    # assert device_under_test.telescopeHealthState == 'DEGRADED'
+    pass
+
+
