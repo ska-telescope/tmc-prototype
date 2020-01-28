ba2f3c86bb6588c55bc24a53f1df139f57131b11
Sett Wai <swai@ska.ac.za>
2020-01-28 15:04:11 +0200

    Passing unit test using mocks
    This completes a passing unit test that mocks CentralNode's DeviceProxy
    instantiation (to connect to CspLeafNodeMaster), by mocking the entire
    DeviceProxy class that it imports.
    
    We also mock the event system. In particular, the `tango.subscribe_event`
    method that is used by CentralNode to subscribe to healthState attribute
    changes.
    
    Bugfixes:
    - initialise some Tango attributes with default values
    - initialiase `_sdp_master_leaf_health` and `_csp_master_leaf_health` in
      `init_device`
    - fix the health_state counting logic

--- tmcprototype/CentralNode/test/central_node_test.py
+++ tmcprototype/CentralNode/test/central_node_test.py
@@ -1,16 +1,47 @@
+import importlib
+import tango
 
-def test_telescope_health_state_is_degraded_when_one_or_more_tango_devices_fail_to_start():
+from unittest import mock
+from unittest.mock import MagicMock, Mock
+
+from tango.test_context import DeviceTestContext
+from tango import DevFailed
+
+
+def test_telescope_health_state_is_degraded_when_csp_master_is_degraded_after_start():
     # arrange:
-    # a telescope with at least one failed device
-    # device_under_test = CentralNode()
+    events = {
+            "cspHealthState": None,
+    }
+
+    def subscribe_event(attribute_name, event_type, callback_fn, *args, **kwargs):
+        events[attribute_name] = callback_fn
+    
+
+    with mock.patch('tango.DeviceProxy') as patched_constructor:
+        device_proxy_mock = Mock()
+        device_proxy_mock.subscribe_event.side_effect = subscribe_event
+
+        patched_constructor.return_value = device_proxy_mock
+        klass = importlib.import_module("CentralNode", "tmcprototype").CentralNode
+
+    degraded_device = "telescope/csp/master"
+    tango_context = DeviceTestContext(klass, 
+            properties={"CspMasterLeafNodeFQDN": degraded_device})
+
+    tango_context.start()
 
     # act:
-    # device_under_test.StartUpTelescope()
+    # simulated event
+    event = mock.MagicMock(spec=tango.EventData)
+    event.err = False
+    event.attr_value = Mock()
+    event.attr_value.value = 1
+    event.attr_name = degraded_device + "/healthState"
+    event.device = device_proxy_mock
+
+    events["cspHealthState"](event)
+
 
     # assert:
-    # all devices receive command ON (i.e. CentralNode sends Tango Command)
-    # CentralNode.assert_called('_read_device_attribute')
-    # assert device_under_test.telescopeHealthState == 'DEGRADED'
-    pass
-
-
+    assert tango_context.device.telescopeHealthState == 1
