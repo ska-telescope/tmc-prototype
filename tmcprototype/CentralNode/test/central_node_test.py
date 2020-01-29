3b69d6d982f592032dea37f9e2e2c9955f525119
samueltwum1 <samueltwum1@gmail.com>
2020-01-29 16:25:32 +0200

    Refactor: extract fake event system for testing

--- tmcprototype/CentralNode/test/central_node_test.py
+++ tmcprototype/CentralNode/test/central_node_test.py
@@ -1,6 +1,7 @@
 import importlib
 import tango
 import pytest
+import collections
 
 from unittest import mock
 from unittest.mock import MagicMock, Mock
@@ -16,17 +17,10 @@ from tango import DevFailed
 )
 def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after_start(subdevice_fqdn_prop_name, subdevice_fqdn_prop_value, subdevice_health_state_attribute, subdevice_health_state_value, expected_telescope_health_state):
     # arrange:
-    events = {
-            subdevice_health_state_attribute: None,
-    }
-
-    def subscribe_event(attribute_name, event_type, callback_fn, *args, **kwargs):
-        events[attribute_name] = callback_fn
+    device_proxy_mock = Mock()
+    event_system = _prepare_event_system(device_proxy_mock, subdevice_health_state_attribute)
     
     with mock.patch('tango.DeviceProxy') as patched_constructor:
-        device_proxy_mock = Mock()
-        device_proxy_mock.subscribe_event.side_effect = subscribe_event
-
         patched_constructor.return_value = device_proxy_mock
         klass = importlib.import_module("CentralNode", "tmcprototype").CentralNode
 
@@ -37,19 +31,31 @@ def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after
     tango_context.start()
 
     # act:
-    # simulated event
-    event = MagicMock(spec=tango.EventData)
-    event.err = False
-    event.attr_value = Mock()
-    event.attr_value.value = subdevice_health_state_value
-    event.attr_name = subdevice_fqdn_prop_value + '/healthState'
-    event.device = device_proxy_mock
-
-    events[subdevice_health_state_attribute](event)
+    event_system.trigger(subdevice_health_state_attribute, f"{subdevice_fqdn_prop_value}/healthState", subdevice_health_state_value, device_proxy_mock)
 
     # assert:
     assert tango_context.device.telescopeHealthState == expected_telescope_health_state
 
 
-    # assert:
-    assert tango_context.device.telescopeHealthState == 1
+def _prepare_event_system(mocked_device_proxy, attribute_to_mock):
+    events = {
+            attribute_to_mock: None,
+    }
+
+    def fake_subscribe_event(attribute_name, event_type, callback_fn, *args, **kwargs):
+        events[attribute_name] = callback_fn
+    
+    mocked_device_proxy.subscribe_event.side_effect = fake_subscribe_event
+
+    FakeEventSystem = collections.namedtuple('FakeEventSystem', ['events', 'trigger'])
+    def trigger(attribute_name, attr_fqdn, attr_value, device_obj=MagicMock(), error=False):
+        event = MagicMock(spec=tango.EventData)
+        event.device = device_obj
+        event.attr_value.value = attr_value
+        event.attr_name = attr_fqdn
+        event.err = error
+
+        events[attribute_name](event)
+
+    return FakeEventSystem(events, trigger)
+
