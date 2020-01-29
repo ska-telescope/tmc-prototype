70f62ccc180cc3d3a804757f71d6beb1d7ea0bcd
samueltwum1 <samueltwum1@gmail.com>
2020-01-29 15:46:12 +0200

    Parametrise telescope health unit test

--- tmcprototype/CentralNode/test/central_node_test.py
+++ tmcprototype/CentralNode/test/central_node_test.py
@@ -1,5 +1,6 @@
 import importlib
 import tango
+import pytest
 
 from unittest import mock
 from unittest.mock import MagicMock, Mock
@@ -8,16 +9,20 @@ from tango.test_context import DeviceTestContext
 from tango import DevFailed
 
 
-def test_telescope_health_state_is_degraded_when_csp_master_is_degraded_after_start():
+@pytest.mark.parametrize(
+    'subdevice_fqdn_prop_name,subdevice_fqdn_prop_value,subdevice_health_state_attribute,subdevice_health_state_value,expected_telescope_health_state', [
+        ('CspMasterLeafNodeFQDN', 'mid/csp_elt/master','cspHealthState',1, 1)
+    ]
+)
+def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after_start(subdevice_fqdn_prop_name, subdevice_fqdn_prop_value, subdevice_health_state_attribute, subdevice_health_state_value, expected_telescope_health_state):
     # arrange:
     events = {
-            "cspHealthState": None,
+            subdevice_health_state_attribute: None,
     }
 
     def subscribe_event(attribute_name, event_type, callback_fn, *args, **kwargs):
         events[attribute_name] = callback_fn
     
-
     with mock.patch('tango.DeviceProxy') as patched_constructor:
         device_proxy_mock = Mock()
         device_proxy_mock.subscribe_event.side_effect = subscribe_event
@@ -25,22 +30,25 @@ def test_telescope_health_state_is_degraded_when_csp_master_is_degraded_after_st
         patched_constructor.return_value = device_proxy_mock
         klass = importlib.import_module("CentralNode", "tmcprototype").CentralNode
 
-    degraded_device = "telescope/csp/master"
-    tango_context = DeviceTestContext(klass, 
-            properties={"CspMasterLeafNodeFQDN": degraded_device})
-
+    tango_context = DeviceTestContext(klass, properties={
+        subdevice_fqdn_prop_name: subdevice_fqdn_prop_value
+    })
+    
     tango_context.start()
 
     # act:
     # simulated event
-    event = mock.MagicMock(spec=tango.EventData)
+    event = MagicMock(spec=tango.EventData)
     event.err = False
     event.attr_value = Mock()
-    event.attr_value.value = 1
-    event.attr_name = degraded_device + "/healthState"
+    event.attr_value.value = subdevice_health_state_value
+    event.attr_name = subdevice_fqdn_prop_value + '/healthState'
     event.device = device_proxy_mock
 
-    events["cspHealthState"](event)
+    events[subdevice_health_state_attribute](event)
+
+    # assert:
+    assert tango_context.device.telescopeHealthState == expected_telescope_health_state
 
 
     # assert:
