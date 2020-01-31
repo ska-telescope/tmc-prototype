8616cc05643f0a29c3a1716a6f65f6c912dd0f85
Sett Wai <swai@ska.ac.za>
2020-01-31 17:35:43 +0200

    Refactor: Unravel everything to get minimum viable example for demonstration

--- tmcprototype/CentralNode/test/unit/central_node_test.py
+++ tmcprototype/CentralNode/test/unit/central_node_test.py
@@ -1,79 +1,55 @@
-import collections
 import importlib
 import sys
 from unittest import mock
-from unittest.mock import MagicMock, Mock
+from unittest.mock import Mock
 
-import pytest
-import tango
 from CONST import ENUM_DEGRADED
 from CentralNode import CentralNode
 from tango.test_context import DeviceTestContext
 
 
-@pytest.mark.parametrize(
-    'subdevice_fqdn_prop_name,subdevice_fqdn_prop_value,subdevice_health_state_attribute,subdevice_health_state_value,expected_telescope_health_state',
-    [
-        ('CspMasterLeafNodeFQDN', 'mid/csp_elt/master', 'cspHealthState', ENUM_DEGRADED, ENUM_DEGRADED)
-    ]
-)
-def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after_start(subdevice_fqdn_prop_name,
-                                                                                       subdevice_fqdn_prop_value,
-                                                                                       subdevice_health_state_attribute,
-                                                                                       subdevice_health_state_value,
-                                                                                       expected_telescope_health_state):
+def test_telescope_health_state_is_degraded_when_csp_master_leaf_node_is_degraded_after_start():
     # arrange:
     device_under_test = CentralNode
+    csp_master_fqdn = 'mid/csp_elt/master'
+    csp_master_health_attribute = 'cspHealthState'
     initial_dut_properties = {
-        subdevice_fqdn_prop_name: subdevice_fqdn_prop_value
+        'CspMasterLeafNodeFQDN': csp_master_fqdn
     }
 
-    device_proxy_mock = Mock()
-    event_system = _prepare_event_system(device_proxy_mock, subdevice_health_state_attribute)
+    event_subscription_map = {}
 
-    tango_context = fake_tango_system(device_under_test, device_proxy_mock, initial_dut_properties)
+    csp_master_device_proxy_mock = Mock()
+    csp_master_device_proxy_mock.subscribe_event.side_effect = (
+        lambda attr_name, event_type, callback, *args, **kwargs: event_subscription_map.update({attr_name: callback}))
+
+    proxies_to_mock = {
+        csp_master_fqdn: csp_master_device_proxy_mock
+    }
+
+    tango_context = fake_tango_system(device_under_test, initial_dut_properties, proxies_to_mock)
 
     tango_context.start()
 
     # act:
-    event_system.trigger(subdevice_health_state_attribute, f"{subdevice_fqdn_prop_value}/healthState",
-                         subdevice_health_state_value, device_proxy_mock)
+    fake_event = Mock()
+    fake_event.err = False
+    fake_event.attr_name = f"{csp_master_fqdn}/healthState"
+    fake_event.attr_value.value = ENUM_DEGRADED
+    event_subscription_map[csp_master_health_attribute](fake_event)
 
     # assert:
-    assert tango_context.device.telescopeHealthState == expected_telescope_health_state
+    assert tango_context.device.telescopeHealthState == ENUM_DEGRADED
     tango_context.stop()
 
 
-def fake_tango_system(device_under_test, dependent_device_proxy_mock, initial_dut_properties={},
+def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_mock={},
                       device_proxy_import_path='tango.DeviceProxy'):
+
     with mock.patch(device_proxy_import_path) as patched_constructor:
-        patched_constructor.return_value = dependent_device_proxy_mock
+        patched_constructor.side_effect = lambda device_fqdn: proxies_to_mock.get(device_fqdn, Mock())
         patched_module = importlib.reload(sys.modules[device_under_test.__module__])
 
     device_under_test = getattr(patched_module, device_under_test.__name__)
 
     return DeviceTestContext(device_under_test, properties=initial_dut_properties)
-
-
-def _prepare_event_system(mocked_device_proxy, attribute_to_mock):
-    events = {
-        attribute_to_mock: None,
-    }
-
-    def fake_subscribe_event(attribute_name, callback_fn):
-        events[attribute_name] = callback_fn
-
-    mocked_device_proxy.subscribe_event.side_effect = fake_subscribe_event
-
-    FakeEventSystem = collections.namedtuple('FakeEventSystem', ['events', 'trigger'])
-
-    def trigger(attribute_name, attr_fqdn, attr_value, device_obj=MagicMock(), error=False):
-        event = MagicMock(spec=tango.EventData)
-        event.device = device_obj
-        event.attr_value.value = attr_value
-        event.attr_name = attr_fqdn
-        event.err = error
-
-        events[attribute_name](event)
-
-    return FakeEventSystem(events, trigger)
