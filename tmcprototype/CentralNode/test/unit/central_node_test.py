483d8b69c471e4bf65b08ef7211544279b65274e
Sett Wai <swai@ska.ac.za>
2020-01-31 11:29:03 +0200

    Refactor: extract patching logic into a nifty method

--- tmcprototype/CentralNode/test/unit/central_node_test.py
+++ tmcprototype/CentralNode/test/unit/central_node_test.py
@@ -6,9 +6,9 @@ from unittest.mock import MagicMock, Mock
 
 import pytest
 import tango
+from CONST import ENUM_DEGRADED
 from tango.test_context import DeviceTestContext
 
-from CONST import ENUM_DEGRADED, ENUM_OK
 
 @pytest.mark.parametrize(
     'subdevice_fqdn_prop_name,subdevice_fqdn_prop_value,subdevice_health_state_attribute,subdevice_health_state_value,expected_telescope_health_state',
@@ -30,15 +30,7 @@ def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after
     device_proxy_mock = Mock()
     event_system = _prepare_event_system(device_proxy_mock, subdevice_health_state_attribute)
 
-    tango_device_proxy_import_path = 'tango.DeviceProxy'
-    patcher = mock.patch(tango_device_proxy_import_path)
-    patched_constructor = patcher.start()
-
-    patched_constructor.return_value = device_proxy_mock
-
-    patched_module = importlib.reload(sys.modules[device_under_test.__module__])
-    device_under_test = getattr(patched_module, device_under_test.__name__)
-    tango_context = DeviceTestContext(device_under_test, properties=initial_dut_properties)
+    tango_context = fake_tango_system(device_under_test, device_proxy_mock, initial_dut_properties)
 
     tango_context.start()
 
@@ -49,7 +41,19 @@ def test_telescope_health_state_is_degraded_when_any_subdevice_is_degraded_after
     # assert:
     assert tango_context.device.telescopeHealthState == expected_telescope_health_state
     tango_context.stop()
+
+
+def fake_tango_system(device_under_test, dependent_device_proxy_mock, initial_dut_properties={},
+                      device_proxy_import_path='tango.DeviceProxy'):
+    patcher = mock.patch(device_proxy_import_path)
+    patched_constructor = patcher.start()
+
+    patched_constructor.return_value = dependent_device_proxy_mock
+    patched_module = importlib.reload(sys.modules[device_under_test.__module__])
+    device_under_test = getattr(patched_module, device_under_test.__name__)
     patcher.stop()
+
+    return DeviceTestContext(device_under_test, properties=initial_dut_properties)
 
 
 def _prepare_event_system(mocked_device_proxy, attribute_to_mock):
