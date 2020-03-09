336ee0f34f62fbebfc91965d7c27895115ca4c46
Sett Wai <swai@ska.ac.za>
2020-03-09 16:05:48 +0200

    Implement extract_tango_attributes

--- tmcprototype/CentralNode/test/unit/pytango_parsel_test.py
+++ tmcprototype/CentralNode/test/unit/pytango_parsel_test.py
@@ -1,20 +1,39 @@
 from tango.server import Device, device_property, is_tango_object
+from tango.server import attribute as tango_attribute
 
 from CentralNode.CentralNode import CentralNode
 
 
-class BaseDevice(Device):
+class RootDevice(Device):
     BaseDeviceProperty = device_property(
         dtype='str',
         doc="A base device property that should be inherited.",
     )
 
-
-class ChildDevice(BaseDevice):
-    ChildDeviceProperty = device_property(
-        dtype='str',
-        doc="A child device property.",
+    baseTangoAttribute = tango_attribute(
+        dtype='DevEnum',
+        enum_labels=["OK", "DEGRADED", "FAILED", "UNKNOWN", ],
+        doc="An Enum Type Tango Attribute",
     )
+
+    def read_baseTangoAttribute(self):
+        return self._base_tango_attribute
+
+
+class LeafDevice(RootDevice):
+    LeafDeviceProperty = device_property(
+        dtype='str',
+        doc="A leaf device property"
+    )
+
+    telescopeHealthState = tango_attribute(
+        dtype='DevEnum',
+        enum_labels=["OK", "DEGRADED", "FAILED", "UNKNOWN", ],
+        doc="An Enum Type Tango Attribute",
+    )
+
+    def read_telescopeHealthState(self):
+        return self._telescope_health_state
 
 
 class PyTangoParsel():
@@ -25,14 +44,32 @@ class PyTangoParsel():
         return [o for o in self._device.__dict__.values() if is_tango_object(o)
                 and isinstance(o, device_property)]
 
+    def extract_tango_attributes(self):
+        return [o for o in self._device.__dict__.values() if is_tango_object(o)
+                and isinstance(o, tango_attribute)]
+
 
 def test_parsel_should_parse_device_properties_from_tango_device_and_all_its_parents():
-    parsel = PyTangoParsel(ChildDevice)
+    parsel = PyTangoParsel(LeafDevice)
 
     device_properties = parsel.extract_tango_properties()
 
-    expected_properties = [BaseDevice.BaseDeviceProperty,
-                           ChildDevice.ChildDeviceProperty]
+    expected_properties = [RootDevice.BaseDeviceProperty,
+                           LeafDevice.LeafDeviceProperty,
+                           LeafDevice.LeafDeviceProperty]
 
     assert len(device_properties) == len(expected_properties)
     assert set(device_properties) == set(expected_properties)
+
+
+def test_parsel_should_parse_device_properties_from_tango_device_and_all_its_parents():
+    parsel = PyTangoParsel(LeafDevice)
+
+    device_attributes = parsel.extract_tango_attributes()
+    expected_attributes = [
+        RootDevice.baseTangoAttribute,
+        LeafDevice.telescopeHealthState]
+    
+    assert len(device_attributes) == len(expected_attributes)
+    assert set(device_attributes) == set(expected_attributes)
+
