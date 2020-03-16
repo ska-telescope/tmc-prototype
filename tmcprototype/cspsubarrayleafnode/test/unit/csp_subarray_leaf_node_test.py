7ed61028a999ed5febc43d2dbe17f5e8f326f4bc
Apurva Patkar <apurva.ska@gmail.com>
2020-03-16 15:59:07 +0530

    AT1-464: Fix import path in test file.

--- tmcprototype/cspsubarrayleafnode/test/unit/csp_subarray_leaf_node_test.py
+++ tmcprototype/cspsubarrayleafnode/test/unit/csp_subarray_leaf_node_test.py
@@ -8,7 +8,7 @@ import types
 from mock import Mock
 
 from cspsubarrayleafnode.src.csp_subarray_leaf_node import CspSubarrayLeafNode
-from src.CONST import CMD_STARTSCAN, ENUM_READY
+from cspsubarrayleafnode.src.CONST import CMD_STARTSCAN, ENUM_READY
 from tango.test_context import DeviceTestContext
 
 def test_start_scan_should_command_csp_subarray_master_to_start_its_scan_when_it_is_ready():
