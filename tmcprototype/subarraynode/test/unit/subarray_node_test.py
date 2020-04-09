fdedd652c67b59f84d9d5f72debc59b4be24ae59
Digvijay Mali <digvijay.ska@gmail.com>
2020-04-09 13:05:36 +0530

    story_AT1-499: Fix unit test for Subarray node.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -34,6 +34,8 @@ def test_start_scan_should_command_subarray_to_start_scan_when_it_is_ready():
 
     csp_subarray_proxy_mock.obsState = ObsState.READY
     sdp_subarray_proxy_mock.obsState = ObsState.READY
+    csp_subarray_proxy_mock.State = DevState.ON
+    sdp_subarray_proxy_mock.State = DevState.ON
 
     proxies_to_mock = {
         csp_subarray_ln_fqdn : csp_subarray_ln_proxy_mock,
@@ -44,7 +46,7 @@ def test_start_scan_should_command_subarray_to_start_scan_when_it_is_ready():
 
     with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) \
             as tango_context:
-        tango_context.device.state = DevState.ON
+        # tango_context.device.state = DevState.ON
         scan_config = '{"scanDuration":10}'
         # act:
         tango_context.device.Scan(scan_config)
