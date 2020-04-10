a5bed1b8b60a56716f120178732e5cb5555602ca
Digvijay Mali <digvijay.ska@gmail.com>
2020-04-10 18:13:30 +0530

    story_AT1-499: Fix unit test case for Scan command on Subarray Node.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -49,6 +49,8 @@ def test_start_scan_should_command_subarray_to_start_scan_when_it_is_ready():
 
     csp_subarray_proxy_mock.obsState = ObsState.READY
     sdp_subarray_proxy_mock.obsState = ObsState.READY
+    csp_subarray_proxy_mock.set_state(DevState.ON)
+    sdp_subarray_proxy_mock.set_state(DevState.ON)
 
     proxies_to_mock = {
         csp_subarray_ln_fqdn : csp_subarray_ln_proxy_mock,
@@ -61,8 +63,9 @@ def test_start_scan_should_command_subarray_to_start_scan_when_it_is_ready():
             as tango_context:
         # tango_context.device.state = DevState.ON
         scan_config = '{"scanDuration":10}'
+        print("device state of subarray state:", tango_context.device.state())
         # act:
-        tango_context.device.State = DevState.ON
+        # tango_context.device.State = DevState.ON
         tango_context.device.Scan(scan_config)
         cmdData = tango.DeviceData()
         cmdData.insert(tango.DevString, scan_config)
