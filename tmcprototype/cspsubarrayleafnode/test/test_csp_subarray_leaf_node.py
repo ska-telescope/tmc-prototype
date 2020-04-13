9324887c05c934f222dbd20260a62960b5ac7505
handeSnehal <snehalhande.ska@gmail.com>
2020-04-13 12:50:36 +0530

    story_At1-479: Remove print statements from central and subarraynode

--- tmcprototype/cspsubarrayleafnode/test/test_csp_subarray_leaf_node.py
+++ tmcprototype/cspsubarrayleafnode/test/test_csp_subarray_leaf_node.py
@@ -108,7 +108,7 @@ class TestCspSubarrayLeafNode(object):
         assignresources_input.append('{"dish":{"receptorIDList":["0001","0002"]}}')
         res = tango_context.device.AssignResources(assignresources_input)
         tango_context.device.status()
-        time.sleep(1)
+        time.sleep(5)
         assert create_cspsubarray1_proxy.state() == DevState.ON
         assert const.STR_ADD_RECEPTORS_SUCCESS in tango_context.device.activityMessage \
                and res is None
