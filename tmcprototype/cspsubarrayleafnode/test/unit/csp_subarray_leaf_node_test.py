12c8b601cc6149a371baa3cf8eb8b1caf63bf9d9
digvijaygautam <digvijay.gautam.ska@gmail.com>
2020-04-15 17:31:44 +0530

    added unit test cases for csp array leaf node

--- tmcprototype/cspsubarrayleafnode/test/unit/csp_subarray_leaf_node_test.py
+++ tmcprototype/cspsubarrayleafnode/test/unit/csp_subarray_leaf_node_test.py
@@ -1,6 +1,7 @@
 import contextlib
 import importlib
 import sys
+import os
 import json
 import mock
 import types
@@ -9,6 +10,12 @@ from mock import Mock
 from cspsubarrayleafnode import CspSubarrayLeafNode, const
 from tango.test_context import DeviceTestContext
 from ska.base.control_model import ObsState
+file_path = os.path.dirname(os.path.abspath(__file__))
+
+SRC_ROOT_DIR = "/app"
+TMC_ROOT_DIR = SRC_ROOT_DIR + "/tmcprototype"
+ska_antennas_path = TMC_ROOT_DIR + "/ska_antennas.txt"
+
 
 def test_start_scan_should_command_csp_subarray_master_to_start_its_scan_when_it_is_ready():
     # arrange:
@@ -26,12 +33,47 @@ def test_start_scan_should_command_csp_subarray_master_to_start_its_scan_when_it
     }
 
     with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) as tango_context:
-        scan_config = '0'                #'{"scanDuration": 10.0}'
+        scan_config = { 'scanDuration': 10.0 }
         # act:
-        tango_context.device.StartScan(scan_config)
+        tango_context.device.StartScan([json.dumps(scan_config)])
 
         # assert:
-        csp_subarray_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_STARTSCAN, float(scan_config), any_method(with_name='commandCallback'))
+        csp_subarray_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_STARTSCAN, '0', any_method(with_name='commandCallback'))
+
+
+def test_assign_resources_should_send_csp_subarray_with_correct_receptor_id_list():
+    # arrange:
+    device_under_test = CspSubarrayLeafNode
+    csp_subarray_fqdn = 'mid_csp/elt/subarray_01'
+    dut_properties = {
+        'CspSubarrayFQDN': csp_subarray_fqdn
+    }
+
+    csp_subarray_proxy_mock = Mock()
+    csp_subarray_proxy_mock.obsState = ObsState.IDLE
+
+    proxies_to_mock = {
+        csp_subarray_fqdn: csp_subarray_proxy_mock
+    }
+
+    with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) as tango_context:
+        assign_config = []
+        assign_config.append('{"dish":{"receptorIDList":["0001","0002"]}}')
+        device_proxy=tango_context.device
+
+        #act
+        device_proxy.AssignResources(assign_config)
+
+        #assert
+        receptorIDList = []
+        jsonArgument = json.loads(assign_config)
+        receptorIDList_str = jsonArgument[const.STR_DISH][const.STR_RECEPTORID_LIST]
+        # convert receptorIDList from list of string to list of int
+        for i in range(0, len(receptorIDList_str)):
+            receptorIDList.append(int(receptorIDList_str[i]))
+        csp_subarray_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_ADD_RECEPTORS, receptorIDList,
+                                                                        any_method(with_name='commandCallback'))
+        assert_activity_message(device_proxy, const.STR_ADD_RECEPTORS_SUCCESS)
 
 
 def any_method(with_name=None):
@@ -59,3 +101,12 @@ def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_m
     device_test_context.start()
     yield device_test_context
     device_test_context.stop()
+
+def assert_activity_message(device_proxy, expected_message):
+    assert device_proxy.activityMessage == expected_message  # reads tango attribute
+
+
+
+
+
+
