d9b42e4d7e7b3462d283dfb0076a118f2e0ee71f
Akash Karangale <akashk.ska@gmail.com>
2020-04-17 15:48:48 +0530

    mocking_subarray_node: assign and release resources test cases.

--- tmcprototype/centralnode/test/unit/central_node_test.py
+++ tmcprototype/centralnode/test/unit/central_node_test.py
@@ -2,11 +2,15 @@ import contextlib
 import importlib
 import sys
 import mock
+import types
+
 from mock import Mock
-from centralnode import CentralNode
+from centralnode import CentralNode,const
 from centralnode.const import CMD_SET_STOW_MODE, STR_STARTUP_CMD_ISSUED, STR_STOW_CMD_ISSUED_CN, STR_STANDBY_CMD_ISSUED
 from ska.base.control_model import HealthState
 from tango.test_context import DeviceTestContext
+from ska.base.control_model import ObsState
+
 
 def test_telescope_health_state_is_degraded_when_csp_master_leaf_node_is_degraded_after_start():
     # arrange:
@@ -96,3 +100,63 @@ def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_m
     device_test_context.start()
     yield device_test_context
     device_test_context.stop()
+
+def any_method(with_name=None):
+    class AnyMethod():
+        def __eq__(self, other):
+            if not isinstance(other, types.MethodType):
+                return False
+
+            return other.__func__.__name__ == with_name if with_name else True
+
+    return AnyMethod()
+
+
+def test_assign_resources_should_send_json_to_subarraynode():
+    # arrange:
+    device_under_test = CentralNode
+    subarray_fqdn = 'ska_mid/tm_subarray_node/1'
+    dut_properties = {
+        'SubarrayFQDN': subarray_fqdn
+    }
+
+    subarray_proxy_mock = Mock()
+    subarray_proxy_mock.obsState = ObsState.IDLE
+    proxies_to_mock = {
+        subarray_fqdn: subarray_proxy_mock
+    }
+
+    with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) \
+            as tango_context:
+        assign_command = '["0001", "0002"]'
+        tango_context.device.AssignResources(assign_command)
+
+        # assert:
+        subarray_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_ASSIGN_RESOURCES, assign_command)
+        assert_activity_message(tango_context.device, const.STR_ASSIGN_RESOURCES_SUCCESS)
+
+
+
+def test_release_resources_when_subarray_is_idle():
+    # arrange:
+    device_under_test = CentralNode
+    subarray_fqdn = 'ska_mid/tm_subarray_node/1'
+    dut_properties = {
+        'SubarrayFQDN': subarray_fqdn
+    }
+
+    subarray_proxy_mock = Mock()
+    subarray_proxy_mock.obsState = ObsState.IDLE
+    proxies_to_mock = {
+        subarray_fqdn: subarray_proxy_mock
+    }
+
+    with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) \
+            as tango_context:
+        # act:
+        tango_context.device.ReleaseAllResources()
+
+        # assert:
+        subarray_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_RELEASE_RESOURCES)
+        assert_activity_message(tango_context.device, const.STR_REL_RESOURCES)
+
