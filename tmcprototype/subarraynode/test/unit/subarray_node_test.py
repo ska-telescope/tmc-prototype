028e40ab16eae91146875f58a9079f607cdd7476
Digvijay Mali <digvijay.ska@gmail.com>
2020-04-20 16:58:02 +0530

    AT1-499: Fix AssignResources command test case on Subarray Node.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -5,7 +5,7 @@ import json
 import mock
 import types
 import tango
-from tango import DevState, DeviceProxy
+from tango import DevState
 
 from mock import Mock
 from subarraynode import SubarrayNode, const
@@ -65,7 +65,7 @@ def test_assignResource_should_command_subarray_AssignResource():
     csp_subarray_proxy_mock = Mock()
     sdp_subarray_ln_proxy_mock = Mock()
     sdp_subarray_proxy_mock = Mock()
-    dish_ln_proxy_mock = Mock()
+    # dish_ln_proxy_mock = Mock()
     # csp_subarray_proxy_mock.obsState = ObsState.READY
     # sdp_subarray_proxy_mock.obsState = ObsState.READY
     # csp_subarray_proxy_mock.set_state(DevState.OFF)
@@ -76,7 +76,7 @@ def test_assignResource_should_command_subarray_AssignResource():
         csp_subarray_fqdn : csp_subarray_proxy_mock,
         sdp_subarray_ln_fqdn : sdp_subarray_ln_proxy_mock,
         sdp_subarray_fqdn : sdp_subarray_proxy_mock,
-        dish_ln_fqdn : dish_ln_proxy_mock
+        # dish_ln_fqdn : dish_ln_proxy_mock
     }
 
 
