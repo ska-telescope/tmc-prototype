082c401d82e6a0a7094117c5459fd6b647eec6fa
Digvijay Mali <digvijay.ska@gmail.com>
2020-04-21 17:39:23 +0530

    AT1-499: Add ReleaseResources test case on Subarray Node

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -106,6 +106,54 @@ def test_assignResource_should_command_subarray_AssignResource():
         # sdp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_ASSIGN_RESOURCES, cmdData)
         csp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_ASSIGN_RESOURCES, arg_list)
 
+
+def test_ReleaseResource_command_subarray():
+    # arrange:
+    device_under_test = SubarrayNode
+    csp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
+    csp_subarray_fqdn = 'mid_csp/elt/subarray_01'
+    sdp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
+    sdp_subarray_fqdn = 'mid_sdp/elt/subarray_1'
+    dish_ln_prefix = 'ska_mid/tm_leaf_node/d'
+    # dish_ln_fqdn = dish_ln_prefix+'0001'
+
+    dut_properties = {
+        'CspSubarrayLNFQDN': csp_subarray_ln_fqdn,
+        'CspSubarrayFQDN': csp_subarray_fqdn,
+        'SdpSubarrayLNFQDN': sdp_subarray_ln_fqdn,
+        'SdpSubarrayFQDN': sdp_subarray_fqdn,
+        'DishLeafNodePrefix' : dish_ln_prefix
+    }
+
+    csp_subarray_ln_proxy_mock = Mock()
+    csp_subarray_proxy_mock = Mock()
+    sdp_subarray_ln_proxy_mock = Mock()
+    sdp_subarray_proxy_mock = Mock()
+    dish_ln_proxy_mock = Mock()
+    # csp_subarray_proxy_mock.obsState = ObsState.READY
+    # sdp_subarray_proxy_mock.obsState = ObsState.READY
+    # csp_subarray_proxy_mock.set_state(DevState.OFF)
+    # sdp_subarray_proxy_mock.set_state(DevState.OFF)
+
+    proxies_to_mock = {
+        csp_subarray_ln_fqdn : csp_subarray_ln_proxy_mock,
+        csp_subarray_fqdn : csp_subarray_proxy_mock,
+        sdp_subarray_ln_fqdn : sdp_subarray_ln_proxy_mock,
+        sdp_subarray_fqdn : sdp_subarray_proxy_mock,
+        dish_ln_prefix+"0001": dish_ln_proxy_mock
+    }
+
+
+    with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) \
+            as tango_context:
+        tango_context.device.On()
+        receptor_list = ['0001']
+        tango_context.device.AssignResources(receptor_list)
+        tango_context.device.ReleaseAllResources()
+        # assert:
+        # sdp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_ASSIGN_RESOURCES, cmdData)
+        csp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_RELEASE_ALL_RESOURCES)
+
 def create_dummy_event_csp_sa(csp_subarray_fqdn):
     fake_event = Mock()
     fake_event.err = False
