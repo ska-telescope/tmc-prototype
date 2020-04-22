25b9dfcd0d63d46f2abb1995f91d0357e88320fa
handeSnehal <snehalhande.ska@gmail.com>
2020-04-22 18:24:13 +0530

    mocking_csp_subarray_leaf_node: Add print path for testing

--- tmcprototype/cspsubarrayleafnode/src/cspsubarrayleafnode/csp_subarray_leaf_node.py
+++ tmcprototype/cspsubarrayleafnode/src/cspsubarrayleafnode/csp_subarray_leaf_node.py
@@ -19,10 +19,13 @@ import katpoint
 import numpy as np
 
 file_path = os.path.dirname(os.path.abspath(__file__))
+SRC_ROOT_DIR = "/app"
+TMC_ROOT_DIR = SRC_ROOT_DIR + "/tmcprototype"
+ska_antennas_path = TMC_ROOT_DIR + "/ska_antennas.txt"
 
 # print ("file_path", file_path)
-print (os. system('find / -name `ska_antennas.txt`'))
-ska_antennas_path = "ska_antennas.txt"
+print(os. system('find / -name "ska_antennas.txt"'))
+
 # PyTango imports
 import tango
 from tango import DebugIt, AttrWriteType, DeviceProxy, DevState, DevFailed
