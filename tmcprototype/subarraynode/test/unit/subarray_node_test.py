6a6833f3c90bad4678597912a28ef1d90cc0e3bb
kalyanithigale <kalyanit.ska@gmail.com>
2020-04-22 15:08:15 +0530

    story_AT1-499: Updated test case for Configure Command to update device data.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -194,7 +194,6 @@ def test_Configure_command_subarray():
         tango_context.device.On()
         receptor_list = ['0001']
         tango_context.device.AssignResources(receptor_list)
-        tango_context.device.ObsState = ObsState.IDLE
 
         tango_context.device.Configure('{"scanID":12345,"pointing":{"target":{"system":"ICRS","name":'
                                        '"Polaris","RA":"02:31:49.0946","dec":"+89:15:50.7923"}},"dish":'
@@ -239,12 +238,59 @@ def test_Configure_command_subarray():
         cmd_data.insert(tango.DevString, json.dumps(dish_configure_input))
         dish_ln_proxy_mock.command_inout.asser_called_with(const.CMD_CONFIGURE, cmd_data)
 
+def test_obs_state_is_ready_when_other_leaf_node_is_ready_after_start():
+    # arrange:
+    device_under_test = SubarrayNode
+    csp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
+    csp_subarray_fqdn = 'mid_csp/elt/subarray_01'
+    sdp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
+    sdp_subarray_fqdn = 'mid_sdp/elt/subarray_1'
+    dish_ln_prefix = 'ska_mid/tm_leaf_node/d'
+
+    dut_properties = {
+        'CspSubarrayLNFQDN': csp_subarray_ln_fqdn,
+        'CspSubarrayFQDN': csp_subarray_fqdn,
+        'SdpSubarrayLNFQDN': sdp_subarray_ln_fqdn,
+        'SdpSubarrayFQDN': sdp_subarray_fqdn,
+        'DishLeafNodePrefix': dish_ln_prefix
+    }
+
+    csp_subarray_ln_proxy_mock = Mock()
+    csp_subarray_proxy_mock = Mock()
+    sdp_subarray_ln_proxy_mock = Mock()
+    sdp_subarray_proxy_mock = Mock()
+    dish_ln_proxy_mock = Mock()
+
+    proxies_to_mock = {
+        csp_subarray_ln_fqdn: csp_subarray_ln_proxy_mock,
+        csp_subarray_fqdn: csp_subarray_proxy_mock,
+        sdp_subarray_ln_fqdn: sdp_subarray_ln_proxy_mock,
+        sdp_subarray_fqdn: sdp_subarray_proxy_mock,
+        dish_ln_prefix + "0001": dish_ln_proxy_mock
+    }
+    csp_subarray_obsstate_attribute = "cspSubarrayObsState"
+    sdp_subarray_obsstate_attribute = "sdpSubarrayObsState"
+
+    event_subscription_map = {}
+
+    csp_subarray_ln_proxy_mock.subscribe_event.side_effect = (
+        lambda attr_name, event_type, callback, *args, **kwargs: event_subscription_map.update({attr_name: callback}))
+
+
+    with fake_tango_system(device_under_test, initial_dut_properties, proxies_to_mock) as tango_context:
+        # act:
+        dummy_event_csp = create_dummy_event_obsstate(csp_subarray_ln_fqdn)
+        event_subscription_map[csp_subarray_obsstate_attribute](dummy_event_csp)
+
+        # assert:
+        assert tango_context.device.obsState == ObsState.CONFIGURING
+
 
 def create_dummy_event_obsstate(device_fqdn):
     fake_event = Mock()
     fake_event.err = False
     fake_event.attr_name = f"{device_fqdn}/ObsState"
-    fake_event.attr_value.value = ObsState.READY
+    fake_event.attr_value.value = ObsState.CONFIGURING
     return fake_event
 #
 # def test_start_scan_should_command_subarray_to_start_scan_when_it_is_ready():
