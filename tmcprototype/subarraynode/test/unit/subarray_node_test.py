652fd73ee6f9b63b747549e69cdda122e6f4ede1
kalyanithigale <kalyanit.ska@gmail.com>
2020-04-27 19:39:56 +0530

    Changed DeviceData for Configure command.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -217,15 +217,34 @@ def test_Configure_command_subarray():
         # scan_configuration = json.loads(argin)
         # self._scan_id = str(scan_configuration["scanID"])
 
-        csp_scan_config = '{"frequencyBand":"1","fsp":[{"fspID":1,"functionMode":"CORR",' \
-                          '"frequencySliceID":1,"integrationTime":1400,"corrBandwidth":0}],' \
-                          '"delayModelSubscriptionPoint":"ska_mid/tm_leaf_node/csp_subarray01/delayModel",' \
-                          '"visDestinationAddressSubscriptionPoint":"mid_sdp/elt/subarray_1/receiveAddresses",' \
-                          '"pointing":{"target":{"system":"ICRS","name":"Polaris","RA":"20:21:10.31",' \
-                          '"dec":"-30:52:17.3"}},"scanID":"12345"}'
+        expected_string_dict = {
+            "frequencyBand": "1",
+            "fsp": [
+                {
+                    "fspID": 1,
+                    "functionMode": "CORR",
+                    "frequencySliceID": 1,
+                    "integrationTime": 1400,
+                    "corrBandwidth": 0
+                }
+            ],
+            "string1": "attr1",
+            "string2": "attr2",
+            "pointing": {
+                "target": {
+                    "system": "ICRS",
+                    "name": "Polaris",
+                    "RA": "02:31:49.0946",
+                    "dec": "+89:15:50.7923"
+                }
+            },
+            "scanID": 1
+        }
         # cmd_data = tango.DeviceData()
         # cmd_data.insert(tango.DevString, json.dumps(csp_scan_config))
-        csp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_CONFIGURE, json.dumps(csp_scan_config))
+        csp_scan_config = json.dumps(expected_string_dict)
+        print ("csp_scan_config:", csp_scan_config)
+        csp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_CONFIGURE, csp_scan_config)
 
         scan_config = '{"sdp":{"configure":{"id":"realtime-20190627-0001","sbiId":"20190627-0001",' \
                      '"workflow":{"id":"vis_ingest","type":"realtime","version":"0.1.0"},"parameters":' \
