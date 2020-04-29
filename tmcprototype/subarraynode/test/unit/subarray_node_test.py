81cee1423f6177685700f4c48bc873f6778f6f07
kalyanithigale <kalyanit.ska@gmail.com>
2020-04-29 14:07:53 +0530

    Added more negative test cases for assignResource command.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -15,7 +15,7 @@ from subarraynode.const import PointingState
 from tango.test_context import DeviceTestContext
 from ska.base.control_model import AdminMode, HealthState, ObsState, ObsMode, TestMode, SimulationMode, LoggingLevel
 
-def test_On_should_command_subarray_to_On():
+def test_on_command_should_change_subarray_device_state_from_disable_to_off():
     # arrange:
     device_under_test = SubarrayNode
     dut_properties = {
@@ -30,7 +30,7 @@ def test_On_should_command_subarray_to_On():
         # assert:
         assert tango_context.device.state() == DevState.OFF
 
-def test_Standby_command_subarray_to_OFF():
+def test_standby_command_should_change_subarray_device_state_to_disable():
     # arrange:
     device_under_test = SubarrayNode
     dut_properties = {
@@ -46,7 +46,7 @@ def test_Standby_command_subarray_to_OFF():
         assert tango_context.device.state() == DevState.DISABLE
 
 
-def test_assignResource_should_command_subarray_AssignResource():
+def test_assign_resource_should_command_dish_csp_sdp_subarray_to_assign_valid_resources():
     # arrange:
     device_under_test = SubarrayNode
     csp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
@@ -99,39 +99,11 @@ def test_assignResource_should_command_subarray_AssignResource():
         arg_list.append(json.dumps(json_argument))
         csp_subarray_ln_proxy_mock.command_inout.assert_called_with(const.CMD_ASSIGN_RESOURCES, arg_list)
 
-def test_assignResource_should_raise_exception_when_called_before_on():
+def test_assignResource_should_raise_exception_when_called_when_device_state_disable():
     # arrange:
     device_under_test = SubarrayNode
-    csp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
-    csp_subarray_fqdn = 'mid_csp/elt/subarray_01'
-    sdp_subarray_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
-    sdp_subarray_fqdn = 'mid_sdp/elt/subarray_1'
-    dish_ln_prefix = 'ska_mid/tm_leaf_node/d'
-
-    dut_properties = {
-        'CspSubarrayLNFQDN': csp_subarray_ln_fqdn,
-        'CspSubarrayFQDN': csp_subarray_fqdn,
-        'SdpSubarrayLNFQDN': sdp_subarray_ln_fqdn,
-        'SdpSubarrayFQDN': sdp_subarray_fqdn,
-        'DishLeafNodePrefix' : dish_ln_prefix
-    }
-
-    csp_subarray_ln_proxy_mock = Mock()
-    csp_subarray_proxy_mock = Mock()
-    sdp_subarray_ln_proxy_mock = Mock()
-    sdp_subarray_proxy_mock = Mock()
-    dish_ln_proxy_mock = Mock()
-
-    proxies_to_mock = {
-        csp_subarray_ln_fqdn : csp_subarray_ln_proxy_mock,
-        csp_subarray_fqdn : csp_subarray_proxy_mock,
-        sdp_subarray_ln_fqdn : sdp_subarray_ln_proxy_mock,
-        sdp_subarray_fqdn : sdp_subarray_proxy_mock,
-        dish_ln_prefix+"0001": dish_ln_proxy_mock
-    }
-
-
-    with fake_tango_system(device_under_test, initial_dut_properties=dut_properties, proxies_to_mock=proxies_to_mock) \
+    # act
+    with fake_tango_system(device_under_test) \
             as tango_context:
         receptor_list = ['0001']
         with pytest.raises(tango.DevFailed):
@@ -139,6 +111,20 @@ def test_assignResource_should_raise_exception_when_called_before_on():
 
         # assert:
         assert tango_context.device.State() == DevState.DISABLE
+
+def test_assignResource_should_raise_exception_when_called_with_invalid_input():
+    # arrange:
+    device_under_test = SubarrayNode
+    # act
+    with fake_tango_system(device_under_test) \
+            as tango_context:
+        tango_context.device.On()
+        receptor_list = ['abc']
+        with pytest.raises(tango.DevFailed):
+            tango_context.device.AssignResources(receptor_list)
+
+        # assert:
+        assert tango_context.device.State() == DevState.OFF
 
 def test_ReleaseResource_command_subarray():
     # arrange:
