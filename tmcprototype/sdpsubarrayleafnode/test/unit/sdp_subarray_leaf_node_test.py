b6a28622ab9ebd50a262649e178f1b8faf3f5559
handeSnehal <snehalhande.ska@gmail.com>
2020-05-05 14:17:17 +0530

    change name scan_input in sdpsubarrayleafnode

--- tmcprototype/sdpsubarrayleafnode/test/unit/sdp_subarray_leaf_node_test.py
+++ tmcprototype/sdpsubarrayleafnode/test/unit/sdp_subarray_leaf_node_test.py
@@ -43,7 +43,7 @@ def test_start_scan_should_command_sdp_subarray_to_start_scan_when_it_is_ready()
         tango_context.device.Scan(scan_input)
 
         # assert:
-        sdp_subarray1_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_SCAN, scan_config,
+        sdp_subarray1_proxy_mock.command_inout_asynch.assert_called_with(const.CMD_SCAN, scan_input,
                                                                  any_method(with_name='commandCallback'))
 
 
