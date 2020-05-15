576c76920bdec88d244f0a359c393ad3312ba13a
Aditya Dange <adityadange.ska@gmail.com>
2020-05-15 07:29:39 +0000

    Changes to move the reports for persistance.

--- tmcprototype/run_unit_test.sh
+++ tmcprototype/run_unit_test.sh
@@ -7,5 +7,14 @@ export TMC_ELEMENT=$(basename $(dirname $(dirname $path)));
 echo $TMC_ELEMENT;
 echo +++ Trying tests for $TMC_ELEMENT;
 pytest -v ./${TMC_ELEMENT}/test/unit --forked --cov=./${TMC_ELEMENT} --cov-report=html:./test_results/${TMC_ELEMENT}_htmlcov --json-report --json-report-file=./test_results/${TMC_ELEMENT}_report.json --junitxml=./test_results/${TMC_ELEMENT}-unit-tests.xml;
-done 
+done
 
+mv /app/tmcprototype/dishmaster/test_results/* /build/unit_test_result/dishmaster \
+mv /app/tmcprototype/cspsubarrayleafnode/test_results/* /build/unit_test_result/cspsubarrayleafnode \
+mv /app/tmcprototype/dishleafnode/test_results/* /build/unit_test_result/dishleafnode \
+mv /app/tmcprototype/sdpsubarrayleafnode/test_results/* /build/unit_test_result/sdpsubarrayleafnode \
+mv /app/tmcprototype/cspmasterleafnode/test_results/* /build/unit_test_result/cspmasterleafnode \
+mv /app/tmcprototype/cspsubarrayleafnode/test_results/* /build/unit_test_result/cspsubarrayleafnode \
+mv /app/tmcprototype/sdpmasterleafnode/test_results/* /build/unit_test_result/sdpmasterleafnode \
+mv /app/tmcprototype/subarraynode/test_results/* /build/unit_test_result/subarraynode \
+mv /app/tmcprototype/centralnode/test_results/* /build/unit_test_result/centralnode
