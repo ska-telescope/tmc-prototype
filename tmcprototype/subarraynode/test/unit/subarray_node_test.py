a3428d8cc9444b4091efa7b42c1c3ee56daceb1f
kalyanithigale <kalyanit.ska@gmail.com>
2020-05-18 20:21:26 +0530

    Resolve review comments.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -971,7 +971,7 @@ def test_start_scan_should_should_raise_devfailed_exception():
         assert const.ERR_SCAN_CMD in tango_context.device.activityMessage
 
 
-def test_start_scan_should_should_raise_assertion_exception():
+def test_start_scan_should_raise_assertion_exception():
     csp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
     csp_subarray1_fqdn = 'mid_csp/elt/subarray_01'
     sdp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
@@ -1019,20 +1019,20 @@ def test_start_scan_should_should_raise_assertion_exception():
         sdp_subarray1_proxy_mock.obsState = ObsState.READY
         attribute = 'ObsState'
         dummy_event_csp = create_dummy_event_state(csp_subarray1_ln_proxy_mock, csp_subarray1_ln_fqdn,
-                                                   attribute, ObsState.READY)
+                                                   attribute, ObsState.SCANNING)
         event_subscription_map[csp_subarray1_obsstate_attribute](dummy_event_csp)
 
         dummy_event_sdp = create_dummy_event_state(sdp_subarray1_ln_proxy_mock, sdp_subarray1_ln_fqdn,
-                                                   attribute, ObsState.READY)
+                                                   attribute, ObsState.SCANNING)
         event_subscription_map[sdp_subarray1_obsstate_attribute](dummy_event_sdp)
-        while tango_context.device.obsState != ObsState.READY:
+        while tango_context.device.obsState != ObsState.SCANNING:
             pass
         scan_input = '{"id": 1}'
         with pytest.raises(tango.DevFailed) as assert_error:
             tango_context.device.Scan(scan_input)
 
         # assert:
-        assert tango_context.device.obsState == ObsState.READY
+        assert tango_context.device.obsState == ObsState.SCANNING
 
 
 def test_end_scan_should_command_subarray_to_end_scan_when_it_is_scanning():
