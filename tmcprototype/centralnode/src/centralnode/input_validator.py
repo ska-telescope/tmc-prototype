b8e9b642138d81a23501b34ada7eeb3c038453d1
Aditya Dange <adityadange.ska@gmail.com>
2020-06-09 17:30:48 +0530

    sp-709: Added module to validate the JSON input

---
+++ tmcprototype/centralnode/src/centralnode/input_validator.py
@@ -0,0 +1,25 @@
+import json
+from json import JSONDecodeError
+from exceptions import InvalidJSONError
+class AssignResourceValidator():
+    """Class to validate the input string of AssignResources command of Central Node"""
+
+    def validate(self, input_string):
+        """
+        Validates the input string received as an argument of AssignResources command.
+
+        :param input_string: A JSON string
+
+        :return: True if input string is valid.
+
+        :throws:
+            InvalidJSONError: When the JSON string is not formatted properly.
+
+            InvalidParameterValue: When a value of a JSON key is not valid. E.g. Receptor id
+            not present.
+        """
+        try:
+            input_json = json.loads(input_string)
+            print(input_json)
+        except JSONDecodeError as json_error:
+            raise InvalidJSONError(json_error.msg)
---
+++ tmcprototype/centralnode/test/unit/input_validator_test.py
@@ -0,0 +1,28 @@
+from input_validator import AssignResourceValidator
+
+class TestAssignResourceValidator():
+    """Class to test the AssignResourceValidator class methods"""
+
+    def test_validate_good_json():
+        """This function tests the validate method when good formatted json is provided"""
+
+        good_json_string = \
+            '{"subarrayID":1,"dish":{"receptorIDList":["0001"]},"sdp":{"id":"sbi-mvp01-' \
+            '20200325-00001","max_length":100.0,"scan_types":[{"id":"science_A","coordinate' \
+            '_system":"ICRS","ra":"21:08:47.92","dec":"-88:57:22.9","subbands":[{"freq_min"' \
+            ':0.35e9,"freq_max":1.05e9,"nchan":372,"input_link_map":[[1,0],[101,1]]}]},{"id"' \
+            ':"calibration_B","coordinate_system":"ICRS","ra":"21:08:47.92","dec":"-88:57:22.9",' \
+            '"subbands":[{"freq_min":0.35e9,"freq_max":1.05e9,"nchan":372,"input_link_' \
+            'map":[[1,0],[101,1]]}]}],"processing_blocks":[{"id":"pb-mvp01-20200325-00001",' \
+            '"workflow":{"type":"realtime","id":"vis_receive","version":"0.1.0"},"parameters"' \
+            ':{}},{"id":"pb-mvp01-20200325-00002","workflow":{"type":"realtime","id":"test_' \
+            'realtime","version":"0.1.0"},"parameters":{}},{"id":"pb-mvp01-20200325-00003",' \
+            '"workflow":{"type":"batch","id":"ical","version":"0.1.0"},"parameters":{},' \
+            '"dependencies":[{"pb_id":"pb-mvp01-20200325-00001","type":["visibilities"]}]}' \
+            ',{"id":"pb-mvp01-20200325-00004","workflow":{"type":"batch","id":"dpreb","' \
+            'version":"0.1.0"},"parameters":{},"dependencies":[{"pb_id":"pb-mvp01-20200325-' \
+            '00003","type":["calibration"]}]}]}}'
+
+        input_validator = AssignResourceValidator()
+        result = input_validator.validate(good_json_string)
+        assert result == True
