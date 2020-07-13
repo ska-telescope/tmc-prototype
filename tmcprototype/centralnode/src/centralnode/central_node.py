fee1d41bd0227f05b7d744d006524302844a4f52
user <jayant.ska@gmail.com>
2020-07-13 12:02:58 +0530

    Resolve pylint warnings on SubarrayNode.

--- tmcprototype/centralnode/src/centralnode/central_node.py
+++ tmcprototype/centralnode/src/centralnode/central_node.py
@@ -13,7 +13,6 @@ of state and mode attributes defined by the SKA Control Model.
 """
 from __future__ import print_function
 from __future__ import absolute_import
-import json
 
 # Tango imports
 import tango
