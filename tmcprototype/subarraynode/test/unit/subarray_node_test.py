f2deccae73604855668bc783f068608ff7c32bbf
Digvijay Mali <digvijay.ska@gmail.com>
2020-07-15 10:21:09 +0530

    ADR-8-abort: Add negative test case for Restart command on Subarray node.

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -1685,7 +1685,7 @@ def test_restart_should_command_subarray_to_restart_when_it_is_Fault():
         assert tango_context.device.obsState == ObsState.EMPTY
 
 
-def test_restart_should_command_subarray_to_restart_when_it_is_devfailed():
+def test_restart_should_command_subarray_to_restart_when_it_is_invalid_state():
     csp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
     csp_subarray1_fqdn = 'mid_csp/elt/subarray_01'
     sdp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
@@ -1719,6 +1719,67 @@ def test_restart_should_command_subarray_to_restart_when_it_is_devfailed():
         with pytest.raises(tango.DevFailed) as df:
             tango_context.device.Restart()
         assert "Error executing command RestartCommand" in str(df)
+
+
+def test_restart_should_raise_devfailed_exception():
+    csp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/csp_subarray01'
+    csp_subarray1_fqdn = 'mid_csp/elt/subarray_01'
+    sdp_subarray1_ln_fqdn = 'ska_mid/tm_leaf_node/sdp_subarray01'
+    sdp_subarray1_fqdn = 'mid_sdp/elt/subarray_1'
+    dish_ln_prefix = 'ska_mid/tm_leaf_node/d'
+
+    dut_properties = {
+        'CspSubarrayLNFQDN': csp_subarray1_ln_fqdn,
+        'CspSubarrayFQDN': csp_subarray1_fqdn,
+        'SdpSubarrayLNFQDN': sdp_subarray1_ln_fqdn,
+        'SdpSubarrayFQDN': sdp_subarray1_fqdn,
+        'DishLeafNodePrefix': dish_ln_prefix
+    }
+    csp_subarray1_ln_proxy_mock = Mock()
+    csp_subarray1_proxy_mock = Mock()
+    sdp_subarray1_ln_proxy_mock = Mock()
+    sdp_subarray1_proxy_mock = Mock()
+    dish_ln_proxy_mock = Mock()
+
+    proxies_to_mock = {
+        csp_subarray1_ln_fqdn: csp_subarray1_ln_proxy_mock,
+        csp_subarray1_fqdn: csp_subarray1_proxy_mock,
+        sdp_subarray1_ln_fqdn: sdp_subarray1_ln_proxy_mock,
+        sdp_subarray1_fqdn: sdp_subarray1_proxy_mock,
+        dish_ln_prefix + '0001': dish_ln_proxy_mock
+    }
+
+    csp_subarray1_obsstate_attribute = "cspSubarrayObsState"
+    sdp_subarray1_obsstate_attribute = "sdpSubarrayObsState"
+
+    event_subscription_map = {}
+
+    csp_subarray1_ln_proxy_mock.subscribe_event.side_effect = (
+        lambda attr_name, event_type, callback, *args, **kwargs: event_subscription_map.
+            update({attr_name: callback}))
+
+    sdp_subarray1_ln_proxy_mock.subscribe_event.side_effect = (
+        lambda attr_name, event_type, callback, *args, **kwargs: event_subscription_map.
+            update({attr_name: callback}))
+
+    csp_subarray1_ln_proxy_mock.command_inout.side_effect = raise_devfailed_exception
+    with fake_tango_system(SubarrayNode, initial_dut_properties=dut_properties,
+                           proxies_to_mock=proxies_to_mock) as tango_context:
+        attribute = 'ObsState'
+        dummy_event_csp = create_dummy_event_state(csp_subarray1_ln_proxy_mock, csp_subarray1_ln_fqdn,
+                                                   attribute, ObsState.ABORTED)
+        event_subscription_map[csp_subarray1_obsstate_attribute](dummy_event_csp)
+
+        dummy_event_sdp = create_dummy_event_state(sdp_subarray1_ln_proxy_mock, sdp_subarray1_ln_fqdn,
+                                                   attribute, ObsState.ABORTED)
+        event_subscription_map[sdp_subarray1_obsstate_attribute](dummy_event_sdp)
+        # while tango_context.device.obsState != ObsState.READY:
+        #     pass
+        with pytest.raises(tango.DevFailed):
+            tango_context.device.Restart()
+        # assert:
+        assert tango_context.device.obsState == ObsState.ABORTED
+        assert const.ERR_RESTART_INVOKING_CMD in tango_context.device.activityMessage
 
 
 @pytest.mark.skip("Remove the test case once command is removed from code")
