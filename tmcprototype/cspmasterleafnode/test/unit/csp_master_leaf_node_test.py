d3a189af07de7e9b4df90a68560225b5d530f96e
digvijaygautam <digvijay.gautam.ska@gmail.com>
2020-07-16 12:57:10 +0530

    fixed csp master leaf node test cases of standby

--- tmcprototype/cspmasterleafnode/test/unit/csp_master_leaf_node_test.py
+++ tmcprototype/cspmasterleafnode/test/unit/csp_master_leaf_node_test.py
@@ -101,8 +101,8 @@ def test_standby_should_command_to_standby_with_callback_method():
                            proxies_to_mock=proxies_to_mock) as tango_context:
 
         # act:
-
-        tango_context.device.Standby()
+        standby_input = []
+        tango_context.device.Standby(standby_input)
         dummy_event = command_callback(const.CMD_STANDBY)
         event_subscription_map[const.CMD_STANDBY](dummy_event)
         # assert:
@@ -125,7 +125,8 @@ def test_standby_should_command_with_callback_method_with_event_error():
                            proxies_to_mock=proxies_to_mock) as tango_context:
 
         # act:
-        tango_context.device.Standby()
+        standby_input = []
+        tango_context.device.Standby(standby_input)
         dummy_event = command_callback_with_event_error(const.CMD_STANDBY)
         event_subscription_map[const.CMD_STANDBY](dummy_event)
         # assert:
@@ -146,10 +147,10 @@ def test_standby_should_command_with_callback_method_with_command_error():
                **kwargs: event_subscription_map.update({command_name: callback}))
     with fake_tango_system(CspMasterLeafNode, initial_dut_properties=dut_properties,
                            proxies_to_mock=proxies_to_mock) as tango_context:
-
+        standby_input = []
         # act:
         with pytest.raises(Exception) as excp:
-            tango_context.device.Standby()
+            tango_context.device.Standby(standby_input)
             dummy_event = command_callback_with_command_exception()
             event_subscription_map[const.CMD_STANDBY](dummy_event)
         # assert:
