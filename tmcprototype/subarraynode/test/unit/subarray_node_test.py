81ee2e68cdd7888f5d6735798ce77217f3909869
Kalyani Thigale <kalyanit.ska@gmail.com>
2020-07-17 10:50:52 +0530

    Resolve pylint warnings on CentralNode

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -1170,6 +1170,7 @@ def test_end_scan_should_command_subarray_to_end_scan_when_it_is_scanning():
         dummy_event_sdp = create_dummy_event_state(sdp_subarray1_ln_proxy_mock, sdp_subarray1_ln_fqdn,
                                                    attribute, ObsState.READY)
         event_subscription_map[sdp_subarray1_obsstate_attribute](dummy_event_sdp)
+        attribute = 'PointingState'
         dummy_event_dish = create_dummy_event_state(dish_ln_proxy_mock, dish_ln_prefix + "0001", attribute,
                                                     PointingState.TRACK)
         dish_pointing_state_map[dish_pointing_state_attribute](dummy_event_dish)
@@ -1294,6 +1295,7 @@ def test_end_scan_should_raise_devfailed_exception_when_csp_subbarray_ln_throws_
         dummy_event_sdp = create_dummy_event_state(sdp_subarray1_ln_proxy_mock, sdp_subarray1_ln_fqdn,
                                                    attribute, ObsState.READY)
         event_subscription_map[sdp_subarray1_obsstate_attribute](dummy_event_sdp)
+        attribute = 'PointingState'
         dummy_event_dish = create_dummy_event_state(dish_ln_proxy_mock, dish_ln_prefix + "0001", attribute,
                                                     PointingState.TRACK)
         dish_pointing_state_map[dish_pointing_state_attribute](dummy_event_dish)
@@ -1412,6 +1414,7 @@ def test_end_sb_should_command_subarray_to_end_sb_when_it_is_ready():
         dummy_event_sdp = create_dummy_event_state(sdp_subarray1_ln_proxy_mock, sdp_subarray1_ln_fqdn,
                                                    attribute, ObsState.READY)
         event_subscription_map[sdp_subarray1_obsstate_attribute](dummy_event_sdp)
+        attribute = 'PointingState'
         dummy_event_dish = create_dummy_event_state(dish_ln_proxy_mock, dish_ln_prefix + "0001", attribute,
                                                     PointingState.TRACK)
         dish_pointing_state_map[dish_pointing_state_attribute](dummy_event_dish)
