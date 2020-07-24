6357941c25b1b3cc2cb862292eda246bd1452b9e
user <jayant.ska@gmail.com>
2020-07-24 15:30:51 +0530

    AT1-588: Add test cases for DishLeafNode's utils module.

--- tmcprototype/dishleafnode/src/dishleafnode/utils.py
+++ tmcprototype/dishleafnode/src/dishleafnode/utils.py
@@ -1,6 +1,8 @@
 """
 Module for DishLeafNode utils
 """
+
+# Imports
 import enum
 import math
 import re
@@ -22,7 +24,16 @@ class UnitConverter:
 
     def dms_to_rad(self, dms):
         """
-        Converts dig:min:sec to radians.
+        Converts a number in Deg:Min:Sec to radians.
+
+        :param argin: list of numbers in degrees, minutes, seconds respectively in string.
+
+        Example: ['20', '30', '40']
+
+        :return: A number in radians.
+
+        Example: 20.500193925472445 is the returned value for ['20', '30', '40'] input.
+
         """
         deg = float(dms[0])
         min = float(dms[1])
@@ -32,7 +43,16 @@ class UnitConverter:
 
     def rad_to_dms(self, rad_value):
         """
-        Converts radians to dig:min:sec.
+        Converts a number in radians to dig:min:sec.
+
+        :param argin: A number in radians.
+
+        Example: 0.123472
+
+        :return: List of numbers in degrees, minutes, seconds respectively in string.
+
+        Example: [7.0, 4.0, 27.928156941480466] is returned value for input 0.123472.
+
         """
         dms = []
         frac_min, deg = math.modf(rad_value * (180 / math.pi))
@@ -43,18 +63,20 @@ class UnitConverter:
         dms.append(sec)
         return dms
 
-    def dms_to_dd(self, dish_antenna_latitude):
+    def dms_to_dd(self, angle):
         """
-        Converts latitude from deg:min:sec to decimal degree format.
+        Converts a number in dig:min:sec to decimal degrees.
 
-        :param dish_antenna_latitude: latitude of Dish location in Deg:Min:Sec.
+        :param angle: A number in Deg:Min:Sec.
+
         Example: 18:31:48.0
 
-        :return: latitude of Dish location in decimal Degree.
+        :return: A number in decimal Degrees.
 
-        Example : "18.529999999999998" is the returned value of dmstodd
+        Example : "18.529999999999998" is the returned value for 18:31:48.0 input.
+
         """
-        dd = re.split('[:]+', dish_antenna_latitude)
+        dd = re.split('[:]+', angle)
         deg_dec = abs(float(dd[0])) + ((float(dd[1])) / 60) + ((float(dd[2])) / 3600)
         if "-" in dd[0]:
             return deg_dec * (-1)
