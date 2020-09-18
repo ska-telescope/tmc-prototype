2c2cff8c268ed2388460189f5b1a688dbf8d6fb8
user <jayant.ska@gmail.com>
2020-09-18 12:26:33 +0530

    AT1-612: Update run_tox.sh file.

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -2,9 +2,9 @@
 set -eo pipefail
 #Entering into a bash shell script to run unit-test cases and generating reports
 echo "In run_tox file...."
-python3 -m pip install pytest-forked mock
+python3 -m pip install pytest-forked mock numpy==1.17.2
 python3 -m pip install --index-url https://nexus.engageska-portugal.pt/repository/pypi/simple ska-logging==0.3.0 \
-lmcbaseclasses==0.6.5 cdm-shared-library==2.0.0 ska-telescope-model==0.1.4 numpy==1.17.2 pytango>=9.3.2 jsonschema>=3.2.0 \
+lmcbaseclasses==0.6.5 cdm-shared-library==2.0.0 ska-telescope-model==0.1.4 pytango>=9.3.2 jsonschema>=3.2.0 \
 marshmallow fire
 cd centralnode;
 tox -e py37
