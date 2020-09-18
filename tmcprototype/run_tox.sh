4be7fe3f14e45d433d4b6ec5887df5b91e00fbd9
user <jayant.ska@gmail.com>
2020-09-18 15:49:16 +0530

    AT1-612: Update run_tox file to get xml reports.

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 #Entering into a bash shell script to run unit-test cases and generating reports
 echo "In run_tox file...."
+mkdir tox_report
 cd centralnode;
 tox -e py37
 cd ../cspmasterleafnode
@@ -16,3 +17,12 @@ cd ../sdpsubarrayleafnode
 tox -e py37
 cd ../subarraynode
 tox -e py37
+cd ..
+
+mv build/reports/centralnode-code-coverage.xml tox_report/centralnode-code-coverage.xml
+mv build/reports/cspmasterleafnode-code-coverage.xml tox_report/cspmasterleafnode-code-coverage.xml
+mv build/reports/cspsubarrayleafnode-code-coverage.xml tox_report/cspsubarrayleafnode-code-coverage.xml
+mv build/reports/dishleafnode-code-coverage.xml tox_report/dishleafnode-code-coverage.xml
+mv build/reports/sdpmasterleafnode-code-coverage.xml tox_report/sdpmasterleafnode-code-coverage.xml
+mv build/reports/sdpsubarrayleafnode-code-coverage.xml tox_report/sdpsubarrayleafnode-code-coverage.xml
+mv build/reports/subarraynode-code-coverage.xml tox_report/subarraynode-code-coverage.xml
