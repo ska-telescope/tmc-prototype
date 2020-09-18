de93d60a6c130b160808528442bc82dd66201e73
user <jayant.ska@gmail.com>
2020-09-18 16:10:50 +0530

    AT1-612: Update run_tox file to get xml reports.

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -5,27 +5,24 @@ echo "In run_tox file...."
 mkdir tox_report
 cd centralnode;
 tox -e py37
-#cd ../cspmasterleafnode
-#tox -e py37
-#cd ../cspsubarrayleafnode
-#tox -e py37
-#cd ../dishleafnode
-#tox -e py37
-#cd ../sdpmasterleafnode
-#tox -e py37
-#cd ../sdpsubarrayleafnode
-#tox -e py37
-#cd ../subarraynode
-#tox -e py37
-pwd
+mv build/reports/centralnode-code-coverage.xml ../tox_report/centralnode-code-coverage.xml
+cd ../cspmasterleafnode
+tox -e py37
+mv build/reports/cspmasterleafnode-code-coverage.xml ../tox_report/cspmasterleafnode-code-coverage.xml
+cd ../cspsubarrayleafnode
+tox -e py37
+mv build/reports/cspsubarrayleafnode-code-coverage.xml ../tox_report/cspsubarrayleafnode-code-coverage.xml
+cd ../dishleafnode
+tox -e py37
+mv build/reports/dishleafnode-code-coverage.xml ../tox_report/dishleafnode-code-coverage.xml
+cd ../sdpmasterleafnode
+tox -e py37
+mv build/reports/sdpmasterleafnode-code-coverage.xml ../tox_report/sdpmasterleafnode-code-coverage.xml
+cd ../sdpsubarrayleafnode
+tox -e py37
+mv build/reports/sdpsubarrayleafnode-code-coverage.xml ../tox_report/sdpsubarrayleafnode-code-coverage.xml
+cd ../subarraynode
+tox -e py37
+mv build/reports/subarraynode-code-coverage.xml ../tox_report/subarraynode-code-coverage.xml
+cd ../tox_report
 ls
-cd ..
-pwd
-ls
-mv ./build/reports/centralnode-code-coverage.xml tox_report/centralnode-code-coverage.xml
-#mv build/reports/cspmasterleafnode-code-coverage.xml tox_report/cspmasterleafnode-code-coverage.xml
-#mv build/reports/cspsubarrayleafnode-code-coverage.xml tox_report/cspsubarrayleafnode-code-coverage.xml
-#mv build/reports/dishleafnode-code-coverage.xml tox_report/dishleafnode-code-coverage.xml
-#mv build/reports/sdpmasterleafnode-code-coverage.xml tox_report/sdpmasterleafnode-code-coverage.xml
-#mv build/reports/sdpsubarrayleafnode-code-coverage.xml tox_report/sdpsubarrayleafnode-code-coverage.xml
-#mv build/reports/subarraynode-code-coverage.xml tox_report/subarraynode-code-coverage.xml
