3e031501b21cffb6faaf8ba18fe1caa55f1cc9f6
Kalyani Thigale <kalyanit.ska@gmail.com>
2020-09-21 17:50:23 +0530

    move generated reports into build folder

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -3,7 +3,6 @@ set -eo pipefail
 #Entering into a bash shell script to run unit-test cases and generating reports
 echo "In run_tox file...."
 mkdir tox_report
-mkdir reports
 cd centralnode;
 tox -e py37
 mv centralnode_coverage ../tox_report/centralnode_coverage
@@ -25,27 +24,29 @@ mv sdpsubarrayleafnode_coverage ../tox_report/sdpsubarrayleafnode_coverage
 cd ../subarraynode
 tox -e py37
 mv subarraynode_coverage ../tox_report/subarraynode_coverage
+echo "moving tox folder into build folder"
+mv ../tox_report ../build/reports
 echo "In tox file...."
-cd ../tox_report
+cd ../build/reports/tox_report
 ls
 
 # # Combine coverage reports
-# coverage combine centralnode_coverage cspmasterleafnode_coverage \
-#                   cspsubarrayleafnode_coverage dishleafnode_coverage \
-#                   sdpmasterleafnode_coverage sdpsubarrayleafnode_coverage \
-#                   subarraynode_coverage && coverage xml
-# cd ..
-# cd ./tox_report && mv coverage.xml ../reports/tox_code-coverage.xml
-# cd ..
-# pwd
-# python3 -m pip install junitparser
-# junitparser merge ./centralnode/build/reports/centralnode-unit-tests.xml \
-#                 ./cspmasterleafnode/build/reports/cspmasterleafnode-unit-tests.xml \
-#                 ./cspsubarrayleafnode/build/reports/cspsubarrayleafnode-unit-tests.xml \
-#                 ./dishleafnode/build/reports/dishleafnode-unit-tests.xml \
-#                 ./sdpmasterleafnode/build/reports/sdpmasterleafnode-unit-tests.xml \
-#                 ./sdpsubarrayleafnode/build/reports/sdpsubarrayleafnode-unit-tests.xml \
-#                 ./subarraynode/build/reports/subarraynode-unit-tests.xml \
-#                 ./reports/tox_unit-tests.xml
-# cd reports
-# ls
+coverage combine centralnode_coverage cspmasterleafnode_coverage \
+                  cspsubarrayleafnode_coverage dishleafnode_coverage \
+                  sdpmasterleafnode_coverage sdpsubarrayleafnode_coverage \
+                  subarraynode_coverage && coverage xml
+cd ..
+cd ./tox_report && mv coverage.xml ../reports/tox_code-coverage.xml
+cd ..
+pwd
+ls
+python3 -m pip install junitparser
+junitparser merge ./centralnode/build/reports/centralnode-unit-tests.xml \
+                ./cspmasterleafnode/build/reports/cspmasterleafnode-unit-tests.xml \
+                ./cspsubarrayleafnode/build/reports/cspsubarrayleafnode-unit-tests.xml \
+                ./dishleafnode/build/reports/dishleafnode-unit-tests.xml \
+                ./sdpmasterleafnode/build/reports/sdpmasterleafnode-unit-tests.xml \
+                ./sdpsubarrayleafnode/build/reports/sdpsubarrayleafnode-unit-tests.xml \
+                ./subarraynode/build/reports/subarraynode-unit-tests.xml \
+                ./reports/tox_unit-tests.xml
+ls
