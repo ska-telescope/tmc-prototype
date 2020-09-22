a102849fc9f4e317e90178efb56257c8ff41744e
Kalyani Thigale <kalyanit.ska@gmail.com>
2020-09-22 13:10:46 +0530

    Update lint job to install centralnode package

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eo pipefail
-export working_dir=./build/reports/tox_report
+export working_dir=../build/reports/tox_report
 #Entering into a bash shell script to run unit-test cases and generating reports
 echo "In run_tox file...."
 mkdir -p tox_report
-echo "Check reports if tox_reports already exists...."
-ls 
+echo "Check reports dir if tox_reports already exists...."
 ls ../build/reports/
 if [ -d "$working_dir" ]; then rm -rf $working_dir; fi
-
+echo "After removing tox_reports"
+ls ../build/reports/
 cd centralnode
 tox -e py37
 mv centralnode_coverage ../tox_report/centralnode_coverage
