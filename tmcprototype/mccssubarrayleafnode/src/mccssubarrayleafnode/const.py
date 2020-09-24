fe516ceec376547e3732da9e710175a1df6ade60
Akash Karangale <akashk.ska@gmail.com>
2020-09-24 18:12:34 +0530

    AT1-637: Add End command on Mccs Subarray leafnode.

--- tmcprototype/mccssubarrayleafnode/src/mccssubarrayleafnode/const.py
+++ tmcprototype/mccssubarrayleafnode/src/mccssubarrayleafnode/const.py
@@ -2,15 +2,22 @@
 const file for MCCSSubarrayLeafNode
 """
 # In/Out command constants
-
+CMD_END = "End"
 #Event messages
 
 #Error messages
 ERR_IN_CREATE_PROXY_MCCSSA = "Error in creating proxy of the MCCS Subarray device."
+ERR_INVOKING_CMD = "Error in invoking command: "
+ERR_DEVICE_NOT_READY = "Mccs subarray is not in READY obsState."
+ERR_END_INVOKING_CMD = "Error while invoking End command on MCCS Subarray."
 
 #strings
 #General strings
 STR_MCCSSALN_INIT_SUCCESS = "MCCSSubarrayLeafNode initialized successfully."
+STR_COMMAND = "Command :-> "
+STR_INVOKE_SUCCESS = " invoked successfully."
+STR_END_SUCCESS = "End command is invoked successfully on MccsSubarray."
+STR_OBS_STATE = "MCCS Subarray Leaf Node obsState is: "
 
 #INTEGERS
 INT_SKA_LEVEL = 3
