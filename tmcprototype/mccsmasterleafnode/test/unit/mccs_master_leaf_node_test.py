ba6b6a761fd666fb124f0b8dc1ccca6129d9bf61
Akash Karangale <akashk.ska@gmail.com>
2020-09-24 19:20:10 +0530

    AT1-637: Add End command test case on Mccs Subarray leafnode.

--- tmcprototype/mccsmasterleafnode/test/unit/mccs_master_leaf_node_test.py
+++ tmcprototype/mccsmasterleafnode/test/unit/mccs_master_leaf_node_test.py
@@ -1,171 +1,86 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# This file is part of the MCCSMasterLeafNode project
-#
-#
-#
-# Distributed under the terms of the GPL license.
-# See LICENSE.txt for more info.
-"""Contain the tests for the ."""
-
 # Path
+import contextlib
+import importlib
 import sys
 import os
+import pytest
 path = os.path.join(os.path.dirname(__file__), os.pardir)
 sys.path.insert(0, os.path.abspath(path))
 
 # Imports
 from time import sleep
+import mock
+from mock import Mock
 from mock import MagicMock
 from PyTango import DevFailed, DevState
 from devicetest import DeviceTestCase, main
-from MCCSMasterLeafNode import MCCSMasterLeafNode
-
-# Note:
-#
-# Since the device uses an inner thread, it is necessary to
-# wait during the tests in order the let the device update itself.
-# Hence, the sleep calls have to be secured enough not to produce
-# any inconsistent behavior. However, the unittests need to run fast.
-# Here, we use a factor 3 between the read period and the sleep calls.
-#
-# Look at devicetest examples for more advanced testing
+from MCCSMasterLeafNode import MCCSMasterLeafNode,const, release
+from ska.base.control_model import HealthState, ObsState, LoggingLevel
+from tango.test_context import DeviceTestContext
 
 
-# Device test case
-class MCCSMasterLeafNodeDeviceTestCase(DeviceTestCase):
-    """Test case for packet generation."""
-    # PROTECTED REGION ID(MCCSMasterLeafNode.test_additionnal_import) ENABLED START #
-    # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_additionnal_import
-    device = MCCSMasterLeafNode
-    properties = {'SkaLevel': '3', 'GroupDefinitions': '', 'LoggingLevelDefault': '5', 'LoggingTargetsDefault': 'tango::logger', 'MCCSMasterFQDN': 'low_mccs/elt/master', 
-                  }
-    empty = None  # Should be []
-
-    @classmethod
-    def mocking(cls):
-        """Mock external libraries."""
-        # Example : Mock numpy
-        # cls.numpy = MCCSMasterLeafNode.numpy = MagicMock()
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_mocking) ENABLED START #
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_mocking
-
-    def test_properties(self):
-        # test the properties
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_properties) ENABLED START #
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_properties
-        pass
-
-    def test_State(self):
-        """Test for State"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_State) ENABLED START #
-        self.device.State()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_State
-
-    def test_Status(self):
-        """Test for Status"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_Status) ENABLED START #
-        self.device.Status()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_Status
-
-    def test_GetVersionInfo(self):
-        """Test for GetVersionInfo"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_GetVersionInfo) ENABLED START #
-        self.device.GetVersionInfo()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_GetVersionInfo
-
-    def test_Reset(self):
-        """Test for Reset"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_Reset) ENABLED START #
-        self.device.Reset()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_Reset
-
-    def test_AssignResource(self):
-        """Test for AssignResource"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_AssignResource) ENABLED START #
-        self.device.AssignResource("")
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_AssignResource
-
-    def test_ReleaseResources(self):
-        """Test for ReleaseResources"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_ReleaseResources) ENABLED START #
-        self.device.ReleaseResources("")
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_ReleaseResources
-
-    def test_On(self):
-        """Test for On"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_On) ENABLED START #
-        self.device.On()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_On
-
-    def test_Off(self):
-        """Test for Off"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_Off) ENABLED START #
-        self.device.Off()
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_Off
-
-    def test_buildState(self):
-        """Test for buildState"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_buildState) ENABLED START #
-        self.device.buildState
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_buildState
-
-    def test_versionId(self):
-        """Test for versionId"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_versionId) ENABLED START #
-        self.device.versionId
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_versionId
-
-    def test_loggingLevel(self):
-        """Test for loggingLevel"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_loggingLevel) ENABLED START #
-        self.device.loggingLevel
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_loggingLevel
-
-    def test_healthState(self):
-        """Test for healthState"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_healthState) ENABLED START #
-        self.device.healthState
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_healthState
-
-    def test_adminMode(self):
-        """Test for adminMode"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_adminMode) ENABLED START #
-        self.device.adminMode
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_adminMode
-
-    def test_controlMode(self):
-        """Test for controlMode"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_controlMode) ENABLED START #
-        self.device.controlMode
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_controlMode
-
-    def test_simulationMode(self):
-        """Test for simulationMode"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_simulationMode) ENABLED START #
-        self.device.simulationMode
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_simulationMode
-
-    def test_testMode(self):
-        """Test for testMode"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_testMode) ENABLED START #
-        self.device.testMode
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_testMode
-
-    def test_activitymessage(self):
-        """Test for activitymessage"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_activitymessage) ENABLED START #
-        self.device.activitymessage
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_activitymessage
-
-    def test_loggingTargets(self):
-        """Test for loggingTargets"""
-        # PROTECTED REGION ID(MCCSMasterLeafNode.test_loggingTargets) ENABLED START #
-        self.device.loggingTargets
-        # PROTECTED REGION END #    //  MCCSMasterLeafNode.test_loggingTargets
+@pytest.fixture(scope="function")
+def event_subscription_with_arg(mock_mccs_subarray):
+    event_subscription_map = {}
+    mock_mccs_subarray[1].command_inout_asynch.side_effect = (
+        lambda command_name, argument, callback, *args,
+               **kwargs: event_subscription_map.update({command_name: callback}))
+    yield event_subscription_map
 
 
-# Main execution
-if __name__ == "__main__":
-    main()
+@pytest.fixture(scope="function")
+def event_subscription_without_arg(mock_mccs_subarray):
+    event_subscription_map = {}
+    mock_mccs_subarray[1].command_inout_asynch.side_effect = (
+        lambda command_name, callback, *args,
+               **kwargs: event_subscription_map.update({command_name: callback}))
+    yield event_subscription_map
+
+
+@pytest.fixture(scope="function")
+def mock_mccs_subarray():
+    mccs_subarray1_fqdn = "low_mccs/elt/subarray_01"
+    dut_properties = {
+        'MCCSSubarrayFQDN': mccs_subarray1_fqdn
+    }
+    mccs_subarray1_proxy_mock = Mock()
+    proxies_to_mock = {
+        mccs_subarray1_fqdn: mccs_subarray1_proxy_mock
+    }
+    with fake_tango_system(MCCSMasterLeafNode, initial_dut_properties=dut_properties,
+                           proxies_to_mock=proxies_to_mock) as tango_context:
+        yield tango_context.device, mccs_subarray1_proxy_mock
+
+
+def command_callback(command_name):
+    fake_event = MagicMock()
+    fake_event.err = False
+    fake_event.cmd_name = f"{command_name}"
+    return fake_event
+
+
+def test_End_command_with_callback_method(mock_mccs_subarray, event_subscription_without_arg):
+    # arrange:
+    device_proxy, mccs_subarray1_proxy_mock = mock_mccs_subarray
+    mccs_subarray1_proxy_mock.obsState = ObsState.READY
+    device_proxy.End()
+    dummy_event = command_callback(const.CMD_GOTOIDLE)
+    event_subscription_without_arg[const.CMD_GOTOIDLE](dummy_event)
+    assert const.STR_COMMAND + const.CMD_GOTOIDLE in device_proxy.activityMessage
+
+
+@contextlib.contextmanager
+def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_mock={},
+                      device_proxy_import_path='tango.DeviceProxy'):
+    with mock.patch(device_proxy_import_path) as patched_constructor:
+        patched_constructor.side_effect = lambda device_fqdn: proxies_to_mock.get(device_fqdn, Mock())
+        patched_module = importlib.reload(sys.modules[device_under_test.__module__])
+
+    device_under_test = getattr(patched_module, device_under_test.__name__)
+
+    device_test_context = DeviceTestContext(device_under_test, properties=initial_dut_properties)
+    device_test_context.start()
+    yield device_test_context
+    device_test_context.stop()
+
+
