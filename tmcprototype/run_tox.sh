27a3d467005c0f392b15d7d1e3c012d2fbada597
user <jayant.ska@gmail.com>
2020-10-06 11:41:32 +0530

    AT1-629: Update run_tox.sh file.

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -31,14 +31,14 @@ cd ../../../tmcprototype
 
 python3 -m pip install junitparser
 junitparser merge ./centralnode/build/reports/centralnode-unit-tests.xml \
-                 ./centralnode/build/reports/centralnodelow-unit-tests.xml \
+                 ./centralnodelow/build/reports/centralnodelow-unit-tests.xml \
                 ./cspmasterleafnode/build/reports/cspmasterleafnode-unit-tests.xml \
                 ./cspsubarrayleafnode/build/reports/cspsubarrayleafnode-unit-tests.xml \
                 ./dishleafnode/build/reports/dishleafnode-unit-tests.xml \
                 ./sdpmasterleafnode/build/reports/sdpmasterleafnode-unit-tests.xml \
                 ./sdpsubarrayleafnode/build/reports/sdpsubarrayleafnode-unit-tests.xml \
-                ./sdpsubarrayleafnode/build/reports/mccsmasterleafnode-unit-tests.xml \
-                ./sdpsubarrayleafnode/build/reports/mccssubarrayleafnode-unit-tests.xml \
+                ./mccsmasterleafnode/build/reports/mccsmasterleafnode-unit-tests.xml \
+                ./mccssubarrayleafnode/build/reports/mccssubarrayleafnode-unit-tests.xml \
                 ./subarraynode/build/reports/subarraynode-unit-tests.xml \
-                ./subarraynode/build/reports/subarraynodelow-unit-tests.xml \
+                ./subarraynodelow/build/reports/subarraynodelow-unit-tests.xml \
                 ../build/reports/unit-tests.xml
