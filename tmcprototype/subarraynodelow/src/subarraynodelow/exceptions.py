f1dc22bcaab67b0e040aa9e050c17ebadd7a1f40
user <jayant.ska@gmail.com>
2020-10-13 17:58:13 +0530

    AT1-667: Add Scan Command validator for SubarrayNodeLow.

--- tmcprototype/subarraynodelow/src/subarraynodelow/exceptions.py
+++ tmcprototype/subarraynodelow/src/subarraynodelow/exceptions.py
@@ -1,2 +1,7 @@
+from marshmallow import ValidationError
+
 class InvalidObsStateError(ValueError):
     """Raised when subarray is not in required obsState."""
+
+class InvalidJSONError(ValidationError):
+    """Raised when The JSON format is invalid"""
---
+++ tmcprototype/subarraynodelow/src/subarraynodelow/input_validator.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the SubarrayNodeLow project
+#
+#
+#
+# Distributed under the terms of the BSD-3-Clause license.
+# See LICENSE.txt for more info.
+
+# standard Python imports
+import json
+from json import JSONDecodeError
+import logging
+
+# SKA specific imports
+from .exceptions import InvalidJSONError
+from ska.cdm.schemas import CODEC
+from ska.cdm.messages.subarray_node.scan import ScanRequest
+from marshmallow import ValidationError
+
+module_logger = logging.getLogger(__name__)
+
+
+class ScanValidator():
+    def __init__(self, logger=module_logger):
+        self.logger = logger
+
+    def loads(self, input_string):
+        """
+            Validates the input string received as an argument of Scan command.
+            If the request is correct, returns the deserialized JSON object. The cdm-shared-library
+            is used to validate the JSON.
+
+            :param: input_string: A JSON string
+
+            :return: Deserialized JSON object if successful.
+
+            :throws:
+                InvalidJSONError: When the JSON string is not formatted properly.
+            """
+
+        ## Check if JSON is correct
+        self.logger.info("Checking JSON format.")
+        try:
+            scan_request = CODEC.loads(ScanRequest, input_string)
+        except(ValidationError, JSONDecodeError) as json_error:
+            self.logger.exception("Exception: %s", str(json_error))
+            exception_message = "Malformed input string. Please check the JSON format." + \
+                                "Full exception info: " + \
+                                str(json_error)
+            raise InvalidJSONError(exception_message)
+        return json.loads(input_string)
