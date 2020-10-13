491c2c8e5dbefff379fd371eab4d64776c71025f
Apurva Patkar <apurva.ska@gmail.com>
2020-10-13 10:54:48 +0000

    AT1-660: Add file for scan in TMC Low for integration testing.

--- post-deployment/resources/test_support/tmc_helpers_low.py
+++ post-deployment/resources/test_support/tmc_helpers_low.py
@@ -13,62 +13,63 @@ LOGGER = logging.getLogger(__name__)
 
 @sync_start_up_telescope
 def start_up():
-    CentralNode = DeviceProxy('ska_mid/tm_central/central_node')
-    LOGGER.info("Before Sending StartupTelescope command on CentralNode state :" + str(CentralNode.State()))   
-    CentralNode.StartUpTelescope()
+    CentralNodeLow = DeviceProxy('ska_low/tm_central/central_node')
+    LOGGER.info("Before Sending StartupTelescope command on CentralNodeLow state :" + str(CentralNodeLow.State()))   
+    CentralNodeLow.StartUpTelescope()
 
 @sync_assign_resources(2,300)
 def compose_sub():
-    resource('ska_mid/tm_subarray_node/1').assert_attribute('State').equals('ON')
-    resource('ska_mid/tm_subarray_node/1').assert_attribute('obsState').equals('EMPTY')
-    assign_resources_file = 'resources/test_data/TMC_integration/assign_resources1.json'
-    sdp_block = update_resource_config_file(assign_resources_file)
-    LOGGER.info("_______sdp_block________" + str(sdp_block))
+    resource('ska_low/tm_subarray_node/1').assert_attribute('State').equals('ON')
+    resource('ska_low/tm_subarray_node/1').assert_attribute('obsState').equals('EMPTY')
+    assign_resources_file = 'resources/test_data/TMC_integration/mccs_assign_resources.json'
+    mccs_block = update_resource_config_file(assign_resources_file)
+    LOGGER.info("_______mccs_block________" + str(mccs_block))
     config = load_config_from_file(assign_resources_file)
-    CentralNode = DeviceProxy('ska_mid/tm_central/central_node')
-    CentralNode.AssignResources(config)
+    CentralNodeLow = DeviceProxy('ska_low/tm_central/central_node')
+    CentralNodeLow.AssignResources(config)
     the_waiter = waiter()
     the_waiter.wait()
-    LOGGER.info('Invoked AssignResources on CentralNode')
-    return sdp_block
+    LOGGER.info('Invoked AssignResources on CentralNodeLow')
+    return mccs_block
 
 @sync_end_sb
 def end_sb():
-    resource('ska_mid/tm_subarray_node/1').assert_attribute('obsState').equals('READY')
-    resource('mid_csp/elt/subarray_01').assert_attribute('obsState').equals('READY')
-    resource('mid_sdp/elt/subarray_1').assert_attribute('obsState').equals('READY')
+    resource('ska_low/tm_subarray_node/1').assert_attribute('obsState').equals('READY')
+    # resource('mid_csp/elt/subarray_01').assert_attribute('obsState').equals('READY')
+    # resource('mid_sdp/elt/subarray_1').assert_attribute('obsState').equals('READY')
+    resource('low-mccs/subarray/01').assert_attribute('obsState').equals('READY')
     LOGGER.info('Before invoking End Command all the devices obsstate is ready')
-    SubarrayNode = DeviceProxy('ska_mid/tm_subarray_node/1')
-    SubarrayNode.End()
+    SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
+    SubarrayNodeLow.End()
     LOGGER.info('Invoked End on Subarray')
 
 @sync_release_resources
 def release_resources():
-    resource('ska_mid/tm_subarray_node/1').assert_attribute('obsState').equals('IDLE')
-    CentralNode = DeviceProxy('ska_mid/tm_central/central_node')
-    CentralNode.ReleaseResources('{"subarrayID":1,"releaseALL":true,"receptorIDList":[]}')
-    SubarrayNode = DeviceProxy('ska_mid/tm_subarray_node/1')
-    LOGGER.info('After Release Resource SubarrayNode State and ObsState:' + str(SubarrayNode.State()) + str(SubarrayNode.ObsState))
+    resource('ska_low/tm_subarray_node/1').assert_attribute('obsState').equals('IDLE')
+    CentralNodeLow = DeviceProxy('ska_low/tm_central/central_node')
+    CentralNodeLow.ReleaseResources('{"subarrayID":1,"releaseALL":true,"receptorIDList":[]}')
+    SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
+    LOGGER.info('After Release Resource SubarrayNodeLow State and ObsState:' + str(SubarrayNodeLow.State()) + str(SubarrayNodeLow.ObsState))
     LOGGER.info('Invoked ReleaseResources on Subarray')
 
 
 @sync_set_to_standby
 def set_to_standby():
-    CentralNode = DeviceProxy('ska_mid/tm_central/central_node')
-    CentralNode.StandByTelescope()
-    SubarrayNode = DeviceProxy('ska_mid/tm_subarray_node/1')
-    LOGGER.info('After Standby SubarrayNode State and ObsState:' + str(SubarrayNode.State()) + str(SubarrayNode.ObsState))
-    LOGGER.info('After Standby CentralNode State:' + str(CentralNode.State()))
+    CentralNodeLow = DeviceProxy('ska_low/tm_central/central_node')
+    CentralNodeLow.StandByTelescope()
+    SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
+    LOGGER.info('After Standby SubarrayNodeLow State and ObsState:' + str(SubarrayNodeLow.State()) + str(SubarrayNodeLow.ObsState))
+    LOGGER.info('After Standby CentralNodeLow State:' + str(CentralNodeLow.State()))
     LOGGER.info('Standby the Telescope')
 
 @sync_configure
-def configure_sub(sdp_block, configure_file):
-    #resource('ska_mid/tm_subarray_node/1').assert_attribute('State').equals('ON')
-    update_scan_config_file(configure_file, sdp_block)
+def configure_sub(mccs_block, configure_file):
+    #resource('ska_low/tm_subarray_node/1').assert_attribute('State').equals('ON')
+    update_scan_config_file(configure_file, mccs_block)
     config = load_config_from_file(configure_file)
-    SubarrayNode = DeviceProxy('ska_mid/tm_subarray_node/1')
-    SubarrayNode.Configure(config)
-    LOGGER.info("Subarray obsState is: " + str(SubarrayNode.obsState))
+    SubarrayNodeLow = DeviceProxy('ska_low/tm_subarray_node/1')
+    SubarrayNodeLow.Configure(config)
+    LOGGER.info("Subarray obsState is: " + str(SubarrayNodeLow.obsState))
     LOGGER.info('Invoked Configure on Subarray')
 
 
