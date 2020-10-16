51694e7a211187622ac3d9f557d4267d58ee5442
Akash Karangale <akashk.ska@gmail.com>
2020-10-16 10:50:12 +0530

    change sdp master testcases which are failing..

--- tmcprototype/sdpmasterleafnode/test/unit/sdp_master_leaf_node_test.py
+++ tmcprototype/sdpmasterleafnode/test/unit/sdp_master_leaf_node_test.py
@@ -113,7 +113,7 @@ def test_command_should_raise_exception(mock_sdp_master, command_name_to_raise_d
     device_proxy.On()
     sdp_master_proxy_mock.command_inout_asynch.side_effect = raise_devfailed_without_arg
     with pytest.raises(tango.DevFailed) as df:
-        tango_context.device.command_inout(cmd_name)
+        device_proxy.command_inout(cmd_name)
     print("Value of DF in other commands::::",str(df))
     assert error_msg in str(df)
 
