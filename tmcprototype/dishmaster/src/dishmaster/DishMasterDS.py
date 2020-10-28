e797861c9c01ff89fba3f2397a9f3de6c2835a03
kmadisa <katleho.madisa47@gmail.com>
2020-10-28 13:05:49 +0200

    SAR-156 Fixed the issue whereby tags where not added to the log messages.

--- tmcprototype/dishmaster/src/dishmaster/DishMasterDS.py
+++ tmcprototype/dishmaster/src/dishmaster/DishMasterDS.py
@@ -14,22 +14,20 @@ def main():
         pkg_resources.resource_filename('dishmaster', 'dish_master.fgo'),
         pkg_resources.resource_filename('dishmaster', 'dish_master_SimDD.json')
     ]
-    
-    # set up Python logging
-    configure_logging()
-    log_name = 'dish-master.{}'.format(get_instance_name())
-    logger = logging.getLogger(log_name)
-    logger.info("Logging started for DishMaster application")
 
     # add a filter with this device's name
-    device_name_tag = "tango-device:mid_d00{}/elt/master".format(get_instance_name())
-
+    device_name = 'mid_d{get_instance_name().zfill(4)}/elt/master'
+    device_name_tag = f'tango-device:{device_name}'
     class TangoDeviceTagsFilter(logging.Filter):
         def filter(self, record):
             record.tags = device_name_tag
             return True
 
-    logger.addFilter(TangoDeviceTagsFilter())
+    # set up Python logging
+    configure_logging(tags_filter=TangoDeviceTagsFilter)
+    log_name = 'dish-master-{get_instance_name()}'
+    logger = logging.getLogger(log_name)
+    logger.info('Logging started for {device_name}')
 
     model = configure_device_model(sim_data_files, logger=logger)
     TangoDeviceServers = get_tango_device_server(model, sim_data_files)
