103917ae6131a58a36e657876423140d97c3c5f8
kmadisa <katleho.madisa47@gmail.com>
2020-10-29 17:41:03 +0200

    SAR-156 Formatted code using the black tool.

--- tmcprototype/dishmaster/test/unit/conftest.py
+++ tmcprototype/dishmaster/test/unit/conftest.py
@@ -13,6 +13,7 @@ from tango.test_context import DeviceTestContext
 from tango_simlib import tango_sim_generator
 from tango_simlib.utilities import helper_module
 
+
 @pytest.fixture(scope="class")
 def tango_context(request):
     """Creates and returns a TANGO DeviceTestContext object.
@@ -22,26 +23,21 @@ def tango_context(request):
     request: _pytest.fixtures.SubRequest
         A request object gives access to the requesting test context.
     """
-    properties = {'GroupDefinitions': ''}
-    tango_db = tempfile.mkstemp(prefix='tango')
+    properties = {"GroupDefinitions": ""}
+    tango_db = tempfile.mkstemp(prefix="tango")
     data_descr_files = []
-    data_descr_files.append(
-        pkg_resources.resource_filename('dishmaster', 'dish_master.fgo'))
-    data_descr_files.append(
-        pkg_resources.resource_filename(
-            'dishmaster', 'dish_master_SimDD.json'))
-    device_name = 'test/nodb/dishelementmaster'
-    model = tango_sim_generator.configure_device_model(data_descr_files,
-                                                       device_name)
-    DishMaster = tango_sim_generator.get_tango_device_server(
-        model, data_descr_files)[0]
-    tango_context = DeviceTestContext(DishMaster, db=tango_db,
-                                      process=False, properties=properties)
+    data_descr_files.append(pkg_resources.resource_filename("dishmaster", "dish_master.fgo"))
+    data_descr_files.append(pkg_resources.resource_filename("dishmaster", "dish_master_SimDD.json"))
+    device_name = "test/nodb/dishelementmaster"
+    model = tango_sim_generator.configure_device_model(data_descr_files, device_name)
+    DishMaster = tango_sim_generator.get_tango_device_server(model, data_descr_files)[0]
+    tango_context = DeviceTestContext(DishMaster, db=tango_db, process=False, properties=properties)
     mock_get_db = mock.Mock(return_value=Database(tango_context.db))
     helper_module.get_database = mock_get_db
     tango_context.start()
     yield tango_context
     tango_context.stop()
+
 
 @pytest.fixture(scope="class")
 def initialize_device(tango_context):
