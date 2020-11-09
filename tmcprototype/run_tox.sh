5327009aec4f7699aae122df5632664ed9a19919
shraddhabajare <shraddhab.ska@gmail.com>
2020-11-09 16:57:58 +0530

    AT1-683:Revert run_tox.sh file changes

--- tmcprototype/run_tox.sh
+++ tmcprototype/run_tox.sh
@@ -10,17 +10,14 @@ if [ -d "$REPORTS_DIR" ]; then rm -rf $REPORTS_DIR; fi
 
 mkdir -p $REPORTS_DIR
 
-#for path in $(find ./*/test  -type d -name unit); do
-#	export TMC_ELEMENT=$(basename $(dirname $(dirname $path)));
-#	echo +++ Trying tests for $TMC_ELEMENT;
-#	cd $TMC_ELEMENT;
-#	tox -e py37
-#mv ${TMC_ELEMENT}_coverage ../$REPORTS_DIR;
-#cd ..
-#done
-
-cd subarraynode
-tox -e py37
+for path in $(find ./*/test  -type d -name unit); do
+	export TMC_ELEMENT=$(basename $(dirname $(dirname $path)));
+	echo +++ Trying tests for $TMC_ELEMENT;
+	cd $TMC_ELEMENT;
+	tox -e py37
+mv ${TMC_ELEMENT}_coverage ../$REPORTS_DIR;
+cd ..
+done
 
 # Combine coverage reports
 cd $REPORTS_DIR
