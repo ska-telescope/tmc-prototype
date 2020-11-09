fa8d8e108726b1b9152e46b57fb649965afaf959
shraddhabajare <shraddhab.ska@gmail.com>
2020-11-09 15:56:41 +0530

    AT1-683:Check pipeline

--- tmcprototype/subarraynode/test/unit/subarray_node_test.py
+++ tmcprototype/subarraynode/test/unit/subarray_node_test.py
@@ -1872,16 +1872,32 @@ def command_callback_with_devfailed_exception():
     return fake_event
 
 
+# @contextlib.contextmanager
+# def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_mock={},
+#                       device_proxy_import_path='tango.DeviceProxy'):
+
+#     with mock.patch(device_proxy_import_path) as patched_constructor:
+#         patched_constructor.side_effect = lambda device_fqdn: proxies_to_mock.get(device_fqdn, Mock())
+#         patched_module = importlib.reload(sys.modules[device_under_test.__module__])
+
+#     device_under_test = getattr(patched_module, device_under_test.__name__)
+
+#     device_test_context = DeviceTestContext(device_under_test, properties=initial_dut_properties)
+#     device_test_context.start()
+#     yield device_test_context
+#     device_test_context.stop()
+
 @contextlib.contextmanager
-def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_mock={},
-                      device_proxy_import_path='tango.DeviceProxy'):
+def fake_tango_system(device_under_test, initial_dut_properties={}, proxies_to_mock={}, group_to_mock={},
+                    device_proxy_import_path='tango.DeviceProxy',device_group_import_path='tango.Group'):
 
     with mock.patch(device_proxy_import_path) as patched_constructor:
-        patched_constructor.side_effect = lambda device_fqdn: proxies_to_mock.get(device_fqdn, Mock())
-        patched_module = importlib.reload(sys.modules[device_under_test.__module__])
+        with mock.patch(device_group_import_path) as group_constructor:
+            patched_constructor.side_effect = lambda device_fqdn: proxies_to_mock.get(device_fqdn, Mock())
+            group_constructor.side_effect = lambda group_mock : group_to_mock.get(group_mock, Mock()) #group_to_mock: Mock()
+            patched_module = importlib.reload(sys.modules[device_under_test.__module__])
 
     device_under_test = getattr(patched_module, device_under_test.__name__)
-
     device_test_context = DeviceTestContext(device_under_test, properties=initial_dut_properties)
     device_test_context.start()
     yield device_test_context
