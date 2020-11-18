54562c7453f1d3a10753d28217a103032a766d8a
Gerhard le Roux <gerhardlr@gmail.com>
2020-11-18 09:19:27 +0200

    removed all references to env var

--- tmcprototype/subarraynode/src/subarraynode/transaction_id.py
+++ tmcprototype/subarraynode/src/subarraynode/transaction_id.py
@@ -31,22 +31,20 @@ def identify_with_id(name:str,arg_name:str):
 
 def inject_id(obj,data:Dict)->Dict:
     id = getattr(obj,"transaction_id",None)
-    if ENABLE_TRANSACTION_IDS:
-        if id:
-            data['transaction_id'] = id
+    if id:
+        data['transaction_id'] = id
     return data
     
 
 def update_with_id(obj,parameters:Any)->Union[Dict,str]:
-    if ENABLE_TRANSACTION_IDS:
-        if isinstance(parameters,str):
-            parameters = json.loads(parameters)
-            inject_id(obj,parameters)
-            return json.dumps(parameters)
-        elif isinstance(parameters,dict):
-            inject_id(obj,parameters)
-            return parameters
-        else: raise Exception(f'arg {parameters} is of not type dict or string')
+    if isinstance(parameters,str):
+        parameters = json.loads(parameters)
+        inject_id(obj,parameters)
+        return json.dumps(parameters)
+    elif isinstance(parameters,dict):
+        inject_id(obj,parameters)
+        return parameters
+    else: raise Exception(f'arg {parameters} is of not type dict or string')
     return parameters
 
 def inject_with_id(arg_position:int,arg_name:str):
@@ -61,7 +59,5 @@ def inject_with_id(arg_position:int,arg_name:str):
                 kwargs[arg_name] = update_with_id(obj,kwargs[arg_name])
             else: raise Exception('arguments not matching wrap function')
             return func(obj,*args,**kwargs)
-        if ENABLE_TRANSACTION_IDS:
-            return wrap
-        else: return func
+        return func
     return wrapper
