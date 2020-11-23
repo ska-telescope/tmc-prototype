444c98c6e6fc0a8c1b7c5a9a96532c6cabfa205d
kmadisa <katleho.madisa47@gmail.com>
2020-11-23 14:49:04 +0200

    SAR-157 Code cleanup

--- tmcprototype/dishleafnode/setup.py
+++ tmcprototype/dishleafnode/setup.py
@@ -40,7 +40,6 @@ setup(
     url='https://www.skatelescope.org',
     platforms="Linux",
     install_requires=['pytango>=9.3.2', 'mock', 'katpoint', 'ska_logging>=0.3.0', 'lmcbaseclasses>=0.7.1'],
-    #test_suite='test',
     setup_requires=[
         # dependency for `python setup.py test`
         'pytest-runner',
