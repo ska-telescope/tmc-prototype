86671cfff41a8be500f4ccacae6e1df6b391aa6d
Kalyani Thigale <kalyanit.ska@gmail.com>
2020-12-21 10:19:45 +0530

    AT1-711: Fix issues while running unit test cases

--- tmcprototype/subarraynode/src/subarraynode/subarray_node.py
+++ tmcprototype/subarraynode/src/subarraynode/subarray_node.py
@@ -22,8 +22,7 @@ from tango.server import run,attribute, command, device_property
 # Additional imports
 from . import const, release, assign_resources_command, release_all_resources_command, configure_command,\
     scan_command, end_scan_command, end_command, on_command, off_command, track_command,\
-    abort_command, restart_command, obsreset_command, tango_client, tango_server_helper, tango_group_client,\
-    health_state_agrregator, obs_state_aggregator
+    abort_command, restart_command, obsreset_command, tango_client, tango_server_helper, tango_group_client
 from .const import PointingState
 from ska.base.commands import ResultCode
 from ska.base.control_model import HealthState, ObsMode, ObsState
@@ -36,8 +35,7 @@ from subarraynode.exceptions import InvalidObsStateError
 __all__ = ["SubarrayNode", "main", "assign_resources_command", "release_all_resources_command",
            "configure_command", "scan_command", "end_scan_command", "end_command", "on_command",
            "off_command", "track_command", "abort_command", "restart_command", "obsreset_command",
-           "device_data", "tango_client", "health_state_agrregator", "obs_state_aggregator",
-           "tango_group_client", "tango_server_helper"]
+           "device_data", "tango_client", "tango_group_client", "tango_server_helper"]
 
 
 class SubarrayNode(SKASubarray):
