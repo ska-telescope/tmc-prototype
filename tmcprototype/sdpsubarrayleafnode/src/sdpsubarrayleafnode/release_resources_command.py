16620266dd0a3d2303e3f2271e19c3532ea92a69
apurvapatkar <apurva.ska@gmail.com>
2020-12-29 18:16:49 +0530

    AT1-728: Update ReleaseResources command to utilise tmc-common package and device_data moudule.

--- tmcprototype/sdpsubarrayleafnode/src/sdpsubarrayleafnode/release_resources_command.py
+++ tmcprototype/sdpsubarrayleafnode/src/sdpsubarrayleafnode/release_resources_command.py
@@ -9,6 +9,7 @@ from tango import DevState, DevFailed
 from ska.base.commands import BaseCommand
 from . import const
 from ska.base.control_model import ObsState
+from tmc.common.tango_client import TangoClient
 
 class ReleaseAllResources(BaseCommand):
     """
@@ -26,7 +27,7 @@ class ReleaseAllResources(BaseCommand):
         :raises: Exception if command execution throws any type of exception
 
         """
-        device = self.target
+        device_data = self.target
         if self.state_model.op_state in [DevState.FAULT, DevState.UNKNOWN, DevState.DISABLE]:
             tango.Except.throw_exception("ReleaseAllResources() is not allowed in current state",
                                             "Failed to invoke ReleaseAllResources command on "
@@ -34,11 +35,12 @@ class ReleaseAllResources(BaseCommand):
                                             "SdpSubarrayLeafNode.ReleaseAllResources()",
                                             tango.ErrSeverity.ERR)
         
-        if device._sdp_subarray_proxy.obsState != ObsState.IDLE:
-            tango.Except.throw_exception(const.STR_RELEASE_RES_EXEC, "Failed to invoke ReleaseAllResources command on "
-                                            "SdpSubarrayLeafNode.",
-                                            "SdpSubarrayLeafNode.ReleaseAllResourcesCommand()",
-                                            tango.ErrSeverity.ERR)
+        # sdp_sa_ln_client = TangoClient(device_data._sdp_sa_fqdn)
+        # if sdp_sa_ln_client.get_attribute("obsState") != ObsState.IDLE:
+        #     tango.Except.throw_exception(const.STR_RELEASE_RES_EXEC, "Failed to invoke ReleaseAllResources command on "
+        #                                     "SdpSubarrayLeafNode.",
+        #                                     "SdpSubarrayLeafNode.ReleaseAllResourcesCommand()",
+        #                                     tango.ErrSeverity.ERR)
         return True
 
     def releaseallresources_cmd_ended_cb(self, event):
@@ -62,14 +64,14 @@ class ReleaseAllResources(BaseCommand):
 
         :return: none
         """
-        device = self.target
+        device_data = self.target
         if event.err:
             log = const.ERR_INVOKING_CMD + str(event.cmd_name) + str(event.errors)
-            device._read_activity_message = log
+            device_data._read_activity_message = log
             self.logger.error(log)
         else:
             log = const.STR_COMMAND + event.cmd_name + const.STR_INVOKE_SUCCESS
-            device._read_activity_message = log
+            device_data._read_activity_message = log
             self.logger.info(log)
 
 
@@ -84,19 +86,21 @@ class ReleaseAllResources(BaseCommand):
 
         :raises: DevFailed if the command execution is not successful.
         """
-        device = self.target
+        device_data = self.target
         try:
             # Call SDP Subarray Command asynchronously
-            device._sdp_subarray_proxy.command_inout_asynch(const.CMD_RELEASE_RESOURCES,
-                                                            self.releaseallresources_cmd_ended_cb)
+            sdp_sa_ln_client = TangoClient(device_data._sdp_sa_fqdn)
+            sdp_sa_ln_client_obj.send_command_async(const.CMD_RELEASE_RESOURCES, None, self.releaseallresources_cmd_ended_cb)
+            # device._sdp_subarray_proxy.command_inout_asynch(const.CMD_RELEASE_RESOURCES,
+            #                                                 self.releaseallresources_cmd_ended_cb)
             # Update the status of command execution status in activity message
-            device._read_activity_message = const.STR_REL_RESOURCES
+            device_data._read_activity_message = const.STR_REL_RESOURCES
             self.logger.info(const.STR_REL_RESOURCES)
 
         except DevFailed as dev_failed:
             log_msg = const.ERR_RELEASE_RESOURCES + str(dev_failed)
-            device._read_activity_message = log_msg
+            device_data._read_activity_message = log_msg
             self.logger.exception(dev_failed)
             tango.Except.throw_exception(const.STR_RELEASE_RES_EXEC, log_msg,
-                                            "SdpSubarrayLeafNode.ReleaseAllResourcesCommand()",
+                                            "SdpSubarrayLeafNode.ReleaseAllResources()",
                                             tango.ErrSeverity.ERR)
