272a3e11a3eb0ac4c4400afbc5294b35a73b5578
shraddhabajare <shraddhab.ska@gmail.com>
2020-12-30 10:01:04 +0530

    AT1-728: Resolve linting warnings for AssignResources command implementation.

--- tmcprototype/sdpsubarrayleafnode/src/sdpsubarrayleafnode/sdp_subarray_leaf_node.py
+++ tmcprototype/sdpsubarrayleafnode/src/sdpsubarrayleafnode/sdp_subarray_leaf_node.py
@@ -25,6 +25,9 @@ from ska.base.commands import ResultCode
 #from .transaction_id import identify_with_id
 from . import const, release, on_command, off_command, assign_resources_command, release_resources_command, configure_command, abort_command, restart_command, obsreset_command, scan_command, end_command, endscan_command
 from .device_data import DeviceData
+from tmc.common.tango_client import TangoClient
+from .exceptions import InvalidObsStateError
+
 
 # PROTECTED REGION END #    //  SdpSubarrayLeafNode.additionnal_import
 
