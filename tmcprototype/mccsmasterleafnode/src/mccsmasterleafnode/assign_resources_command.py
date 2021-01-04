d1bc7dc2758821a30fa8eefa27aa225e002b2255
shraddhabajare <shraddhab.ska@gmail.com>
2021-01-04 12:03:18 +0530

    AT1-738: Implement DeviceData class. Create separate modules for each command.

---
+++ tmcprototype/mccsmasterleafnode/src/mccsmasterleafnode/assign_resources_command.py
@@ -0,0 +1,139 @@
+# PROTECTED REGION ID(MccsMasterLeafNode.import) ENABLED START #
+# Third party imports
+# Tango imports
+import tango
+from tango import DeviceProxy, ApiUtil, DebugIt, DevState, AttrWriteType, DevFailed
+from tango.server import run, command, device_property, attribute
+
+# Additional import
+from ska.base import SKABaseDevice
+from ska.base.commands import BaseCommand
+from ska.base.control_model import HealthState, SimulationMode, TestMode
+from tmc.common.tango_client import TangoClient
+from . import const, release
+# PROTECTED REGION END #    //  MccsMasterLeafNode imports
+
+
+class AssignResourcesCommand(BaseCommand):
+    """
+    A class for MccsMasterLeafNode's AssignResources() command.
+    """
+
+    def check_allowed(self):
+        """
+        Checks whether the command is allowed to be run in the current state
+
+        :return: True if this command is allowed to be run in current device state
+
+        :rtype: boolean
+
+        :raises: DevFailed if this command is not allowed to be run
+            in current device state
+
+        """
+        if self.state_model.op_state in [DevState.FAULT, DevState.UNKNOWN, DevState.DISABLE]:
+            tango.Except.throw_exception("AssignResources() is not allowed in current state",
+                                            "Failed to invoke AssignResources command on "
+                                            "mccsmasterleafnode.",
+                                            "mccsmasterleafnode.AssignResources()",
+                                            tango.ErrSeverity.ERR)
+        return True
+
+    def allocate_ended(self, event):
+        """
+        Callback function immediately executed when the asynchronous invoked
+        command returns.
+
+        :param event: a CmdDoneEvent object. This class is used to pass data
+            to the callback method in asynchronous callback model for command
+            execution.
+        :type: CmdDoneEvent object
+            It has the following members:
+                - device     : (DeviceProxy) The DeviceProxy object on which the call was executed.
+                - cmd_name   : (str) The command name
+                - argout_raw : (DeviceData) The command argout
+                - argout     : The command argout
+                - err        : (bool) A boolean flag set to true if the command failed. False otherwise
+                - errors     : (sequence<DevError>) The error stack
+                - ext
+        :return: none
+
+        :raises: DevFailed if this command is not allowed to be run
+        in current device state
+
+        """
+        device = self.target
+        self.logger.info("Executing callback allocate_ended")
+        try:
+            if event.err:
+                device._read_activity_message = const.ERR_INVOKING_CMD + str(event.cmd_name) + "\n" + str(
+                    event.errors)
+                log = const.ERR_INVOKING_CMD + event.cmd_name
+                self.logger.error(log)
+            else:
+                log = const.STR_COMMAND + event.cmd_name + const.STR_INVOKE_SUCCESS
+                device._read_activity_message = log
+                self.logger.info(log)
+
+        except tango.DevFailed as df:
+            self.logger.exception(df)
+            tango.Except.re_throw_exception(df, "MCCS master gave an error response",
+                                            "MCCS master threw error in Allocate MCCS LMC_CommandFailed",
+                                            "Allocate", tango.ErrSeverity.ERR)
+
+    def do(self, argin):
+        """
+        It accepts stationiDList list, channels and stationBeamiDList in JSON string format and invokes allocate command on MccsMaster
+        with JSON string as an input argument.
+
+        :param argin:StringType. The string in JSON format.
+            
+        Example:
+        {
+            "subarray_id": 1,
+            "station_ids": [1,2],
+            "channels": [1,2,3,4,5,6,7,8],
+            "station_beam_ids": [1]
+        }
+
+        :return: None
+
+        Note: Enter the json string without spaces as an input.
+
+        :raises: ValueError if input argument json string contains invalid value
+                    KeyError if input argument json string contains invalid key
+                    DevFailed if the command execution is not successful
+        """
+        device = self.target
+        try:
+            log_msg = "Input JSON for MCCS master leaf node AssignResources command is: " + argin
+            self.logger.debug(log_msg)
+            self.logger.info("Invoking Allocate on MCCS master")
+            device._mccs_master_proxy.command_inout_asynch(const.CMD_ALLOCATE, argin,
+                                                    self.allocate_ended)
+            self.logger.info("After invoking Allocate on MCCS master")
+            device._read_activity_message = const.STR_ALLOCATE_SUCCESS
+            self.logger.info(const.STR_ALLOCATE_SUCCESS)
+
+        except ValueError as value_error:
+            log_msg = const.ERR_INVALID_JSON_ASSIGN_RES_MCCS + str(value_error)
+            device._read_activity_message = const.ERR_INVALID_JSON_ASSIGN_RES_MCCS + str(value_error)
+            self.logger.exception(value_error)
+            tango.Except.re_throw_exception(value_error, const.STR_ASSIGN_RES_EXEC, log_msg,
+                                            "MccsMasterLeafNode.AssignResourcesCommand",
+                                            tango.ErrSeverity.ERR)
+
+        except KeyError as key_error:
+            log_msg = const.ERR_JSON_KEY_NOT_FOUND + str(key_error)
+            device._read_activity_message = const.ERR_JSON_KEY_NOT_FOUND + str(key_error)
+            self.logger.exception(key_error)
+            tango.Except.re_throw_exception(key_error,const.STR_ASSIGN_RES_EXEC, log_msg,
+                                            "MccsMasterLeafNode.AssignResourcesCommand",
+                                            tango.ErrSeverity.ERR)
+        except DevFailed as dev_failed:
+            log_msg = const.ERR_ASSGN_RESOURCE_MCCS + str(dev_failed)
+            device._read_activity_message = log_msg
+            self.logger.exception(dev_failed)
+            tango.Except.re_throw_exception(dev_failed, const.STR_ASSIGN_RES_EXEC, log_msg,
+                                            "MccsMasterLeafNode.AssignResourcesCommand",
+                                            tango.ErrSeverity.ERR)
---
+++ tmcprototype/mccsmasterleafnode/src/mccsmasterleafnode/release_resources_command.py
@@ -0,0 +1,117 @@
+# PROTECTED REGION ID(MccsMasterLeafNode.import) ENABLED START #
+# Third party imports
+# Tango imports
+import tango
+from tango import DeviceProxy, ApiUtil, DebugIt, DevState, AttrWriteType, DevFailed
+from tango.server import run, command, device_property, attribute
+
+# Additional import
+from ska.base import SKABaseDevice
+from ska.base.commands import BaseCommand
+from ska.base.control_model import HealthState, SimulationMode, TestMode
+from tmc.common.tango_client import TangoClient
+from . import const, release
+# PROTECTED REGION END #    //  MccsMasterLeafNode imports
+
+
+class ReleaseResourcesCommand(BaseCommand):
+    """
+    A class for MccsMasterLeafNode's ReleaseResources() command.
+    """
+
+    def check_allowed(self):
+        """
+        Checks whether the command is allowed to be run in the current state
+
+        :return: True if this command is allowed to be run in current device state
+
+        :rtype: boolean
+
+        :raises: ValueError if input argument json string contains invalid value
+                DevFailed if this command is not allowed to be run in current device state
+
+        """
+        if self.state_model.op_state in [DevState.FAULT, DevState.UNKNOWN, DevState.DISABLE]:
+            tango.Except.throw_exception("ReleaseResources() is not allowed in current state",
+                                            "Failed to invoke ReleaseResources command on "
+                                            "mccsmasterleafnode.",
+                                            "mccsmasterleafnode.ReleaseResources()",
+                                            tango.ErrSeverity.ERR)
+        return True
+
+    def releaseresources_cmd_ended_cb(self, event):
+        """
+        Callback function immediately executed when the asynchronous invoked
+        command returns.
+
+        :param event: a CmdDoneEvent object. This class is used to pass data
+            to the callback method in asynchronous callback model for command
+            execution.
+        :type: CmdDoneEvent object
+            It has the following members:
+                - device     : (DeviceProxy) The DeviceProxy object on which the call was executed.
+                - cmd_name   : (str) The command name
+                - argout_raw : (DeviceData) The command argout
+                - argout     : The command argout
+                - err        : (bool) A boolean flag set to true if the command failed. False otherwise
+                - errors     : (sequence<DevError>) The error stack
+                - ext
+        :return: none
+
+        """
+        device = self.target
+        # Update logs and activity message attribute with received event
+        if event.err:
+            log_msg = const.ERR_INVOKING_CMD + str(event.cmd_name) + "\n" + str(event.errors)
+            self.logger.error(log_msg)
+            device._read_activity_message = log_msg
+        else:
+            log_msg = const.STR_COMMAND + str(event.cmd_name) + const.STR_INVOKE_SUCCESS
+            self.logger.info(log_msg)
+            device._read_activity_message = log_msg
+
+    def do(self, argin):
+        """
+        It invokes ReleaseResources command on MccsMaster and releases all the resources assigned to
+        MccsMaster.
+
+        :param argin:StringType. The string in JSON format.
+
+        Example: 
+            {
+                "subarray_id": 1,
+                "release_all": true,
+            }
+
+        :return: None.
+
+        :raises: DevFailed if the command execution is not successful
+
+
+        """
+        device = self.target
+        log_msg = "Input JSON for MCCS master leaf node Release command is: " + argin
+        self.logger.debug(log_msg)
+        self.logger.info("Invoking Release on MCCS master")
+
+        try:
+            device._mccs_master_proxy.command_inout_asynch(const.CMD_Release, argin,
+                                                    self.releaseresources_cmd_ended_cb)
+            device._read_activity_message = const.STR_REMOVE_ALL_RECEPTORS_SUCCESS
+            self.logger.info(const.STR_REMOVE_ALL_RECEPTORS_SUCCESS)
+
+        except ValueError as value_error:
+            log_msg = const.ERR_INVALID_JSON_RELEASE_RES_MCCS + str(value_error)
+            device._read_activity_message = const.ERR_INVALID_JSON_RELEASE_RES_MCCS + str(value_error)
+            self.logger.exception(value_error)
+            tango.Except.re_throw_exception(value_error, const.STR_RELEASE_RES_EXEC, log_msg,
+                                            "MccsMasterLeafNode.ReleaseResourcesCommand",
+                                            tango.ErrSeverity.ERR)
+        
+        except DevFailed as dev_failed:
+            log_msg = const.ERR_RELEASE_ALL_RESOURCES + str(dev_failed)
+            device._read_activity_message = log_msg
+            self.logger.exception(dev_failed)
+            tango.Except.re_throw_exception(dev_failed, const.STR_RELEASE_RES_EXEC, log_msg,
+                                            "MccsMasterLeafNode.ReleaseAllResourcesCommand",
+                                            tango.ErrSeverity.ERR)
