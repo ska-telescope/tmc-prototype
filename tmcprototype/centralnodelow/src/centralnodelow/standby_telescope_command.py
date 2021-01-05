affe0ad15a6a70859c47a59364cdf0a3b7778663
handeSnehal <snehalhande.ska@gmail.com>
2021-01-05 15:36:52 +0530

    AT1-736: Create separate files for each command and use ska-tmc-common package

---
+++ tmcprototype/centralnodelow/src/centralnodelow/standby_telescope_command.py
@@ -0,0 +1,67 @@
+class StandByTelescope(SKABaseDevice.OffCommand):
+    """
+    A class for Low CentralNode's StandByTelescope() command.
+    """
+
+    def check_allowed(self):
+
+        """
+        Checks whether this command is allowed to be run in current device state
+
+        :return: True if this command is allowed to be run in current device state
+
+        :rtype: boolean
+
+        :raises: DevFailed if this command is not allowed to be run in current device state
+        """
+        if self.state_model.op_state in [DevState.FAULT, DevState.UNKNOWN, DevState.DISABLE]:
+            tango.Except.throw_exception("Command StandByTelescope is not allowed in current state.",
+                                         "Failed to invoke StandByTelescope command on CentralNodeLow.",
+                                         "CentralNodeLow.StandByTelescope()",
+                                         tango.ErrSeverity.ERR)
+        return True
+
+    def do(self):
+        """
+        Sets the CentralNodeLow into OFF state. Invokes the respective command on lower level nodes and devices.
+
+        param argin: None.
+
+        :return: A tuple containing a return code and a string message indicating status.
+        The message is for information purpose only.
+
+        :rtype: (ResultCode, str)
+
+        :raises: DevFailed if error occurs while invoking command on any of the devices like SubarrayNode or
+                 MccsMasterLeafNode.
+
+        """
+        device = self.target
+        log_msg = const.STR_STANDBY_CMD_ISSUED
+        self.logger.info(log_msg)
+        device._read_activity_message = log_msg
+
+        try:
+            device._mccs_master_leaf_proxy.command_inout(const.CMD_OFF)
+            self.logger.info(const.STR_CMD_OFF_MCCSMLN_DEV)
+        except DevFailed as dev_failed:
+            log_msg = const.ERR_EXE_OFF_CMD + str(dev_failed)
+            self.logger.exception(dev_failed)
+            device._read_activity_message = const.ERR_EXE_OFF_CMD
+            tango.Except.throw_exception(const.STR_STANDBY_EXEC, log_msg,
+                                         "CentralNodeLow.StandByTelescopeCommand",
+                                         tango.ErrSeverity.ERR)
+
+        try:
+            for subarray_id in range(1, len(device.TMLowSubarrayNodes) + 1):
+                device.subarray_FQDN_dict[subarray_id].command_inout(const.CMD_OFF)
+                self.logger.info(const.STR_CMD_OFF_SA_LOW_DEV)
+
+        except DevFailed as dev_failed:
+            log_msg = const.ERR_EXE_OFF(dev_failed)
+            self.logger.exception(dev_failed)
+            device._read_activity_message = const.ERR_EXE_OFF_CMD
+            tango.Except.throw_exception(const.STR_STANDBY_EXEC, log_msg,
+                                         "CentralNodeLow.StandByTelescopeCommand",
+                                         tango.ErrSeverity.ERR)
+        return (ResultCode.OK, device._read_activity_message)
---
+++ tmcprototype/centralnodelow/src/centralnodelow/startup_telescope_command.py
@@ -0,0 +1,99 @@
+"""
+StartUpTelescope class for CentralNodelow.
+"""
+# Tango imports
+import tango
+from tango import DebugIt, AttrWriteType, DeviceProxy, EventType, DevState, DevFailed
+
+# Additional import
+from ska.base import SKABaseDevice
+from ska.base.commands import ResultCode, BaseCommand
+from ska.base.control_model import HealthState
+from . import const
+from centralnodelow.device_data import DeviceData
+from tmc.common.tango_client import TangoClient
+
+class StartUpTelescope(SKABaseDevice.OnCommand):
+    """
+    A class for Low CentralNode's StartupCommand() command.
+    """
+
+    def check_allowed(self):
+
+        """
+        Checks whether this command is allowed to be run in current device state
+
+        :return: True if this command is allowed to be run in current device state
+
+        :rtype: boolean
+
+        :raises: DevFailed if this command is not allowed to be run in current device state
+
+        """
+        if self.state_model.op_state in [DevState.FAULT, DevState.UNKNOWN, DevState.DISABLE]:
+            tango.Except.throw_exception("Command StartUpTelescope is not allowed in current state.",
+                                         "Failed to invoke StartUpTelescope command on CentralNodeLow.",
+                                         "CentralNodeLow.StartUpTelescope()",
+                                         tango.ErrSeverity.ERR)
+        return True
+
+    def do(self):
+        """
+        Setting the startup state to TRUE enables the telescope to accept subarray commands as per the subarray
+        model. Set the CentralNode into ON state.
+
+        :param argin: None.
+
+        :return: A tuple containing a return code and a string message indicating status.
+        The message is for information purpose only.
+
+        :rtype: (ResultCode, str)
+
+        :raises: DevFailed if error occurs while invoking command on any of the devices like SubarrayNode or
+                MccsMasterLeafNode.
+
+        """
+        device_data = self.target
+        log_msg = const.STR_ON_CMD_ISSUED
+        self.logger.info(log_msg)
+        device_data._read_activity_message = log_msg
+        self.startup_mccs(device_data.mccs_master_fqdn)
+        self.startup_subarray(device_data.subarray_low)
+
+
+    def startup_subarray(self, subarray_fqdn):
+        for subarray_id in range(1, len(subarray_fqdn) + 1):
+            subarray_client = TangoClient(subarray_id)
+            self.startup_leaf_node(subarray_client)
+
+
+    def startup_mccs(self, mccs_fqdn):
+        mccs_proxy = TangoClient(mccs_fqdn)
+        self.startup_leaf_node(mccs_proxy)
+
+
+    def startup_leaf_node(self, tango_client):
+        """
+        Invoke On command on leaf nodes.
+
+        :param tango_client: Proxy of corresponding node.
+
+        :return: None
+
+        :raises: Devfailed exception if error occures while  executing On command on leaf node.
+        """
+        device_data = DeviceData.get_instance()
+        try:
+            tango_client.send_command(const.CMD_ON)
+            log_msg = 'ON command invoked successfully on {}'.format(tango_client.get_device_fqdn)
+            self.logger.debug(log_msg)
+            device_data._read_activity_message = log_msg
+
+        except DevFailed as dev_failed:
+            log_msg = const.ERR_EXE_ON_CMD + str(dev_failed)
+            self.logger.exception(dev_failed)
+            device_data._read_activity_message = const.ERR_EXE_ON_CMD
+            tango.Except.re_throw_exception(dev_failed, const.STR_ON_EXEC, log_msg,
+                                            "CentralNodeLow.StartUpTelescopeCommand",
+                                            tango.ErrSeverity.ERR)
+        return (ResultCode.OK, device_data._read_activity_message)
