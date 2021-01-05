91027fb35dcc0669127e03df1d5f516598dba52a
Apurva Patkar <apurva.ska@gmail.com>
2021-01-05 10:42:11 +0000

    Resolve integration test cases issues

--- post-deployment/resources/test_support/controls.py
+++ post-deployment/resources/test_support/controls.py
@@ -28,6 +28,5 @@ def telescope_is_in_standby():
 
     return  [resource('ska_mid/tm_subarray_node/1').get("State"),
             resource('mid_csp/elt/subarray_01').get("State"),
-            resource('mid_csp_cbf/sub_elt/subarray_01').get("State"),
-            resource('mid_sdp/elt/subarray_1').get("State")] == \
-            ['OFF','OFF','OFF', 'OFF']
+            resource('mid_csp_cbf/sub_elt/subarray_01').get("State")]== \
+            ['OFF','OFF','OFF']
