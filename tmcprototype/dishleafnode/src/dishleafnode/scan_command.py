1113eb2905a3fc22558438d6aee1fc11efe773c5
Katleho Madisa <katleho.madisa47@gmail.com>
2021-01-12 16:45:41 +0200

    Test changes

--- tmcprototype/dishleafnode/src/dishleafnode/scan_command.py
+++ tmcprototype/dishleafnode/src/dishleafnode/scan_command.py
@@ -51,7 +51,8 @@ class Scan(BaseCommand):
 
         try:
             dish_client = TangoClient(device_data._dish_master_fqdn)
-            dish_client.send_command_async(command_name, cmd_ended_cb)
+            dish_client.deviceproxy.command_inout_asynch(command_name, cmd_ended_cb)
+            # dish_client.send_command_async(command_name, cmd_ended_cb)
             self.logger.info("'%s' command executed successfully.", command_name)
         except DevFailed as dev_failed:
             self.logger.exception(dev_failed)
