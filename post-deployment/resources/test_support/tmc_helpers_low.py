58245b0eba9c985e53a5b6ee70ffbe0fc784e043
Akash Karangale <akashk.ska@gmail.com>
2021-01-25 16:28:04 +0530

    AT1-748: Updated master in branch.

--- post-deployment/resources/test_support/tmc_helpers_low.py
+++ post-deployment/resources/test_support/tmc_helpers_low.py
@@ -1,5 +1,6 @@
 from resources.test_support.sync_decorators_low import sync_start_up_telescope, sync_assign_resources,\
-                    sync_scan, sync_configure, sync_end, sync_release_resources, sync_set_to_standby, time_it
+                    sync_scan, sync_configure, sync_end, sync_release_resources, sync_abort, \
+                    sync_obsreset,sync_set_to_standby, time_it
 from resources.test_support.logging_decorators import log_it
 from tango import DeviceProxy   
 from resources.test_support.helpers_low import waiter,watch,resource
