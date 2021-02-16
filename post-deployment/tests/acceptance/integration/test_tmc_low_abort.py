d27c199e0ba6e7eec84cdcfb4723263a5ab3d14b
SnehalNakave <snehal.ska@gmail.com>
2021-02-16 07:33:57 +0000

    mccs_testing: updated obsreset in tear down of testcase..

--- post-deployment/tests/acceptance/integration/test_tmc_low_abort.py
+++ post-deployment/tests/acceptance/integration/test_tmc_low_abort.py
@@ -61,8 +61,8 @@ def test_abort():
         fixture["state"] = "Subarray Aborted"
 
         # tear down
-        # TODO: Waiting for obsreset tobe implemented.
-        tmc.obsreset()
+        # TODO: Waiting for obsreset to be implemented.
+        tmc.ObsReset_sub()
         LOGGER.info("Obsreset is complete on Subarray")
         fixture["state"] = "Subarray IDLE"
         tmc.release_resources()
