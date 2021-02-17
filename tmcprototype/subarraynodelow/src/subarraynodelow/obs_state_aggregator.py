68fe179cf5b1fcd549c6b939b8f1db85addc2a30
Apurva Patkar <apurva.ska@gmail.com>
2021-02-17 13:19:07 +0000

    Update obs_state_aggregator on subarraynodelow

--- tmcprototype/subarraynodelow/src/subarraynodelow/obs_state_aggregator.py
+++ tmcprototype/subarraynodelow/src/subarraynodelow/obs_state_aggregator.py
@@ -74,11 +74,11 @@ class ObsStateAggregator:
         """
         try:
             if not evt.err:
-                event_observetion_state = evt.attr_value.value
+                event_observation_state = evt.attr_value.value
                 if const.PROP_DEF_VAL_TMMCCS_MID_SALN in evt.attr_name:
-                    mccs_obsState = ObsState(event_observetion_state)
-                    self._mccs_sa_obs_state = str(mccs_obsState.name)
-                    self._read_activity_message = f"{const.STR_MCCS_SUBARRAY_OBS_STATE}{event_observetion_state}"
+                    # Typacasted the event values to obsState ENUM labels.
+                    self._mccs_sa_obs_state = ObsState(event_observation_state)
+                    self._read_activity_message = f"{const.STR_MCCS_SUBARRAY_OBS_STATE}{event_observation_state}"
                 else:
                     self.logger.info(const.EVT_UNKNOWN)
                     self._read_activity_message = const.EVT_UNKNOWN
@@ -99,16 +99,13 @@ class ObsStateAggregator:
         """
         log_msg = f"MCCS ObsState is: {self._mccs_sa_obs_state}"
         self.logger.info(log_msg)
-        
-        # Check mccs subarray obsState if it is EMPTY
-        if self._mccs_sa_obs_state is "EMPTY":
+        if self._mccs_sa_obs_state == ObsState.EMPTY:
             if self.device_data.is_release_resources:
                 self.logger.info(
                     "Calling ReleaseAllResource command succeeded() method"
                 )
                 self.this_server.device.release.succeeded()
-        # Check mccs subarray obsState if it is READY
-        elif self._mccs_sa_obs_state is "READY":
+        elif self._mccs_sa_obs_state == ObsState.READY:
             if self.device_data.is_scan_completed:
                 self.logger.info("Calling EndScan command succeeded() method")
                 self.this_server.device.endscan.succeeded()
@@ -116,8 +113,7 @@ class ObsStateAggregator:
                 # Configure command success
                 self.logger.info("Calling Configure command succeeded() method")
                 self.this_server.device.configure.succeeded()
-        # Check mccs subarray obsState if it is IDLE
-        elif self._mccs_sa_obs_state is "IDLE":
+        elif self._mccs_sa_obs_state == ObsState.IDLE:
             if self.device_data.is_end_command:
                 # End command success
                 self.logger.info("Calling End command succeeded() method")
@@ -130,8 +126,7 @@ class ObsStateAggregator:
                 # Assign Resource command success
                 self.logger.info("Calling AssignResource command succeeded() method")
                 self.this_server.device.assign.succeeded()
-        # Check mccs subarray obsState if it is ABORTED
-        elif self._mccs_sa_obs_state is "ABORTED":
+        elif self._mccs_sa_obs_state == ObsState.ABORTED:
             if self.device_data.is_abort_command_executed:
                 # Abort command success
                 self.logger.info("Calling Abort command succeeded() method")
