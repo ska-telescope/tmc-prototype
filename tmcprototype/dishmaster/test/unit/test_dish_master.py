0703b4f9a299dae74ae51a943f86d5ab6b0009ad
Katleho Madisa <katleho.madisa47@gmail.com>
2021-03-01 08:26:32 +0000

    Revert "Improve debugging"
    This reverts commit 5beee6786ab3cd1438afcca64c4a3264aa69e10f.

--- tmcprototype/dishmaster/test/unit/test_dish_master.py
+++ tmcprototype/dishmaster/test/unit/test_dish_master.py
@@ -4,8 +4,9 @@ from tango import DevState
 
 class TestDishMaster(object):
     def test_device_state(self, tango_context):
-        assert tango_context.device.State() == DevState.ON
+        assert tango_context.device.State() == DevState.STANDBY
 
     def test_device_mode_transition(self, tango_context):
         tango_context.device.SetStandbyFPMode()
         assert str(tango_context.device.dishMode) == "dishMode.STANDBY_FP"
+        assert tango_context.device.State() == DevState.STANDBY
