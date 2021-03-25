42e4ae3ad233e61336b086fa1bda57854c89f482
shraddhabajare <shraddhab.ska@gmail.com>
2021-03-25 18:24:55 +0530

    AT1-820 - Update branch with master

--- tmcprototype/cspsubarrayleafnode/src/cspsubarrayleafnode/end_scan_command.py
+++ tmcprototype/cspsubarrayleafnode/src/cspsubarrayleafnode/end_scan_command.py
@@ -6,6 +6,7 @@ from tango import DevState, DevFailed
 from ska.base.commands import BaseCommand
 
 from tmc.common.tango_client import TangoClient
+from tmc.common.tango_server_helper import TangoServerHelper
 
 from . import const
 
@@ -72,16 +73,16 @@ class EndScanCommand(BaseCommand):
 
         :return: none
         """
-        device_data = self.target
+        this_server = TangoServerHelper.get_instance()
         # Update logs and activity message attribute with received event
         if event.err:
             log_msg = f"{const.ERR_INVOKING_CMD}{event.cmd_name}\n{event.errors}"
             self.logger.error(log_msg)
-            device_data._read_activity_message = log_msg
+            this_server.write_attr("activityMessage", log_msg)
         else:
             log_msg = f"{const.STR_COMMAND}{event.cmd_name}{const.STR_INVOKE_SUCCESS}"
             self.logger.info(log_msg)
-            device_data._read_activity_message = log_msg
+            this_server.write_attr("activityMessage", log_msg)
 
     def do(self):
         """
@@ -94,18 +95,21 @@ class EndScanCommand(BaseCommand):
             DevFailed if the command execution is not successful
 
         """
-        device_data = self.target
         try:
-            csp_sub_client_obj = TangoClient(device_data.csp_subarray_fqdn)
+            this_server = TangoServerHelper.get_instance()
+            csp_subarray_fqdn = ""
+            property_val = this_server.read_property("CspSubarrayFQDN")
+            csp_subarray_fqdn = csp_subarray_fqdn.join(property_val)
+            csp_sub_client_obj = TangoClient(csp_subarray_fqdn)
             csp_sub_client_obj.send_command_async(
                 const.CMD_ENDSCAN, None, self.endscan_cmd_ended_cb
             )
-            device_data._read_activity_message = const.STR_ENDSCAN_SUCCESS
+            this_server.write_attr("activityMessage", const.STR_ENDSCAN_SUCCESS)
             self.logger.info(const.STR_ENDSCAN_SUCCESS)
 
         except DevFailed as dev_failed:
             log_msg = f"{const.ERR_ENDSCAN_INVOKING_CMD}{dev_failed}"
-            device_data._read_activity_message = log_msg
+            this_server.write_attr("activityMessage", log_msg)
             self.logger.exception(dev_failed)
             tango.Except.throw_exception(
                 const.STR_ENDSCAN_EXEC,
