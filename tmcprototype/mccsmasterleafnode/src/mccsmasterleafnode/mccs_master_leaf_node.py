7f7b935788a398b3181d09e312111edec274f5b3
user <jayant.ska@gmail.com>
2021-03-25 17:16:17 +0530

    AT1-821-Resolve review comments

--- tmcprototype/mccsmasterleafnode/src/mccsmasterleafnode/mccs_master_leaf_node.py
+++ tmcprototype/mccsmasterleafnode/src/mccsmasterleafnode/mccs_master_leaf_node.py
@@ -130,9 +130,8 @@ class MccsMasterLeafNode(SKABaseDevice):
             log_msg = f"{const.STR_SETTING_CB_MODEL}{ApiUtil.instance().get_asynch_cb_sub_model()}"
             self.logger.debug(log_msg)
             self.this_server.write_attr("activityMessage", const.STR_INIT_SUCCESS)
-            return_message = self.this_server.read_attr("activityMessage")
-            self.logger.info(return_message)
-            return (ResultCode.OK, return_message)
+            self.logger.info(const.STR_INIT_SUCCESS)
+            return (ResultCode.OK, const.STR_INIT_SUCCESS)
 
     def always_executed_hook(self):
         # PROTECTED REGION ID(MccsMasterLeafNode.always_executed_hook) ENABLED START #
