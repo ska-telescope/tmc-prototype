2caa4b00f491f196b8447c972b4c6b335e278f09
user <jayant.ska@gmail.com>
2021-04-12 16:50:44 +0530

    AT1-835 - Resolve review comments

--- tmcprototype/subarraynodelow/src/subarraynodelow/assigned_resources_maintainer.py
+++ tmcprototype/subarraynodelow/src/subarraynodelow/assigned_resources_maintainer.py
@@ -23,9 +23,7 @@ class AssignedResourcesMaintainer:
         self.mccs_ln_asigned_res_event_id = {}
         self.this_server = TangoServerHelper.get_instance()
         self.device_data = DeviceData.get_instance()
-        mccs_subarray_ln_fqdn = ""
-        property_val = self.this_server.read_property("MccsSubarrayLNFQDN")
-        mccs_subarray_ln_fqdn = mccs_subarray_ln_fqdn.join(property_val)
+        mccs_subarray_ln_fqdn = self.this_server.read_property("MccsSubarrayLNFQDN")[0]
         self.mccs_client = TangoClient(mccs_subarray_ln_fqdn)
 
     def subscribe(self):
@@ -43,7 +41,7 @@ class AssignedResourcesMaintainer:
         """
         Receives the subscribed assigned_resources attribute value.
 
-        :param evt: A event on MCCS Subarray assigned_resources attribute.
+        :param evt: Tango event on MCCS Subarray assigned_resources attribute.
 
         :type: Event object
             It has the following members:
@@ -63,14 +61,14 @@ class AssignedResourcesMaintainer:
         :return: None
         """
         device_name = event.device.dev_name()
-        # log_msg = "Event on assigned_resources attribute is: " + str(event)
-        # self.logger.debug(log_msg)
+        log_msg = "Event on assigned_resources attribute is: " + str(event)
+        self.logger.debug(log_msg)
         if not event.err:
-            event_assigned_resources = event.attr_value.value
-            self.device_data.assigned_resources_maintainer = event_assigned_resources
-            self.update_assigned_resources_attribute(event_assigned_resources)
+            assigned_resources = event.attr_value.value
+            self.device_data.assigned_resources_maintainer = assigned_resources
+            self.update_assigned_resources_attribute(assigned_resources)
             self.logger.info("assigned_resources attribute subscribed successfully.")
-            log_msg = "MccsSubarray.assigned_resources attribute value is: " + str(event_assigned_resources)
+            log_msg = "MccsSubarray.assigned_resources attribute value is: " + str(assigned_resources)
             self.logger.info(log_msg)
         else:
             log_message = f"{const.ERR_SUBSR_MCCSSA_ASSIGNED_RES_ATTR}{device_name}{event}"
@@ -79,15 +77,14 @@ class AssignedResourcesMaintainer:
 
     def update_assigned_resources_attribute(self, mccs_assigned_resources):
         """
-        This method modifies assigned_resources value coming from MCCS Subarray and assignes to
-         SubarrayNode.assigned_resources attribute.
+        This method updates the SubarrayNode.assigned_resources attribute.
         """
         json_argument = json.loads(mccs_assigned_resources)
         del json_argument["interface"]
         assigned_resources_dict = {"interface": "https://schema.skatelescope.org/ska-low-tmc-assignedresources/1.0",
                                    "mccs": json_argument}
         assigned_resources_attr_value = json.dumps(assigned_resources_dict)
-        self.this_server.write_attr("assigned_resources", assigned_resources_attr_value, False)
+        self.this_server.write_attr("assigned_resources", assigned_resources_attr_value)
         log_msg = "assigned_resources attribute value is: " + str(assigned_resources_attr_value)
         self.logger.info(log_msg)
 
