b7a8423e5b308373b91252c6c9d2cc9f83214508
Payal Gupta <payal.ska@gmail.com>
2021-04-12 09:42:38 +0000

    AT1-839- Add start_time in json string on Subarraynodelow and remove inapplicable test case.

--- tmcprototype/subarraynodelow/src/subarraynodelow/scan_command.py
+++ tmcprototype/subarraynodelow/src/subarraynodelow/scan_command.py
@@ -37,7 +37,6 @@ class Scan(SKASubarray.ScanCommand):
 
         JSON string example as follows:
 
-        # {"mccs":{"id":1,"scan_time":0.0}}
         {"interface":"https://schema.skatelescope.org/ska-low-tmc-scan/1.0","scan_id":1}       
         Note: Above JSON string can be used as an input argument while invoking this command from JIVE.
 
@@ -58,9 +57,7 @@ class Scan(SKASubarray.ScanCommand):
         device_data.is_obsreset_command_executed = False
         this_server = TangoServerHelper.get_instance()
         try:
-            input_scan = json.loads(argin)
-            print("ARGIN INPUT SCAN-----" ,input_scan )
-            # mccs_input_scan = input_scan["mccs"]
+            input_to_mccs = self.update_json(argin)
             log_msg = f"{const.STR_SCAN_IP_ARG}{argin}"
             self.logger.info(log_msg)
             this_server.write_attr("activityMessage", log_msg, False)
@@ -71,7 +68,7 @@ class Scan(SKASubarray.ScanCommand):
             mccs_subarray_ln_fqdn = mccs_subarray_ln_fqdn.join(property_val)
             mccs_subarray_ln_client = TangoClient(mccs_subarray_ln_fqdn)
             mccs_subarray_ln_client.send_command(
-                const.CMD_SCAN, json.dumps(input_scan)
+                const.CMD_SCAN, json.dumps(input_to_mccs)
             )
             self.logger.info(const.STR_MCCS_SCAN_INIT)
             this_server.write_attr("activityMessage", const.STR_MCCS_SCAN_INIT, False)
@@ -115,3 +112,10 @@ class Scan(SKASubarray.ScanCommand):
                 "SubarrayNode.ScanCommand",
                 tango.ErrSeverity.ERR,
             )
+
+    def update_json(self, argin):
+        input_scan = json.loads(argin)
+        input_scan = {"interface":"https://schema.skatelescope.org/ska-low-mccs-scan/1.0","scan_id":1} 
+        start_time = {"start_time":0.0}
+        input_scan.update(start_time)
+        return input_scan
