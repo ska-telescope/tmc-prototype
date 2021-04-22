76c33cd4efb2d6fad5dbbe2490030794363f377d
DigvijayMali92 <digvijay.ska@gmail.com>
2021-04-22 13:37:13 +0000

    AT1-842-Update Dish Integration test case

--- ska-tmc/dishleafnode/test/integration/test_dish_leaf_node.py
+++ ska-tmc/dishleafnode/test/integration/test_dish_leaf_node.py
@@ -195,7 +195,7 @@ class TestDishLeafNode:
 
     def test_SetStowMode(self, dish_leaf_node_dp, dish_master_dp):
         dish_leaf_node_dp.SetStowMode()
-        time.sleep(0.2)
+        # time.sleep(0.2)
         assert dish_master_dp.dishmode.name == "STOW"
 
     def test_Slew(self, dish_leaf_node_dp, dish_master_dp):
@@ -209,7 +209,7 @@ class TestDishLeafNode:
         dish_leaf_node_dp.Track(input_string)
         assert dish_master_dp.pointingState == PointingState.TRACK
         dish_leaf_node_dp.StopTrack()
-        time.sleep(0.2)
+        # time.sleep(0.2)
         assert dish_master_dp.pointingState == PointingState.READY
 
     def test_Restart(self, dish_leaf_node_dp, dish_master_dp):
