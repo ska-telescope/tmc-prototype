2870cf875eec1d5be5678106ceec66be192aa1e3
apurvapatkar <apurva.ska@gmail.com>
2021-04-23 17:02:29 +0530

    AT1-859-Test the functionality.

--- ska-tmc/dishleafnode/setup.py
+++ ska-tmc/dishleafnode/setup.py
@@ -45,6 +45,7 @@ setup(
         "pytango>=9.3.2",
         "mock",
         "astropy>=4.1",
+        "katpoint==1.0a1",
         "ska_logging>=0.3.0",
         "lmcbaseclasses>=0.7.2",
         "skatmccommon==0.1.6+3aaa7bbe",
