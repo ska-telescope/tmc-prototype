1d4413dba8b390b041bbad8e8dd2e342f5ad5e7c
Digvijay Mali <digvijay.ska@gmail.com>
2021-05-05 15:20:30 +0530

    AT1-882: Comment health state call from startup command.

--- ska-tmc/centralnodelow/src/centralnodelow/startup_telescope_command.py
+++ ska-tmc/centralnodelow/src/centralnodelow/startup_telescope_command.py
@@ -76,8 +76,8 @@ class StartUpTelescope(SKABaseDevice.OnCommand):
         cmd_res_subscriber_unsubscriber_obj = CommandResultFetcher()
         cmd_res_subscriber_unsubscriber_obj._subscribe_cmd_res_attribute_events(
             attributes_to_subscribe_to)
-        device_data.health_aggreegator = HealthStateAggreegator(self.logger)
-        device_data.health_aggreegator.subscribe_event()
+        # device_data.health_aggreegator = HealthStateAggreegator(self.logger)
+        # device_data.health_aggreegator.subscribe_event()
         try:
             self.this_server = TangoServerHelper.get_instance()
             # Check if Mccs Off command is completed
