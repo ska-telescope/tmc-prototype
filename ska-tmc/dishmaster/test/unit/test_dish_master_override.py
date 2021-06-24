5ae66eefb655eeb64487823c0785c7e968f2f2bc
samueltwum1 <samueltwum1@gmail.com>
2021-06-24 14:59:27 +0200

    SAR-244 Add unit test for track command - Update existing test to use pytest - Add new test for update track behaviour
    Co-authored-by: kmadisa<kmadisa47@gmail.com>

--- ska-tmc/dishmaster/test/unit/test_dish_master_override.py
+++ ska-tmc/dishmaster/test/unit/test_dish_master_override.py
@@ -8,24 +8,23 @@ import pkg_resources
 import time
 import pytest
 
+from assertpy import assert_that
+
 from tango_simlib import tango_sim_generator
-from dishmaster.dish_master_behaviour import OverrideDish
+from dishmaster.dish_master_behaviour import OverrideDish, set_enum, get_enum_str
 
 FGO_FILE_PATH = pkg_resources.resource_filename("dishmaster", "dish_master.fgo")
 JSON_FILE_PATH = pkg_resources.resource_filename("dishmaster", "dish_master_SimDD.json")
 
 
 class TestMpiDshModel:
-    device_model = None
-    dish_override = None
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture(scope="function")
     def provision_setup(self):
         model = tango_sim_generator.configure_device_models(
             [FGO_FILE_PATH, JSON_FILE_PATH], "test/nodb/mpidish"
         )
-        self.device_model = model["test/nodb/mpidish"]
-        self.dish_override = OverrideDish()
+        return model["test/nodb/mpidish"], OverrideDish()
 
 
     def test_update_desired_pointing_history(self, provision_setup):
@@ -33,9 +32,10 @@ class TestMpiDshModel:
         applied correctly
         """
         # Note: coords are are sets of 3: [timestamp, azim, elev]
+        device_model, dish_override = provision_setup
         now = time.time()
         now_millisec = now * 1000.0
-        self.dish_override.desired_pointings = [[now_millisec + 10.0, 2.0, 3.0]]
+        dish_override.desired_pointings = [[now_millisec + 10.0, 2.0, 3.0]]
         desired_pointing_coordinates = [now_millisec + 40.0, 5.0, 6.0]
         program_track_table_coordinates = [
             now_millisec + 70.0,
@@ -47,54 +47,54 @@ class TestMpiDshModel:
         ]
 
         # desiredPointing is newest, so must be used
-        self.dish_override.last_coordinate_update_timestamp = now - 5.0
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        dish_override.last_coordinate_update_timestamp = now - 5.0
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now - 3.0
         )
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now - 2.0
         )
-        current_pointings = list(self.dish_override.desired_pointings)
-        self.dish_override.update_desired_pointing_history(self.device_model)
+        current_pointings = list(dish_override.desired_pointings)
+        dish_override.update_desired_pointing_history(device_model)
         expected_pointings = current_pointings + [desired_pointing_coordinates]
-        self.assertEqual(self.dish_override.desired_pointings, expected_pointings)
+        assert_that(dish_override.desired_pointings).is_equal_to(expected_pointings)
 
         # programTrackTable is newest, so must be used
-        self.dish_override.last_coordinate_update_timestamp = now - 5.0
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        dish_override.last_coordinate_update_timestamp = now - 5.0
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now - 3.0
         )
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now - 2.0
         )
-        current_pointings = list(self.dish_override.desired_pointings)
-        self.dish_override.update_desired_pointing_history(self.device_model)
+        current_pointings = list(dish_override.desired_pointings)
+        dish_override.update_desired_pointing_history(device_model)
         expected_pointings = (
             current_pointings
             + [program_track_table_coordinates[0:3]]
             + [program_track_table_coordinates[3:6]]
         )
-        self.assertEqual(self.dish_override.desired_pointings, expected_pointings)
+        assert_that(dish_override.desired_pointings).is_equal_to(expected_pointings)
 
         # Neither is newest, so no update expected
-        current_pointings = list(self.dish_override.desired_pointings)
-        self.dish_override.last_coordinate_update_timestamp = now
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        current_pointings = list(dish_override.desired_pointings)
+        dish_override.last_coordinate_update_timestamp = now
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now - 2.0
         )
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now - 3.0
         )
-        self.dish_override.update_desired_pointing_history(self.device_model)
-        self.assertEqual(self.dish_override.desired_pointings, current_pointings)
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        dish_override.update_desired_pointing_history(device_model)
+        assert_that(dish_override.desired_pointings).is_equal_to(current_pointings)
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now - 3.0
         )
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now - 2.0
         )
-        self.dish_override.update_desired_pointing_history(self.device_model)
-        self.assertEqual(self.dish_override.desired_pointings, current_pointings)
+        dish_override.update_desired_pointing_history(device_model)
+        assert_that(dish_override.desired_pointings).is_equal_to(current_pointings)
 
         # New updates, but timestamps in the past, so no update expected
         desired_pointing_coordinates = [now_millisec - 40.0, 5.0, 6.0]
@@ -107,26 +107,44 @@ class TestMpiDshModel:
             11.0,
         ]
 
-        self.dish_override.last_coordinate_update_timestamp = now - 10
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        dish_override.last_coordinate_update_timestamp = now - 10
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now
         )
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now - 1.0
         )
-        self.dish_override.update_desired_pointing_history(self.device_model)
-        self.assertEqual(self.dish_override.desired_pointings, current_pointings)
+        dish_override.update_desired_pointing_history(device_model)
+        assert_that(dish_override.desired_pointings).is_equal_to(current_pointings)
 
-        self.dish_override.last_coordinate_update_timestamp = now - 10
-        self.device_model.sim_quantities["desiredPointing"].set_val(
+        dish_override.last_coordinate_update_timestamp = now - 10
+        device_model.sim_quantities["desiredPointing"].set_val(
             desired_pointing_coordinates, now - 1.0
         )
-        self.device_model.sim_quantities["programTrackTable"].set_val(
+        device_model.sim_quantities["programTrackTable"].set_val(
             program_track_table_coordinates, now
         )
-        self.dish_override.update_desired_pointing_history(self.device_model)
-        self.assertEqual(self.dish_override.desired_pointings, current_pointings)
+        dish_override.update_desired_pointing_history(device_model)
+        assert_that(dish_override.desired_pointings).is_equal_to(current_pointings)
 
     def test_pointing_state_reports_track_when_on_target(self, provision_setup):
-        pass
-    
+        device_model, dish_override = provision_setup
+        now = time.time()
+        timestamp_ms = now * 1000.0
+
+        # requested new pointing position
+        desired_pointing_coordinates = [timestamp_ms + 40.0, 10, 40]
+        device_model.sim_quantities["desiredPointing"].set_val(
+            desired_pointing_coordinates, now)
+        # ensure dish is in allowed mode before requesting track and check pointing state is SLEW
+        set_enum(device_model.sim_quantities["dishMode"], "OPERATE", now)
+        dish_override.action_track(device_model)
+        current_pointing_state = get_enum_str(device_model.sim_quantities["pointingState"])
+        assert_that(current_pointing_state).is_equal_to("SLEW")
+
+        # move the dish to the desired position and check that pointing state is TRACK
+        device_model.sim_quantities["achievedPointing"].set_val(
+            desired_pointing_coordinates, now)
+        dish_override.update_movement_attributes(device_model, now)
+        current_pointing_state = get_enum_str(device_model.sim_quantities["pointingState"])
+        assert_that(current_pointing_state).is_equal_to("TRACK")
