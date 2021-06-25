a07aab3a3ee2e581b9e08375bfeefdefad1c16ed
samueltwum1 <samueltwum1@gmail.com>
2021-06-25 14:25:44 +0200

    SAR-244 Use targetLock attribute to determine pointingState update

--- ska-tmc/dishmaster/src/dishmaster/dish_master_behaviour.py
+++ ska-tmc/dishmaster/src/dishmaster/dish_master_behaviour.py
@@ -608,7 +608,7 @@ class OverrideDish(object):
     def update_movement_attributes(self, model, sim_time):
         self.set_lock_attribute(model, self.is_on_target())
         self.set_achieved_pointing_attribute(model, sim_time, self.actual_position)
-        self.set_track_pointing_state_on_target(model, self.is_on_target())
+        self.set_track_pointing_state_on_target(model)
 
     @staticmethod
     def set_lock_attribute(model, target_reached):
@@ -617,10 +617,11 @@ class OverrideDish(object):
             target_lock.last_val = target_reached
             model.logger.info("Attribute 'targetLock' set to %s.", target_reached)
 
-    def set_track_pointing_state_on_target(self, model, target_reached):
+    def set_track_pointing_state_on_target(self, model):
         pointing_state = get_enum_str(model.sim_quantities["pointingState"])
+        target_lock = model.sim_quantities["targetLock"].last_val
         # update the pointing state to TRACK when the dish arrives on target
-        if target_reached == True and pointing_state == "SLEW":
+        if target_lock == True and pointing_state == "SLEW":
             self._change_pointing_state(model, "TRACK", ("OPERATE",))
 
     def set_achieved_pointing_attribute(self, model, sim_time, position):
