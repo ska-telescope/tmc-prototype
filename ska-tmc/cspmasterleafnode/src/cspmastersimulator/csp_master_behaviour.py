6cc11567fb1c4c621b8d7a9692b36c99a55f611e
rushikesh.ska <rushikesh.ska@gmail.com>
2021-07-15 12:37:24 +0000

    AT1-952-Update csp_master_behaviour.py file

--- ska-tmc/cspmasterleafnode/src/cspmastersimulator/csp_master_behaviour.py
+++ ska-tmc/cspmasterleafnode/src/cspmastersimulator/csp_master_behaviour.py
@@ -16,20 +16,49 @@ class OverrideCspMaster(object):
     def action_on(self, models, tango_dev=None, data_input=None):
         """Changes the State of the device to ON.
         """
-        tango_dev.set_state(DevState.ON)
-        print("******On Invoked successfully******",str(tango_dev.state()))
+        on = "ON"
+        _allowed_modes = (
+            "OFF",
+            "STANDBY"
+        )
+        if str(tango_dev.get_state()) in ["ON"]:
+            print("CSP master is already in '%s' state", on)
+            return
 
+        if str(tango_dev.get_state()) in _allowed_modes:
+            tango_dev.set_state(DevState.ON)
+            print("Csp Master transitioned to the '%s' state.", on)
 
     def action_off(self, models, tango_dev=None, data_input=None):
         """Changes the State of the device to OFF.
         """
-        tango_dev.set_state(DevState.OFF)
-        print("******OFF Invoked successfully******",str(tango_dev.state()))
+        off = "OFF"
+        _allowed_modes = (
+            "ON",
+            "ALARM"
+            "STANDBY"
+        )
+        if str(tango_dev.get_state()) in ["OFF"]:
+            print("CSP master is already in '%s' state", off)
+            return
+
+        if str(tango_dev.get_state()) in _allowed_modes:
+            tango_dev.set_state(DevState.OFF)
+            print("Csp Master transitioned to the '%s' state.", off)
 
     def action_standby(self, models, tango_dev=None, data_input=None):
         """Changes the State of the device to STANDBY.
         """
-        tango_dev.set_state(DevState.OFF)
-        print("******Standby Invoked successfully*****",str(tango_dev.state()))
+        standby = "STANDBY"
+        _allowed_modes = (
+            "ON",
+            "ALARM"
+            "OFF"
+        )
+        if str(tango_dev.get_state()) in ["STANDBY"]:
+            print("CSP master is already in '%s' state", standby)
+            return
 
-    
+        if str(tango_dev.get_state()) in _allowed_modes:
+            tango_dev.set_state(DevState.STANDBY)
+            print("Csp Master transitioned to the '%s' state.", standby)
