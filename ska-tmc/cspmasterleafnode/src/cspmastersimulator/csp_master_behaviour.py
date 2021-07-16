4c4234fd584c86ce5302472c5746f2957e516a35
rushikesh.ska <rushikesh.ska@gmail.com>
2021-07-16 12:06:44 +0000

    AT1-952-Update csp_master_behaviour.py file

--- ska-tmc/cspmasterleafnode/src/cspmastersimulator/csp_master_behaviour.py
+++ ska-tmc/cspmasterleafnode/src/cspmastersimulator/csp_master_behaviour.py
@@ -17,48 +17,140 @@ class OverrideCspMaster(object):
         """Changes the State of the device to ON.
         """
         on = "ON"
+        ok = "OK"
         _allowed_modes = (
             "OFF",
             "STANDBY"
         )
         if str(tango_dev.get_state()) in ["ON"]:
-            print("CSP master is already in '%s' state", on)
+            print(f"CSP master is already in {on} state")
             return
 
         if str(tango_dev.get_state()) in _allowed_modes:
             tango_dev.set_state(DevState.ON)
-            print("Csp Master transitioned to the '%s' state.", on)
+            print (f"Csp Master transitioned to the {on} state.")
+            csp_mode_healthState = models.sim_quantities["healthState"]
+            set_enum(csp_mode_healthState, ok, models.time_func())
+            print(f"heathState transitioned to {ok} state")
+
+            csp_mode_cspCbfHealthState = models.sim_quantities["cspCbfHealthState"]
+            set_enum(csp_mode_cspCbfHealthState, ok, models.time_func())
+            print(f"cspCbfHealthState transitioned to {ok} state")
+
+            csp_mode_cspPssHealthState = models.sim_quantities["cspPssHealthState"]
+            set_enum(csp_mode_cspPssHealthState, ok, models.time_func())
+            print(f"cspPssHealthState transitioned to {ok} state")
+
+            csp_mode_cspPstHealthState = models.sim_quantities["cspPstHealthState"]
+            set_enum(csp_mode_cspPstHealthState, ok, models.time_func())
+            print(f"cspPstHealthState transitioned to {ok} state")
+        else:
+            Except.throw_exception(
+                    "ON Command Failed",
+                    "Not allowed",
+                    ErrSeverity.WARN,
+                )
 
     def action_off(self, models, tango_dev=None, data_input=None):
         """Changes the State of the device to OFF.
         """
         off = "OFF"
+        ok = "OK"
         _allowed_modes = (
             "ON",
-            "ALARM"
+            "ALARM",
             "STANDBY"
         )
         if str(tango_dev.get_state()) in ["OFF"]:
-            print("CSP master is already in '%s' state", off)
+            print(f"CSP master is already in {off} state")
             return
 
         if str(tango_dev.get_state()) in _allowed_modes:
             tango_dev.set_state(DevState.OFF)
-            print("Csp Master transitioned to the '%s' state.", off)
+            print (f"Csp Master transitioned to the {off} state.")
+            csp_mode_healthState = models.sim_quantities["healthState"]
+            set_enum(csp_mode_healthState, ok, models.time_func())
+            print(f"heathState transitioned to {ok} state")
+
+            csp_mode_cspCbfHealthState = models.sim_quantities["cspCbfHealthState"]
+            set_enum(csp_mode_cspCbfHealthState, ok, models.time_func())
+            print(f"cspCbfHealthState transitioned to {ok} state")
+
+            csp_mode_cspPssHealthState = models.sim_quantities["cspPssHealthState"]
+            set_enum(csp_mode_cspPssHealthState, ok, models.time_func())
+            print(f"cspPssHealthState transitioned to {ok} state")
+
+            csp_mode_cspPstHealthState = models.sim_quantities["cspPstHealthState"]
+            set_enum(csp_mode_cspPstHealthState, ok, models.time_func())
+            print(f"cspPstHealthState transitioned to {ok} state")
+            
+        else:
+            Except.throw_exception(
+                    "Off Command Failed",
+                    "Not allowed",
+                    ErrSeverity.WARN,
+                )
 
     def action_standby(self, models, tango_dev=None, data_input=None):
         """Changes the State of the device to STANDBY.
         """
         standby = "STANDBY"
+        ok = "OK"
         _allowed_modes = (
             "ON",
-            "ALARM"
+            "ALARM",
             "OFF"
         )
         if str(tango_dev.get_state()) in ["STANDBY"]:
-            print("CSP master is already in '%s' state", standby)
+            print(f"CSP master is already in {standby} state")
             return
 
         if str(tango_dev.get_state()) in _allowed_modes:
             tango_dev.set_state(DevState.STANDBY)
-            print("Csp Master transitioned to the '%s' state.", standby)
+            print(f"Csp Master transitioned to the {standby} state.")
+            csp_mode_healthState = models.sim_quantities["healthState"]
+            set_enum(csp_mode_healthState, ok, models.time_func())
+            print(f"heathState transitioned to {ok} state")
+
+            csp_mode_cspCbfHealthState = models.sim_quantities["cspCbfHealthState"]
+            set_enum(csp_mode_cspCbfHealthState, ok, models.time_func())
+            print(f"cspCbfHealthState transitioned to {ok} state")
+
+            csp_mode_cspPssHealthState = models.sim_quantities["cspPssHealthState"]
+            set_enum(csp_mode_cspPssHealthState, ok, models.time_func())
+            print(f"cspPssHealthState transitioned to {ok} state")
+
+            csp_mode_cspPstHealthState = models.sim_quantities["cspPstHealthState"]
+            set_enum(csp_mode_cspPstHealthState, ok, models.time_func())
+            print(f"cspPstHealthState transitioned to {ok} state")
+        else:
+            Except.throw_exception(
+                    "STANDBY Command Failed",
+                    "Not allowed",
+                    ErrSeverity.WARN,
+                )
+
+def get_enum_str(quantity):
+    """Returns the enum label of an enumerated data type
+
+    :param quantity: object
+        The quantity object of a DevEnum attribute
+    :return: str
+        Current string value of a DevEnum attribute
+    """
+    EnumClass = enum.IntEnum("EnumLabels", quantity.meta["enum_labels"], start=0)
+    return EnumClass(quantity.last_val).name
+
+
+def set_enum(quantity, label, timestamp):
+    """Sets the quantity last_val attribute to index of label
+
+    :param quantity: object
+        The quantity object from models
+    :param label: str
+        The desired label from enum list
+    :param timestamp: float
+        The time now
+    """
+    value = quantity.meta["enum_labels"].index(label)
+    quantity.set_val(value, timestamp)
