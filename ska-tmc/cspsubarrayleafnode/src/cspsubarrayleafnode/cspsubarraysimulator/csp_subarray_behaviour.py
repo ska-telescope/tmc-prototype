58711af83e7eb3c31bc675de0f603a3cc919c0ba
Rushikesh <rushikesh.ska@gmail.com>
2021-07-27 14:19:18 +0000

    AT1-952 - Resolve review comments on CspSubarray.

--- ska-tmc/cspsubarrayleafnode/src/cspsubarrayleafnode/cspsubarraysimulator/csp_subarray_behaviour.py
+++ ska-tmc/cspsubarrayleafnode/src/cspsubarrayleafnode/cspsubarraysimulator/csp_subarray_behaviour.py
@@ -21,24 +21,22 @@ MODULE_LOGGER = logging.getLogger(__name__)
 
 class OverrideCspSubarray(object):
 
-    def action_on(self, models, tango_dev=None, data_input=[]):
+    def action_on(self, model, tango_dev=None, data_input=[]):
         """Changes the State of the device to ON.
         """
-        on = "ON"
-        ok = "OK"
         _allowed_modes = (
             "OFF",
             "STANDBY"
         )
-        if str(tango_dev.get_state()) in ["ON"]:
-            models.logger.info(f"CSP Subarray is already in {on} state")
+        if str(tango_dev.get_state())== DevState.ON:
+            model.logger.info(f"CSP Subarray is already in ON state")
             return
 
         if str(tango_dev.get_state()) in _allowed_modes:
             tango_dev.set_state(DevState.ON)
-            models.logger.info(f"Csp Subarray transitioned to the {on} state.")
-            csp_mode_healthState = models.sim_quantities["healthState"]
-            set_enum(csp_mode_healthState, ok, models.time_func())
+            model.logger.info(f"Csp Subarray transitioned to the ON state.")
+            csp_mode_healthState = model.sim_quantities["healthState"]
+            set_enum(csp_mode_healthState, "OK", model.time_func())
         else:
             Except.throw_exception(
                     "ON Command Failed",
@@ -46,26 +44,24 @@ class OverrideCspSubarray(object):
                     ErrSeverity.WARN,
                 )
 
-    def action_off(self, models, tango_dev=None, data_input=[]):
+    def action_off(self, model, tango_dev=None, data_input=[]):
         """Changes the State of the device to OFF.
         """
-        off = "OFF"
-        ok = "OK"
         _allowed_modes = (
             "ON",
             "ALARM",
             "STANDBY"
         )
-        if str(tango_dev.get_state()) in ["OFF"]:
-            models.logger.info(f"CSP master is already in {off} state")
+        if str(tango_dev.get_state()) == DevState.OFF:
+            model.logger.info(f"CSP master is already in {off} state")
             return
 
         if str(tango_dev.get_state()) in _allowed_modes:
             tango_dev.set_state(DevState.OFF)
-            models.logger.info(f"Csp Subarray transitioned to the {off} state.")
-            csp_mode_healthState = models.sim_quantities["healthState"]
-            set_enum(csp_mode_healthState, ok, models.time_func())
-            models.logger.info(f"heathState transitioned to {ok} state")
+            model.logger.info(f"Csp Subarray transitioned to the OFF state.")
+            csp_mode_healthState = model.sim_quantities["healthState"]
+            set_enum(csp_mode_healthState, "OK" , model.time_func())
+            model.logger.info(f"heathState transitioned to OK state")
         else:
             Except.throw_exception(
                     "ON Command Failed",
@@ -73,22 +69,22 @@ class OverrideCspSubarray(object):
                     ErrSeverity.WARN,
                 )
 
-    def action_assignresources(self, models, tango_dev=None, data_input=None):
+    def action_assignresources(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate == "EMPTY"):
-            set_enum(obsstate_attribute, "RESOURCING", models.time_func())
+            set_enum(obsstate_attribute, "RESOURCING", model.time_func())
             tango_dev.push_change_event("obsState", 1)
             tango_dev.set_status("ObsState in RESOURCING")
-            models.logger.info("ObsState trasnitioned to RESOURCING")
+            model.logger.info("ObsState trasnitioned to RESOURCING")
 
             time.sleep(1)
-            set_enum(obsstate_attribute, "IDLE", models.time_func())
+            set_enum(obsstate_attribute, "IDLE", model.time_func())
             tango_dev.push_change_event("obsState", 2)
             tango_dev.set_status("ObsState in Idle")
-            models.logger.info("ObsState trasnitioned to IDLE")
+            model.logger.info("ObsState trasnitioned to IDLE")
             
         else:
             Except.throw_exception(
@@ -97,16 +93,16 @@ class OverrideCspSubarray(object):
                 ErrSeverity.WARN,
             )
 
-    def action_endscan(self, models, tango_dev=None, data_input=None):
+    def action_endscan(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate == "SCANNING"):
-            set_enum(obsstate_attribute, "READY", models.time_func())
+            set_enum(obsstate_attribute, "READY", model.time_func())
             tango_dev.push_change_event("obsState", 4)
             tango_dev.set_status("ObsState in READY")
-            models.logger.info("ObsState trasnitioned to READY")            
+            model.logger.info("ObsState trasnitioned to READY")            
         else:
             Except.throw_exception(
                 "EndScan Command Failed",
@@ -114,7 +110,7 @@ class OverrideCspSubarray(object):
                 ErrSeverity.WARN,
             )
         
-    def action_abort(self, models, tango_dev=None, data_input=None):
+    def action_abort(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
         _allowed_obsstate = (
@@ -124,18 +120,18 @@ class OverrideCspSubarray(object):
             "CONFIGURING",
             "RESETTING"
         )
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate in _allowed_obsstate):
-           set_enum(obsstate_attribute, "ABORTING", models.time_func())
+           set_enum(obsstate_attribute, "ABORTING", model.time_func())
            tango_dev.push_change_event("obsState", 6)
            tango_dev.set_status("ObsState in ABORTING")
-           models.logger.info("ObsState trasnitioned to ABORTING")
+           model.logger.info("ObsState trasnitioned to ABORTING")
            time.sleep(1)
-           set_enum(obsstate_attribute, "ABORTED", models.time_func())
+           set_enum(obsstate_attribute, "ABORTED", model.time_func())
            tango_dev.push_change_event("obsState", 7)
            tango_dev.set_status("ObsState in ABORTED")
-           models.logger.info("ObsState trasnitioned to ABORTED")
+           model.logger.info("ObsState trasnitioned to ABORTED")
             
         else:
             Except.throw_exception(
@@ -144,22 +140,22 @@ class OverrideCspSubarray(object):
                 ErrSeverity.WARN,
             )
 
-    def action_releaseallresources(self, models, tango_dev=None, data_input=None):
+    def action_releaseallresources(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate == "IDLE"):
-            set_enum(obsstate_attribute, "RESOURCING", models.time_func())
+            set_enum(obsstate_attribute, "RESOURCING", model.time_func())
             tango_dev.push_change_event("obsState",1)
             tango_dev.set_status("ObsState in RESOURCING")
-            models.logger.info("ObsState trasnitioned to RESOURCING")
+            model.logger.info("ObsState trasnitioned to RESOURCING")
 
             time.sleep(1)
-            set_enum(obsstate_attribute, "EMPTY", models.time_func())
+            set_enum(obsstate_attribute, "EMPTY", model.time_func())
             tango_dev.push_change_event("obsState", 0)
             tango_dev.set_status("ObsState in EMPTY")
-            models.logger.info("ObsState trasnitioned to EMPTY")
+            model.logger.info("ObsState trasnitioned to EMPTY")
            
         else:
             Except.throw_exception(
@@ -168,26 +164,26 @@ class OverrideCspSubarray(object):
                 ErrSeverity.WARN,
             )
 
-    def action_configure(self, models, tango_dev=None, data_input=None):
+    def action_configure(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
         _allowed_obsstate = (
             "IDLE",
             "READY"
         )
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate in _allowed_obsstate):
-            set_enum(obsstate_attribute, "CONFIGURING", models.time_func())
+            set_enum(obsstate_attribute, "CONFIGURING", model.time_func())
             tango_dev.push_change_event("obsState", 3)
             tango_dev.set_status("ObsState in CONFIGURING")
-            models.logger.info("ObsState trasnitioned to CONFIGURING")
+            model.logger.info("ObsState trasnitioned to CONFIGURING")
 
             time.sleep(1)
-            set_enum(obsstate_attribute, "READY", models.time_func())
+            set_enum(obsstate_attribute, "READY", model.time_func())
             tango_dev.push_change_event("obsState", 4)
             tango_dev.set_status("ObsState in READY")
-            models.logger.info("ObsState trasnitioned to READY")
+            model.logger.info("ObsState trasnitioned to READY")
 
         else:
             Except.throw_exception(
@@ -196,22 +192,22 @@ class OverrideCspSubarray(object):
                 ErrSeverity.WARN,
             )
 
-    def action_scan(self, models, tango_dev=None, data_input=None):
+    def action_scan(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate == "READY"):
-            set_enum(obsstate_attribute, "SCANNING", models.time_func())
+            set_enum(obsstate_attribute, "SCANNING", model.time_func())
             tango_dev.push_change_event("obsState", 5)
             tango_dev.set_status("ObsState in SCANNING")
-            models.logger.info("ObsState trasnitioned to SCANNING")
+            model.logger.info("ObsState trasnitioned to SCANNING")
 
             time.sleep(10)
-            set_enum(obsstate_attribute, "READY", models.time_func())
+            set_enum(obsstate_attribute, "READY", model.time_func())
             tango_dev.push_change_event("obsState", 4)
             tango_dev.set_status("ObsState in READY")
-            models.logger.info("ObsState trasnitioned to READY")
+            model.logger.info("ObsState trasnitioned to READY")
         else:
             Except.throw_exception(
                 "Scan Command Failed",
@@ -220,16 +216,16 @@ class OverrideCspSubarray(object):
             )
         
 
-    def action_gotoidle(self, models, tango_dev=None, data_input=None):
+    def action_gotoidle(self, model, tango_dev=None, data_input=None):
         """Changes the State of the device to .
         """
-        obsstate_attribute = models.sim_quantities['obsState']
+        obsstate_attribute = model.sim_quantities['obsState']
         obsstate = get_enum_str(obsstate_attribute)
         if (obsstate == "READY"):
-            set_enum(obsstate_attribute, "IDLE", models.time_func())
+            set_enum(obsstate_attribute, "IDLE", model.time_func())
             tango_dev.push_change_event("obsState", 2)
             tango_dev.set_status("ObsState in IDLE")
-            models.logger.info("ObsState trasnitioned to IDLE")
+            model.logger.info("ObsState trasnitioned to IDLE")
             
         else:
             Except.throw_exception(
@@ -255,7 +251,7 @@ def set_enum(quantity, label, timestamp):
     """Sets the quantity last_val attribute to index of label
 
     :param quantity: object
-        The quantity object from models
+        The quantity object from model
     :param label: str
         The desired label from enum list
     :param timestamp: float
--- ska-tmc/cspsubarrayleafnode/src/cspsubarrayleafnode/cspsubarraysimulator/utils.py
+++ ska-tmc/cspsubarrayleafnode/src/cspsubarrayleafnode/cspsubarraysimulator/utils.py
@@ -38,6 +38,6 @@ def get_tango_server_class(device_name):
     if device_name == "test/nodb/cspsubarray":
         configure_args["test_device_name"] = device_name
     
-    models = configure_device_models(sim_data_files)
-    tango_ds = get_tango_device_server(models, sim_data_files)
+    model = configure_device_models(sim_data_files)
+    tango_ds = get_tango_device_server(model, sim_data_files)
     return tango_ds
