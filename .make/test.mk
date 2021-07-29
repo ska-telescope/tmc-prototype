0a65c17df29ac37a69e35662604cbae454011c3d
Kalyani Thigale <kalyanit.ska@gmail.com>
2021-07-29 05:21:56 +0000

    AT1-962 Fix integration test cases

--- .make/test.mk
+++ .make/test.mk
@@ -3,8 +3,7 @@
 #
 # IMAGE_TO_TEST defines the tag of the Docker image to test
 #
-# Need to check if this is present on CAR
-IMAGE_TO_TEST ?= nexus.engageska-portugal.pt/ska-docker/tango-vscode:0.2.7## docker image that will be run for testing purpose
+IMAGE_TO_TEST ?= artefact.skao.int/ska-tango-images-tango-itango:9.3.4 ## docker image that will be run for testing purpose
 # Test runner - run to completion job in K8s
 TEST_RUNNER = test-runner-$(CI_JOB_ID)-$(KUBE_NAMESPACE)-$(HELM_RELEASE)##name of the pod running the k8s_tests
 #
