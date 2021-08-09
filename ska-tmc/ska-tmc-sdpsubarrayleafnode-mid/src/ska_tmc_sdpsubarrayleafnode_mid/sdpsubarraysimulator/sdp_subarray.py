d6335125ff1850a8192fe61b3bb467a8404389c7
handeSnehal <snehalhande.ska@gmail.com>
2021-08-09 18:17:42 +0530

    AT1-957 Update fqdn in csp and sdp simulator

--- ska-tmc/ska-tmc-sdpsubarrayleafnode-mid/src/ska_tmc_sdpsubarrayleafnode_mid/sdpsubarraysimulator/sdp_subarray.py
+++ ska-tmc/ska-tmc-sdpsubarrayleafnode-mid/src/ska_tmc_sdpsubarrayleafnode_mid/sdpsubarraysimulator/sdp_subarray.py
@@ -15,7 +15,7 @@ def sdp_subarray_simulator():
     if len(sys.argv) > 0:
         device_name = sys.argv[1]
         if device_name.isdigit():
-            device_name = "mid_sdp/elt/subarray_1"
+            device_name = f"mid_sdp/elt/subarray_{device_name}"
     else:
         device_name = "mid_sdp_unset/elt/master"
 
