587d9b825cd9d5eaaca9b46d7e255f37e6f848b6
Rushikesh <rushikesh.ska@gmail.com>
2021-08-16 11:55:52 +0000

    AT1-957 - Enabled environment variable in sdp master .yaml and corrected fqdn for sdp subarray.

--- ska-tmc/ska-tmc-sdpsubarrayleafnode-mid/src/ska_tmc_sdpsubarrayleafnode_mid/sdpsubarraysimulator/sdp_subarray.py
+++ ska-tmc/ska-tmc-sdpsubarrayleafnode-mid/src/ska_tmc_sdpsubarrayleafnode_mid/sdpsubarraysimulator/sdp_subarray.py
@@ -15,9 +15,9 @@ def sdp_subarray_simulator():
     if len(sys.argv) > 0:
         device_name = sys.argv[1]
         if device_name.isdigit():
-            device_name = f"mid_sdp/elt/subarray_{device_name}"
+            device_name = f"mid_sdp/elt/subarray_{device_name[1]}" #To omit 0 from name of device
     else:
-        device_name = "mid_sdp_unset/elt/master"
+        device_name = "mid_sdp_unset/elt/subarray_1"
 
     tango_ds = get_tango_server_class(device_name)
     return tango_ds[0]
