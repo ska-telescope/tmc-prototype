927337159bac96b916187c6e7287bd214fe90b2a
shraddhabajare <shraddhab.ska@gmail.com>
2021-12-31 15:41:14 +0530

    AT1-1034 - Implement adapterfactory and devfactory class

---
+++ src/ska_tmc_sdpsubarrayleafnode/manager/adapters.py
@@ -0,0 +1,80 @@
+# Delete this module once ska-tmc-common classes are ready
+import enum
+
+from ska_tmc_sdpsubarrayleafnode.dev_factory import DevFactory
+
+# TODO: Enable below import when ska-tmc-common is ready
+# from ska_tmc_common.dev_factory import DevFactory
+
+
+class AdapterType(enum.IntEnum):
+    BASE = 0
+    SDPSUBARRAY = 1
+
+
+class AdapterFactory:
+    def __init__(self) -> None:
+        self._adapters = []
+        self._dev_factory = DevFactory()
+
+    def get_or_create_adapter(self, dev_name, adapter_type=AdapterType.BASE):
+        """
+        Get a generic adapter for a device if already created
+        or create new adapter as per the device type and add to adpter list
+
+        :param dev_name: device name
+        :type str
+        """
+        for adapter in self._adapters:
+            if adapter.dev_name == dev_name:
+                return adapter
+
+        new_adapter = None
+        if adapter_type == AdapterType.SDPSUBARRAY:
+            new_adapter = SdpsubarrayAdapter(
+                dev_name, self._dev_factory.get_device(dev_name)
+            )
+        else:
+            new_adapter = BaseAdapter(
+                dev_name, self._dev_factory.get_device(dev_name)
+            )
+
+        self._adapters.append(new_adapter)
+        return new_adapter
+
+
+class BaseAdapter:
+    def __init__(self, dev_name, proxy) -> None:
+        self._proxy = proxy
+        self._dev_name = dev_name
+
+    @property
+    def proxy(self):
+        return self._proxy
+
+    @property
+    def dev_name(self):
+        return self._dev_name
+
+    def On(self):
+        self.proxy.TelescopeOn()
+
+    def Off(self):
+        self.proxy.TelescopeOff()
+
+    def StandBy(self):
+        self.proxy.TelescopeStandBy()
+
+
+class SdpsubarrayAdapter(BaseAdapter):
+    def __init__(self, dev_name, proxy) -> None:
+        super().__init__(dev_name, proxy)
+
+    def AssignResources(self, argin):
+        return self._proxy.AssignResources(argin)
+
+    def ReleaseAllResources(self):
+        return self._proxy.ReleaseAllResources()
+
+    def ReleaseResources(self, argin):
+        return self._proxy.ReleaseResources(argin)
