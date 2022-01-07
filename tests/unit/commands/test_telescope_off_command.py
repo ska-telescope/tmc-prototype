7c7ebac8294f8719cf357c27bb60ef883a054cb8
shraddhabajare <shraddhab.ska@gmail.com>
2022-01-07 18:48:39 +0530

    AT1-1035 - Refactored Restart and ObsReset coammnd. Added unit test cases.

--- tests/unit/commands/test_telescope_off_command.py
+++ tests/unit/commands/test_telescope_off_command.py
@@ -1,81 +1,81 @@
-# import time
+import time
 
-# import pytest
-# from ska_tango_base.base.base_device import SKABaseDevice
-# from ska_tango_base.commands import ResultCode
-# from ska_tmc_common.adapters import SdpSubArrayAdapter
+import pytest
+from ska_tango_base.base.base_device import SKABaseDevice
+from ska_tango_base.commands import ResultCode
+from ska_tmc_common.adapters import SdpSubArrayAdapter
 
-# from ska_tmc_sdpsubarrayleafnode.commands.telescope_off_command import (
-#     TelescopeOff,
-# )
-# from ska_tmc_sdpsubarrayleafnode.exceptions import CommandNotAllowed
-# from tests.helpers.helper_adapter_factory import HelperAdapterFactory
-# from tests.settings import create_cm, logger
-
-
-# @pytest.fixture()
-# def devices_to_load():
-#     return (
-#         {
-#             "class": SKABaseDevice,
-#             "devices": [
-#                 {"name": "mid_sdp/elt/subarray_01"},
-#             ],
-#         },
-#     )
+from ska_tmc_sdpsubarrayleafnode.commands.telescope_off_command import (
+    TelescopeOff,
+)
+from ska_tmc_sdpsubarrayleafnode.exceptions import CommandNotAllowed
+from tests.helpers.helper_adapter_factory import HelperAdapterFactory
+from tests.settings import create_cm, logger
 
 
-# def test_telescope_off_command(tango_context):
-#     logger.info("%s", tango_context)
-#     cm, start_time = create_cm()
-#     elapsed_time = time.time() - start_time
-#     logger.info(
-#         "checked %s devices in %s", len(cm.checked_devices), elapsed_time
-#     )
-
-#     my_adapter_factory = HelperAdapterFactory()
-#     off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
-#     assert off_command.check_allowed()
-#     (result_code, _) = off_command.do()
-#     assert result_code == ResultCode.OK
-#     for adapter in my_adapter_factory.adapters:
-#         if isinstance(adapter, SdpSubArrayAdapter):
-#             adapter.proxy.Off.assert_called()
-#             continue
+@pytest.fixture()
+def devices_to_load():
+    return (
+        {
+            "class": SKABaseDevice,
+            "devices": [
+                {"name": "mid_sdp/elt/subarray_01"},
+            ],
+        },
+    )
 
 
-# def test_telescope_off_command_fail_sdp_subarray(tango_context):
-#     logger.info("%s", tango_context)
-#     cm, start_time = create_cm()
-#     elapsed_time = time.time() - start_time
-#     logger.info(
-#         "checked %s devices in %s", len(cm.checked_devices), elapsed_time
-#     )
-#     my_adapter_factory = HelperAdapterFactory()
+def test_telescope_off_command(tango_context):
+    logger.info("%s", tango_context)
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info(
+        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    )
 
-#     # include exception in TelescopeOff command
-#     failing_dev = "mid_sdp/elt/subarray_01"
-#     my_adapter_factory.get_or_create_adapter(
-#         failing_dev, attrs={"TelescopeOff.side_effect": Exception}
-#     )
-
-#     off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
-#     assert off_command.check_allowed()
-#     (result_code, message) = off_command.do()
-#     assert result_code == ResultCode.FAILED
-#     assert failing_dev in message
+    my_adapter_factory = HelperAdapterFactory()
+    off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
+    assert off_command.check_allowed()
+    (result_code, _) = off_command.do()
+    assert result_code == ResultCode.OK
+    for adapter in my_adapter_factory.adapters:
+        if isinstance(adapter, SdpSubArrayAdapter):
+            adapter.proxy.Off.assert_called()
+            continue
 
 
-# def test_telescope_off_fail_check_allowed(tango_context):
+def test_telescope_off_command_fail_sdp_subarray(tango_context):
+    logger.info("%s", tango_context)
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info(
+        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    )
+    my_adapter_factory = HelperAdapterFactory()
 
-#     logger.info("%s", tango_context)
-#     cm, start_time = create_cm()
-#     elapsed_time = time.time() - start_time
-#     logger.info(
-#         "checked %s devices in %s", len(cm.checked_devices), elapsed_time
-#     )
-#     my_adapter_factory = HelperAdapterFactory()
-#     cm.input_parameter.sdp_subarray_dev_name = ""
-#     off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
-#     with pytest.raises(CommandNotAllowed):
-#         off_command.check_allowed()
+    # include exception in TelescopeOff command
+    failing_dev = "mid_sdp/elt/subarray_01"
+    my_adapter_factory.get_or_create_adapter(
+        failing_dev, attrs={"TelescopeOff.side_effect": Exception}
+    )
+
+    off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
+    assert off_command.check_allowed()
+    (result_code, message) = off_command.do()
+    assert result_code == ResultCode.FAILED
+    assert failing_dev in message
+
+
+def test_telescope_off_fail_check_allowed(tango_context):
+
+    logger.info("%s", tango_context)
+    cm, start_time = create_cm()
+    elapsed_time = time.time() - start_time
+    logger.info(
+        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    )
+    my_adapter_factory = HelperAdapterFactory()
+    cm.input_parameter.sdp_subarray_dev_name = ""
+    off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
+    with pytest.raises(CommandNotAllowed):
+        off_command.check_allowed()
