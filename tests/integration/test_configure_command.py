596e419202fcbfc66326a74c428e9da6a7969c85
shraddhabajare <shraddhab.ska@gmail.com>
2022-01-19 15:08:04 +0530

    AT1-1047 - Add integration test for Configure command

--- tests/integration/test_configure_command.py
+++ tests/integration/test_configure_command.py
@@ -13,8 +13,13 @@ def get_assign_input_str(path):
         assign_input_str = f.read()
     return assign_input_str
 
+def get_configure_input_str(path):
+    with open(path, "r") as f:
+        configure_input_str = f.read()
+    return configure_input_str
 
-def assign_resouces(tango_context, sdpsaln_name, assign_input_str):
+
+def assign_resouces(tango_context, sdpsaln_name, assign_input_str, configure_input_str):
     logger.info("%s", tango_context)
     dev_factory = DevFactory()
     sdpsal_node = dev_factory.get_device(sdpsaln_name)
@@ -22,9 +27,10 @@ def assign_resouces(tango_context, sdpsaln_name, assign_input_str):
     initial_len = len(sdpsal_node.commandExecuted)
     (result, unique_id) = sdpsal_node.TelescopeOn()
     (result, unique_id) = sdpsal_node.AssignResources(assign_input_str)
+    (result, unique_id) = sdpsal_node.Configure(configure_input_str)
     assert result[0] == ResultCode.QUEUED
     start_time = time.time()
-    while len(sdpsal_node.commandExecuted) != initial_len + 2:
+    while len(sdpsal_node.commandExecuted) != initial_len + 3:
         time.sleep(SLEEP_TIME)
         elapsed_time = time.time() - start_time
         if elapsed_time > TIMEOUT:
@@ -34,26 +40,6 @@ def assign_resouces(tango_context, sdpsaln_name, assign_input_str):
         if command[0] == unique_id[0]:
             logger.info("command result: %s", command)
             assert command[2] == "ResultCode.OK"
-
-    # def get_subarray_device(json_model):
-    #     for device in json_model["devices"]:
-    #         if device["dev_name"] == "ska_mid/tm_subarray_node/1":
-    #             return device
-    #     return None
-
-    # if "ska_mid" in sdpsal_node:
-    #     device = get_subarray_device(json.loads(sdpsal_node.internalModel))
-    #     start_time = time.time()
-    #     while len(device["resources"]) == 0:
-    #         time.sleep(SLEEP_TIME)
-    #         device = get_subarray_device(
-    #             json.loads(sdpsal_node.internalModel)
-    #         )
-    #         elapsed_time = time.time() - start_time
-    #         if elapsed_time > TIMEOUT:
-    #             pytest.fail("Timeout occurred while executing the test")
-
-    #     assert len(device["resources"]) > 0
 
 
 @pytest.mark.post_deployment
@@ -69,6 +55,11 @@ def test_assign_res_command_mid(tango_context, sdpsaln_name):
         get_assign_input_str(
             join(
                 dirname(__file__), "..", "data", "command_AssignResources.json"
+            )
+        ),
+        get_configure_input_str(
+            join(
+                dirname(__file__), "..", "data", "command_Configure.json"
             )
         ),
     )
