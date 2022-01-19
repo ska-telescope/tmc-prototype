750ad3035c65e8b4fcebb2bf2452a626aa50be42
shraddhabajare <shraddhab.ska@gmail.com>
2022-01-19 16:42:54 +0530

    AT1-1047 - Fix integration test case for AssignResources command

--- tests/integration/test_release_res_command.py
+++ tests/integration/test_release_res_command.py
@@ -5,35 +5,31 @@ import pytest
 from ska_tango_base.commands import ResultCode
 from ska_tmc_common.dev_factory import DevFactory
 
-from tests.settings import SLEEP_TIME, TIMEOUT, logger
+from tests.settings import SLEEP_TIME, logger
 
 
-# def get_assign_input_str(path):
-#     with open(path, "r") as f:
-#         assign_input_str = f.read()
-#     return assign_input_str
-
-def get_assign_input_str(assign_input_file="command_AssignResources.json"):
-    path = join(dirname(__file__), "..", "data", assign_input_file)
+def get_input_str(path):
     with open(path, "r") as f:
         assign_input_str = f.read()
     return assign_input_str
 
 
-def assign_resouces(tango_context, sdpsaln_name, assign_input_str):
+def release_resources(tango_context, sdpsaln_name, assign_input_str):
     logger.info("%s", tango_context)
     dev_factory = DevFactory()
     sdpsal_node = dev_factory.get_device(sdpsaln_name)
-
     initial_len = len(sdpsal_node.commandExecuted)
-    (result, unique_id) = sdpsal_node.TelescopeOn()
-    sdpsal_node.AssignResources(assign_input_str)
+    (result, unique_id) = sdpsal_node.TelescopOn()
+    (result, unique_id) = sdpsal_node.AssignResources(assign_input_str)
+    (result, unique_id) = sdpsal_node.ReleaseResources()
+    if result[0] != ResultCode.QUEUED:
+        logger.error("Result: %s message: %s", result[0], unique_id)
     assert result[0] == ResultCode.QUEUED
     start_time = time.time()
-    while len(sdpsal_node.commandExecuted) != initial_len + 2:
+    while len(sdpsal_node.commandExecuted) != initial_len + 3:
         time.sleep(SLEEP_TIME)
         elapsed_time = time.time() - start_time
-        if elapsed_time > TIMEOUT:
+        if elapsed_time > 100:
             pytest.fail("Timeout occurred while executing the test")
 
     for command in sdpsal_node.commandExecuted:
@@ -44,13 +40,11 @@ def assign_resouces(tango_context, sdpsaln_name, assign_input_str):
 
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
-@pytest.mark.parametrize(
-    "sdpsaln_name",
-    [("ska_mid/tm_leaf_node/sdp_subarray01")],
-)
-def test_assign_res_command_mid(tango_context, sdpsaln_name):
-    return assign_resouces(
+def test_release_res_command_mid(tango_context):
+    return release_resources(
         tango_context,
-        sdpsaln_name,
-        assign_input_str = get_assign_input_str(),
+        "ska_mid/tm_central/central_node",
+        get_input_str(
+            join(dirname(__file__), "..", "data", "command_AssignResources.json")
+        ),
     )
