7693d24dbb25e9ca4f1203509dcc7fc766c1670c
shraddhabajare <shraddhab.ska@gmail.com>
2022-01-31 18:22:17 +0530

    AT1-1056 - Resolve review comment

--- tests/ska_tmc_sdpsubarrayleafnode/unit/test_input.py
+++ tests/ska_tmc_sdpsubarrayleafnode/unit/test_input.py
@@ -3,5 +3,5 @@ from ska_tmc_sdpsubarrayleafnode.model.input import SdpSLNInputParameter
 
 def test_properties():
     input = SdpSLNInputParameter(None)
-    input.sdp_subarray_dev_name = "6"
-    assert input.sdp_subarray_dev_name == ("6")
+    input.sdp_subarray_dev_name = "mid_sdp/elt/subarray_1"
+    assert input.sdp_subarray_dev_name == ("mid_sdp/elt/subarray_1")
