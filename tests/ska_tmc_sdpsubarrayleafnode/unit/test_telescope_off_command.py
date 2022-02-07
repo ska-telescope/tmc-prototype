a138a54d6922764d28f9b499fa65e62739913946
shraddhabajare <shraddhab.ska@gmail.com>
2022-02-07 22:56:02 +0530

    AT1-1061 - Update unit tests for TelescopeOn and TelescopeOff command.

--- tests/ska_tmc_sdpsubarrayleafnode/unit/test_telescope_off_command.py
+++ tests/ska_tmc_sdpsubarrayleafnode/unit/test_telescope_off_command.py
@@ -8,7 +8,8 @@ from ska_tmc_sdpsubarrayleafnode.commands.telescope_off_command import (
     TelescopeOff,
 )
 from ska_tmc_sdpsubarrayleafnode.exceptions import DeviceUnresponsive
-from ska_tmc_sdpsubarrayleafnode.model.input import SdpSLNInputParameter
+
+# from ska_tmc_sdpsubarrayleafnode.model.input import SdpSLNInputParameter
 from tests.helpers.helper_adapter_factory import HelperAdapterFactory
 from tests.settings import (
     SDP_SUBARRAY_DEVICE,
@@ -18,7 +19,7 @@ from tests.settings import (
 )
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_off_command(tango_context):
     logger.info("%s", tango_context)
     _, off_command, my_adapter_factory = get_sdpsln_command_obj(
@@ -33,40 +34,35 @@ def test_telescope_off_command(tango_context):
         adapter.proxy.Off.assert_called()
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_off_command_fail_sdp_subarray(tango_context):
     logger.info("%s", tango_context)
-    input_parameter = SdpSLNInputParameter(None)
-    cm, start_time = create_cm(
-        "SdpSLNComponentManager", input_parameter, SDP_SUBARRAY_DEVICE
-    )
-    elapsed_time = time.time() - start_time
-    logger.info(
-        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
-    )
-    my_adapter_factory = HelperAdapterFactory()
+    # input_parameter = SdpSLNInputParameter(None)
+    cm, _ = create_cm("SdpSLNComponentManager", SDP_SUBARRAY_DEVICE)
+    # elapsed_time = time.time() - start_time
+    # logger.info(
+    #     "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    # )
+    adapter_factory = HelperAdapterFactory()
 
     # include exception in TelescopeOff command
-    failing_dev = "mid_sdp/elt/subarray_1"
-    my_adapter_factory.get_or_create_adapter(
-        failing_dev, attrs={"TelescopeOff.side_effect": Exception}
+    adapter_factory.get_or_create_adapter(
+        SDP_SUBARRAY_DEVICE, attrs={"TelescopeOff.side_effect": Exception}
     )
 
-    off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
+    off_command = TelescopeOff(cm, cm.op_state_model, adapter_factory)
     assert off_command.check_allowed()
     (result_code, message) = off_command.do()
     assert result_code == ResultCode.FAILED
-    assert failing_dev in message
+    assert SDP_SUBARRAY_DEVICE in message
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_off_fail_check_allowed(tango_context):
 
     logger.info("%s", tango_context)
-    cm, off_command, my_adapter_factory = get_sdpsln_command_obj(
-        TelescopeOff, None
-    )
-    cm.input_parameter.sdp_subarray_dev_name = ""
-    off_command = TelescopeOff(cm, cm.op_state_model, my_adapter_factory)
+    cm, off_command, _ = get_sdpsln_command_obj(TelescopeOff, None)
+    devInfo = cm.get_device()
+    devInfo.update_unresponsive(True)
     with pytest.raises(DeviceUnresponsive):
         off_command.check_allowed()
--- tests/ska_tmc_sdpsubarrayleafnode/unit/test_telescope_on_command.py
+++ tests/ska_tmc_sdpsubarrayleafnode/unit/test_telescope_on_command.py
@@ -8,7 +8,8 @@ from ska_tmc_sdpsubarrayleafnode.commands.telescope_on_command import (
     TelescopeOn,
 )
 from ska_tmc_sdpsubarrayleafnode.exceptions import DeviceUnresponsive
-from ska_tmc_sdpsubarrayleafnode.model.input import SdpSLNInputParameter
+
+# from ska_tmc_sdpsubarrayleafnode.model.input import SdpSLNInputParameter
 from tests.helpers.helper_adapter_factory import HelperAdapterFactory
 from tests.settings import (
     SDP_SUBARRAY_DEVICE,
@@ -18,7 +19,7 @@ from tests.settings import (
 )
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_on_command(tango_context):
     logger.info("%s", tango_context)
     _, on_command, my_adapter_factory = get_sdpsln_command_obj(
@@ -33,38 +34,35 @@ def test_telescope_on_command(tango_context):
         adapter.proxy.On.assert_called()
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_on_command_fail_sdp_subarray(tango_context):
     logger.info("%s", tango_context)
-    input_parameter = SdpSLNInputParameter(None)
-    cm, start_time = create_cm(
-        "SdpSLNComponentManager", input_parameter, SDP_SUBARRAY_DEVICE
-    )
-    elapsed_time = time.time() - start_time
-    logger.info(
-        "checked %s devices in %s", len(cm.checked_devices), elapsed_time
-    )
-    my_adapter_factory = HelperAdapterFactory()
+    # input_parameter = SdpSLNInputParameter(None)
+    cm, _ = create_cm("SdpSLNComponentManager", SDP_SUBARRAY_DEVICE)
+    # elapsed_time = time.time() - start_time
+    # logger.info(
+    #     "checked %s devices in %s", len(cm.checked_devices), elapsed_time
+    # )
+    adapter_factory = HelperAdapterFactory()
 
     # include exception in TelescopeOn command
-    failing_dev = "mid_sdp/elt/subarray_1"
-
-    my_adapter_factory.get_or_create_adapter(
-        failing_dev, attrs={"TelescopeOn.side_effect": Exception}
+    adapter_factory.get_or_create_adapter(
+        SDP_SUBARRAY_DEVICE, attrs={"TelescopeOn.side_effect": Exception}
     )
 
-    on_command = TelescopeOn(cm, cm.op_state_model, my_adapter_factory)
+    on_command = TelescopeOn(cm, cm.op_state_model, adapter_factory)
     assert on_command.check_allowed()
     (result_code, message) = on_command.do()
     assert result_code == ResultCode.FAILED
-    assert failing_dev in message
+    assert SDP_SUBARRAY_DEVICE in message
 
 
-@pytest.mark.sdpsln
+@pytest.mark.shraddha
 def test_telescope_on_fail_check_allowed(tango_context):
 
     logger.info("%s", tango_context)
     cm, on_command, _ = get_sdpsln_command_obj(TelescopeOn, None)
-    cm.input_parameter.sdp_subarray_dev_name = ""
+    devInfo = cm.get_device()
+    devInfo.update_unresponsive(True)
     with pytest.raises(DeviceUnresponsive):
         on_command.check_allowed()
