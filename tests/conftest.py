83090409422dd5a9d4a37b5ab03b1458a067d4d1
shraddhabajare <shraddhab.ska@gmail.com>
2022-02-10 12:07:37 +0530

    AT1-1064 - Add integration test for Disable command on sdpmln

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ import pytest
 import tango
 from ska_tmc_common.dev_factory import DevFactory
 
-# from tests.helpers.helper_subarray_device import HelperSubArrayDevice
 from ska_tmc_common.test_helpers.helper_subarray_device import (
     HelperSubArrayDevice,
+)
+from ska_tmc_common.test_helpers.helper_state_device import (
+    HelperStateDevice,
 )
 from tango.test_context import MultiDeviceTestContext
 
@@ -47,6 +49,11 @@ def devices_to_load():
             "class": HelperSubArrayDevice,
             "devices": [
                 {"name": "mid_sdp/elt/subarray_1"},
+            ],
+        },
+        {
+            "class": HelperStateDevice,
+            "devices": [
                 {"name": "mid_sdp/elt/master"},
             ],
         },
