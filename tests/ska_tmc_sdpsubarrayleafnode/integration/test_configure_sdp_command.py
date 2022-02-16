10517cb969575af0982842217ff1bd273a9bc0fd
shraddhabajare <shraddhab.ska@gmail.com>
2022-02-16 20:30:53 +0530

    AT1-1057 - Add tear down for Configure command integration test case

--- tests/ska_tmc_sdpsubarrayleafnode/integration/test_configure_sdp_command.py
+++ tests/ska_tmc_sdpsubarrayleafnode/integration/test_configure_sdp_command.py
@@ -7,6 +7,7 @@ from ska_tango_base.control_model import ObsState
 from ska_tmc_common.dev_factory import DevFactory
 
 from tests.settings import SLEEP_TIME, TIMEOUT, logger
+from tests.ska_tmc_sdpsubarrayleafnode.integration.common import tear_down
 
 
 def get_assign_input_str(path):
@@ -38,8 +39,19 @@ def configure(
     sdp_subarray = dev_factory.get_device("mid_sdp/elt/subarray_1")
     sdp_subarray.SetDirectObsState(ObsState.IDLE)
     assert sdp_subarray.obsState == ObsState.IDLE
-
+    time.sleep(SLEEP_TIME)
     (result, unique_id) = sdpsal_node.Configure(configure_input_str)
+    sdp_subarray.SetDirectObsState(ObsState.READY)
+    assert sdp_subarray.obsState == ObsState.READY
+    # time.sleep(SLEEP_TIME)
+    # (result, unique_id) = sdpsal_node.End()
+    # sdp_subarray.SetDirectObsState(ObsState.IDLE)
+    # assert sdp_subarray.obsState == ObsState.IDLE
+    # time.sleep(SLEEP_TIME)
+    # (result, unique_id) = sdpsal_node.ReleaseResources(configure_input_str)
+    # sdp_subarray.SetDirectObsState(ObsState.EMPTY)
+    # assert sdp_subarray.obsState == ObsState.EMPTY
+
     assert result[0] == ResultCode.QUEUED
     start_time = time.time()
     while len(sdpsal_node.commandExecuted) != initial_len + 3:
@@ -53,7 +65,49 @@ def configure(
             logger.info("command result: %s", command)
             assert command[2] == "ResultCode.OK"
 
+    tear_down(dev_factory, sdp_subarray)
 
+# def tear_down(dev_factory, sdp_subarray):
+#     sdp_subarray = dev_factory.get_device(sdp_subarray)
+#     sdp_subarray_obsstate = sdp_subarray.read_attribute("obsState")
+#     logger.info(f"SDP Subarray ObsState: {sdp_subarray_obsstate.value}")
+
+#     if sdp_subarray_obsstate.value == 0:
+#         sdp_subarray.Off()
+
+#     if sdp_subarray_obsstate.value == 2:
+#         sdp_subarray.ReleaseResources()
+#         sdp_subarray = dev_factory.get_device("mid_sdp/elt/subarray_1")
+#         sdp_subarray.SetDirectObsState(ObsState.EMPTY)
+#         time.sleep(0.5)
+#         sdp_subarray.Off()
+#         # sdp_subarray_obsstate = sdp_subarray.read_attribute("obsState")
+#         # logger.info(f"SDP Subarray ObsState: {sdp_subarray_obsstate.value}")
+
+#     if sdp_subarray_obsstate.value == 4 or 5:
+#         sdp_subarray.Abort()
+#         sdp_subarray = dev_factory.get_device("mid_sdp/elt/subarray_1")
+#         sdp_subarray.SetDirectObsState(ObsState.ABORTED)
+#         time.sleep(1)
+#         sdp_subarray.Restart()
+#         sdp_subarray = dev_factory.get_device("mid_sdp/elt/subarray_1")
+#         sdp_subarray.SetDirectObsState(ObsState.EMPTY)
+#         time.sleep(1)
+#         sdp_subarray.Off()
+#         # sdp_subarray_obsstate = sdp_subarray.read_attribute("obsState")
+#         # logger.info(f"SDP Subarray ObsState: {sdp_subarray_obsstate.value}")
+
+#     if sdp_subarray_obsstate.value == 7:
+#         sdp_subarray.Restart()
+#         sdp_subarray = dev_factory.get_device("mid_sdp/elt/subarray_1")
+#         sdp_subarray.SetDirectObsState(ObsState.EMPTY)
+#         time.sleep(1)
+#         sdp_subarray.Off()
+#         # sdp_subarray_obsstate = sdp_subarray.read_attribute("obsState")
+#         # logger.info(f"SDP Subarray ObsState: {sdp_subarray_obsstate.value}")
+
+
+@pytest.mark.ncra
 @pytest.mark.post_deployment
 @pytest.mark.SKA_mid
 @pytest.mark.parametrize(
