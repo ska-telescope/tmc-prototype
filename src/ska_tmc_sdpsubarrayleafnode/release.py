671615bca43142a0c2e4ed2e236462db8868f992
akshaysonawane <akshayss.ska@gmail.com>
2022-07-15 16:47:55 +0530

    HM-4 : release new tag

--- src/ska_tmc_sdpsubarrayleafnode/release.py
+++ src/ska_tmc_sdpsubarrayleafnode/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska_tmc_sdpsubarrayleafnode"""
-version = "0.8.20"
+version = "0.8.21"
 version_info = version.split(".")
 description = """"""
 author = "Team NCRA"
