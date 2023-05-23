324d6e606b269c53f783325f2ce5d87f7923b8ae
manthan.ska <manthan.ska@gmail.com>
2023-05-23 18:55:25 +0530

    SAH-1326:Revert changes

--- src/ska_tmc_sdpmasterleafnode/commands/on_command.py
+++ src/ska_tmc_sdpmasterleafnode/commands/on_command.py
@@ -27,7 +27,6 @@ class On(SdpMLNCommand):
         ret_code, message = self.init_adapter()
         if ret_code == ResultCode.FAILED:
             return ret_code, message
-        self.check_unresponsive()
         result = self.call_adapter_method(
             "Sdp Master", self.sdp_master_adapter, "On"
         )
