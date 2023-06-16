1cf81c5918ce91a653c303dd201f26350aaf3603
Muskan Sharma <muskans.ska@gmail.com>
2023-06-16 14:51:18 +0530

    HM-239: Version update.

--- src/ska_tmc_sdpsubarrayleafnode/release.py
+++ src/ska_tmc_sdpsubarrayleafnode/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska_tmc_sdpsubarrayleafnode"""
-version = "0.9.3"
+version = "0.10.0"
 version_info = version.split(".")
 description = """"""
 author = "Team Himalaya, Team Sahyadri"
