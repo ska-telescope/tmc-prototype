f69c1553a79ea10189b5e44cc1cfcf72dda3b798
manthan.ska <manthan.ska@gmail.com>
2023-07-28 17:51:25 +0530

    SAH-1368: Updated test case

--- tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_attribute.py
+++ tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_attribute.py
@@ -1,46 +1,17 @@
 import pytest
-import tango
 from ska_control_model import HealthState
-from ska_tango_base.control_model import ControlMode, SimulationMode, TestMode
+from ska_tmc_common.dev_factory import DevFactory
 from tango import DevState
-from tango.test_utils import DeviceTestContext
-
-from ska_tmc_sdpmasterleafnode import release
-from ska_tmc_sdpmasterleafnode.sdp_master_leaf_node import SdpMasterLeafNode
 
 
-@pytest.fixture
-def sdpmln_device(request):
-    """Create DeviceProxy for tests"""
-    true_context = request.config.getoption("--true-context")
-    if not true_context:
-        with DeviceTestContext(SdpMasterLeafNode) as proxy:
-            yield proxy
-    else:
-        database = tango.Database()
-        instance_list = database.get_device_exported_for_class(
-            "SdpMasterLeafNode"
-        )
-        for instance in instance_list.value_string:
-            yield tango.DeviceProxy(instance)
-            break
-
-
-@pytest.mark.sdpmln
-def test_attributes(sdpmln_device):
-    assert sdpmln_device.State() == DevState.ON
-    sdpmln_device.loggingTargets = ["console::cout"]
-    assert "console::cout" in sdpmln_device.loggingTargets
-    sdpmln_device.testMode = TestMode.NONE
-    assert sdpmln_device.testMode == TestMode.NONE
-    sdpmln_device.simulationMode = SimulationMode.FALSE
-    assert sdpmln_device.testMode == SimulationMode.FALSE
-    sdpmln_device.controlMode = ControlMode.REMOTE
-    assert sdpmln_device.controlMode == ControlMode.REMOTE
-    sdpmln_device.sdpMasterDevName = "sdpmln"
-    assert sdpmln_device.sdpMasterDevName == "sdpmln"
-    assert sdpmln_device.versionId == release.version
-    assert sdpmln_device.buildState == (
-        "{},{},{}".format(release.name, release.version, release.description)
-    )
-    assert sdpmln_device.healthState == HealthState.OK
+@pytest.mark.test1
+def test_attributes(tango_context, sdp_master_device):
+    dev_factory = DevFactory()
+    sdpmln_node = dev_factory.get_device(sdp_master_device)
+    assert sdpmln_node.On()
+    assert sdpmln_node.State() == DevState.ON
+    assert sdpmln_node.Off()
+    assert sdpmln_node.State() == DevState.OFF
+    assert sdpmln_node.healthState == HealthState.OK
+    assert not sdpmln_node.isSubsystemAvailable
+    assert sdpmln_node.sdpMasterDevName == "sdpmln"
