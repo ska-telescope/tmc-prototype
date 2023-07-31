c3be227e9b2c404db88bc350fd0924cfeef57360
manthan.ska <manthan.ska@gmail.com>
2023-07-31 10:49:23 +0530

    SAH-1368: Updated unit test case

--- tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_attribute.py
+++ tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_attribute.py
@@ -3,9 +3,9 @@ from ska_tmc_common.dev_factory import DevFactory
 from tango import DevState
 
 
-def test_attributes(tango_context, sdp_master_device):
+def test_attributes(tango_context, SdpMasterLeafNode):
     dev_factory = DevFactory()
-    sdpmln_node = dev_factory.get_device(sdp_master_device)
+    sdpmln_node = dev_factory.get_device(SdpMasterLeafNode)
     assert sdpmln_node.On()
     assert sdpmln_node.State() == DevState.ON
     assert sdpmln_node.Off()
