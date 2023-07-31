e1b45bb7df318aaf2392aecaef4143c74e73d54a
manthan.ska <manthan.ska@gmail.com>
2023-07-31 16:03:35 +0530

    SAH-1368: Updated unit test case

--- tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_healthstate.py
+++ tests/ska_tmc_sdpmasterleafnode/unit/test_sdpmln_healthstate.py
@@ -1,8 +1,6 @@
 import pytest
 import tango
 from ska_control_model import HealthState
-from ska_tmc_common.dev_factory import DevFactory
-from tango import DevState
 from tango.test_utils import DeviceTestContext
 
 from ska_tmc_sdpmasterleafnode.sdp_master_leaf_node import SdpMasterLeafNode
@@ -13,7 +11,7 @@ def sdpmln_device(request):
     """Create DeviceProxy for tests"""
     true_context = request.config.getoption("--true-context")
     if not true_context:
-        with DeviceTestContext(SdpMasterLeafNode) as proxy:
+        with DeviceTestContext(SdpMasterLeafNode, process=True) as proxy:
             yield proxy
     else:
         database = tango.Database()
@@ -25,13 +23,5 @@ def sdpmln_device(request):
             break
 
 
-@pytest.mark.test1
-def test_attributes(sdpmln_device, SdpMasterLeafNode):
-    dev_factory = DevFactory()
-    sdpmln_device = dev_factory.get_device(SdpMasterLeafNode)
-    assert sdpmln_device.On()
-    assert sdpmln_device.State() == DevState.ON
-    assert sdpmln_device.Off()
-    assert sdpmln_device.State() == DevState.OFF
+def test_attributes(sdpmln_device):
     assert sdpmln_device.healthState == HealthState.OK
-    assert not sdpmln_device.isSubsystemAvailable
