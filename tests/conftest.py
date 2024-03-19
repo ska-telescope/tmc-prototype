e20917499282ea9f8e333444ad0ab44a27784b18
devesh.ska <devesh.ska@gmail.com>
2024-03-19 18:30:09 +0530

    HM-429:Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -93,9 +93,7 @@ def tango_context(devices_to_load, request):
     true_context = request.config.getoption("--true-context")
     logging.info("true context: %s", true_context)
     if not true_context:
-        with MultiDeviceTestContext(
-            devices_to_load, process=True, debug=5
-        ) as context:
+        with MultiDeviceTestContext(devices_to_load, process=True) as context:
             DevFactory._test_context = context
             logging.info("test context set")
             yield context
