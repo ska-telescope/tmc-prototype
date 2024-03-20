1cc586d8e41e6b9d1b65642fd749f2c7a418ef5f
devesh.ska <devesh.ska@gmail.com>
2024-03-20 22:44:56 +0530

    HM-429:add log

--- src/ska_tmc_sdpsubarrayleafnode/commands/sdp_sln_command.py
+++ src/ska_tmc_sdpsubarrayleafnode/commands/sdp_sln_command.py
@@ -135,6 +135,7 @@ class SdpSLNCommand(TmcLeafNodeCommand):
         result: Optional[ResultCode] = kwargs.get("result")
         status: TaskStatus = kwargs.get("status", TaskStatus.COMPLETED)
         message: str = kwargs.get("message")
+        self.logger.info(f"updating callback {result} {status} {message}")
         if result == ResultCode.FAILED:
             self.task_callback(
                 status=status,
