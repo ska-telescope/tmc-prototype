71144d0da78538f9024ca4c144bdb45ba3f943c0
manthan <manthan.ska@gmail.com>
2024-08-20 10:08:47 +0530

    SAH-1565: Update tag related changes

--- src/ska_tmc_sdpsubarrayleafnode/release.py
+++ src/ska_tmc_sdpsubarrayleafnode/release.py
@@ -10,7 +10,7 @@
 """Release information for Python Package"""
 
 name = """ska_tmc_sdpsubarrayleafnode"""
-version = "0.16.4"
+version = "0.16.5"
 version_info = version.split(".")
 description = """"""
 author = "Team Himalaya, Team Sahyadri"
