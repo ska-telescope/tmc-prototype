1ec893e21354f37bea6c2a93eeb1f8906e756d6b
krishnakumarsengar <krishnaks.ska@gmail.com>
2024-12-23 17:35:46 +0530

    SAH-1635: Updated test.

--- tests/ska_tmc_sdpsubarrayleafnode/integration/test_scan_error_propogation_and_timeout.py
+++ tests/ska_tmc_sdpsubarrayleafnode/integration/test_scan_error_propogation_and_timeout.py
@@ -7,7 +7,12 @@ from ska_tango_base.control_model import ObsState
 from ska_tmc_common.dev_factory import DevFactory
 
 from tests.conftest import COMMAND_COMPLETED
-from tests.settings import FAILED_RESULT_DEFECT, RESET_DEFECT, logger
+from tests.settings import (
+    FAILED_RESULT_DEFECT,
+    RESET_DEFECT,
+    SDP_ERROR,
+    logger,
+)
 from tests.ska_tmc_sdpsubarrayleafnode.integration.common import tear_down
 
 
@@ -75,7 +80,10 @@ def scan(sdpsaln_name, device, json_factory, change_event_callbacks):
     assert result[0] == ResultCode.QUEUED
 
     change_event_callbacks["longRunningCommandResult"].assert_change_event(
-        (unique_id[0], '[3, ""]'),
+        (
+            unique_id[0],
+            SDP_ERROR,
+        ),
         lookahead=6,
     )
     sdp_subarray.SetDefective(RESET_DEFECT)
