f3d0b59b2ce097d1c83e54c15aeefd4429cd7a5a
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-04-04 03:12:54 +0530

    HM-640 : Fixing testcases

--- tests/ska_tmc_sdpsubarrayleafnode/integration/test_scan_error_propogation_and_timeout.py
+++ tests/ska_tmc_sdpsubarrayleafnode/integration/test_scan_error_propogation_and_timeout.py
@@ -93,9 +93,10 @@ def scan_error_propagation(
     logger.info(f"Command ID: {unique_id} Returned result: {result}")
     assert result[0] == ResultCode.QUEUED
     SDP_ERROR = (
-        '[3, "The invocation of the Scan command is failed on Sdp '
-        + f"Subarray Device {device} "
-        + 'Reason: Error in calling the Scan command on Sdp Subarray"]'
+        '[3, "The invocation of the Scan command is failed on SdpSubarray'
+        + f" Device {device}Reason: Error in calling the Scan command "
+        + "on Sdp Subarray.The command has NOT been executed.This device will "
+        + 'continue with normal operation."]'
     )
     change_event_callbacks["longRunningCommandResult"].assert_change_event(
         (
