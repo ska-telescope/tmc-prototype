0c3efa3112d763e3a718b4c8e06f69bb55ac9ba5
shraddhabhat06 <shraddhabhat.ska@gmail.com>
2025-04-03 23:00:00 +0530

    HM-640 : Fixing testcases

--- src/ska_tmc_sdpsubarrayleafnode/commands/end_scan_command.py
+++ src/ska_tmc_sdpsubarrayleafnode/commands/end_scan_command.py
@@ -75,12 +75,11 @@ class EndScan(SdpSLNCommand):
             )
             return (
                 ResultCode.FAILED,
-                "The invocation of the EndScan "
-                + "command is failed on SDP Subarray "
-                + "Device "
+                "The invocation of the EndScan command is "
+                + "failed on SDP Subarray Device "
                 + f"{self.sdp_subarray_adapter.dev_name} "
-                + "Reason: Error in invoking "
-                + "EndScan command on SDP Subarray",
+                + "Reason: Error in "
+                + "invoking EndScan command on SDP Subarray ",
             )
         return (
             ResultCode.OK,
